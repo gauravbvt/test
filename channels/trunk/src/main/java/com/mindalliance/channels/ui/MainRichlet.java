570b59ec53fa9fe8aa5411fdf441b4848949fd61
davidf <davidf@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2007-02-27 08:36:10 +0000

    sample mxgraph app generates click events on overlays
    git-svn-id: https://wush.net/svn/mindalliance@43 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- channels/trunk/src/main/java/com/mindalliance/channels/ui/MainRichlet.java
+++ channels/trunk/src/main/java/com/mindalliance/channels/ui/MainRichlet.java
@@ -10,8 +10,17 @@ import org.zkoss.zk.ui.GenericRichlet;
 import org.zkoss.zk.ui.Page;
 import org.zkoss.zul.Window;
 
-import com.mindalliance.zk.mxgraph.*;
-import com.mindalliance.zk.mxgraph.dto.*;
+import com.mindalliance.zk.mxgraph.MxCell;
+import com.mindalliance.zk.mxgraph.MxCircleLayout;
+import com.mindalliance.zk.mxgraph.MxConstants;
+import com.mindalliance.zk.mxgraph.MxGraph;
+import com.mindalliance.zk.mxgraph.MxOverlay;
+import com.mindalliance.zk.mxgraph.MxPanningHandler;
+import com.mindalliance.zk.mxgraph.MxStyleSheet;
+import com.mindalliance.zk.mxgraph.MxVertex;
+import com.mindalliance.zk.mxgraph.command.ClickOverlayCommand;
+import com.mindalliance.zk.mxgraph.dto.Menu;
+import com.mindalliance.zk.mxgraph.event.OverlayClickListener;
 
 public class MainRichlet extends GenericRichlet {
 
@@ -22,12 +31,29 @@ public class MainRichlet extends GenericRichlet {
 		graph.setLayout(new MxCircleLayout(40));
 		graph.setWidth("400px");
 		graph.setHeight("400px");
+		
 		graph.setStyle("overflow:hidden; background:url('/channels/images/grid.gif');"); // overflow:hidden not needed anymore?
 		// graph.setProperty(MxGraph.BACKGROUND_IMAGE, "/channels/images/grid.gif", false);
 		// graph.setProperty(MxGraph.AUTO_SIZE, true, false);
 		graph.getPanningHandler().setProperty(MxPanningHandler.IS_SELECT_ON_POPUP, false, false);
 		MxVertex node1 = graph.addVertex("Node A", 0, 0, 60, 30);
 		MxVertex node2 = graph.addVertex("Node B", 0, 0, 60, 30);
+		MxOverlay overlay = new MxOverlay("/channels/images/16x16/add2.png", "Click to create a new node", 4, 4);
+		overlay.addClickListener(new OverlayClickListener() {
+			public void onClick(MxGraph graph, MxCell cell) {
+				MxVertex vertex = new MxVertex(cell.getId(), 0,0,60,30);
+				MxOverlay overlay = new MxOverlay("/channels/images/16x16/arrow_right_green.png", "Using the same listener", 4, 4);
+				overlay.addClickListener(this);
+				vertex.setOverlay(overlay);
+				graph.addVertex(vertex);
+				if (cell instanceof MxVertex) {
+					graph.addEdge("Edgy", (MxVertex)cell, vertex);
+				}
+			}
+		});
+		graph.setOverlay(node1, overlay);
+		
+
 		graph.addEdge("Edge 1", node1, node2);
 		// Define default vertex menu
 		Menu menu = Menu.named(Menu.CELL)
