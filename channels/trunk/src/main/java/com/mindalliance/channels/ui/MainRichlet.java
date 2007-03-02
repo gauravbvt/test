6dcf8546d3a048f63a347acf2341fcfb5860180d
davidf <davidf@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2007-03-02 16:22:55 +0000

    Multi-overlays
    git-svn-id: https://wush.net/svn/mindalliance@49 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- channels/trunk/src/main/java/com/mindalliance/channels/ui/MainRichlet.java
+++ channels/trunk/src/main/java/com/mindalliance/channels/ui/MainRichlet.java
@@ -11,15 +11,15 @@ import org.zkoss.zk.ui.Page;
 import org.zkoss.zul.Window;
 
 import com.mindalliance.zk.mxgraph.MxCell;
-import com.mindalliance.zk.mxgraph.MxCircleLayout;
+import com.mindalliance.zk.mxgraph.MxCellListener;
 import com.mindalliance.zk.mxgraph.MxConstants;
+import com.mindalliance.zk.mxgraph.MxFastOrganicLayout;
 import com.mindalliance.zk.mxgraph.MxGraph;
 import com.mindalliance.zk.mxgraph.MxOverlay;
 import com.mindalliance.zk.mxgraph.MxPanningHandler;
 import com.mindalliance.zk.mxgraph.MxStyleSheet;
 import com.mindalliance.zk.mxgraph.MxVertex;
 import com.mindalliance.zk.mxgraph.dto.Menu;
-import com.mindalliance.zk.mxgraph.event.OverlayClickListener;
 
 public class MainRichlet extends GenericRichlet {
 
@@ -27,31 +27,47 @@ public class MainRichlet extends GenericRichlet {
 		page.setTitle("mxGraph test");
 		Window w = new Window("mxGraph", "normal", false);
 		MxGraph graph = new MxGraph();
-		graph.setLayout(new MxCircleLayout(40));
-		graph.setWidth("400px");
-		graph.setHeight("400px");
+		//graph.setLayout(new MxCircleLayout(40));
+		graph.setLayout(new MxFastOrganicLayout());
+		//graph.setLayout(new MxCompactTreeLayout());
+		graph.setWidth("800px");
+		graph.setHeight("800px");
 		
 		graph.setStyle("overflow:hidden; background:url('/channels/images/grid.gif');"); // overflow:hidden not needed anymore?
 		// graph.setProperty(MxGraph.BACKGROUND_IMAGE, "/channels/images/grid.gif", false);
 		// graph.setProperty(MxGraph.AUTO_SIZE, true, false);
 		graph.getPanningHandler().setProperty(MxPanningHandler.IS_SELECT_ON_POPUP, false, false);
 		MxVertex node1 = graph.addVertex("Node A", 0, 0, 60, 30);
-		MxVertex node2 = graph.addVertex("Node B", 0, 0, 60, 30);
-		MxOverlay overlay = new MxOverlay("/channels/images/16x16/add2.png", "Click to create a new node", 0,0,16, 16);
-		overlay.addClickListener(new OverlayClickListener() {
-			public void onClick(MxGraph graph, MxCell cell) {
+		//MxVertex node1 = new MxVertex("Node A", 0,0,60,30);
+		
+		MxVertex node2 = graph.addVertex("Node B", 100, 0, 60, 30);
+		final MxOverlay addOverlay = new MxOverlay("/channels/images/16x16/add2.png", "Create a new node", 0,0,16, 16);
+		final MxOverlay deleteOverlay = new MxOverlay("/channels/images/16x16/delete2.png", "Delete this node", 0,16,16, 16);
+		deleteOverlay.addClickListener(new MxCellListener() {
+			public void onEvent(MxGraph graph, MxCell cell) {
+				graph.deleteCell(cell.getId());
+			}
+		});
+		addOverlay.addClickListener(new MxCellListener() {
+			public void onEvent(MxGraph graph, MxCell cell) {
 				MxVertex vertex = new MxVertex(cell.getId(), 0,0,60,30);
-				MxOverlay overlay = new MxOverlay("/channels/images/16x16/arrow_right_green.png", "Using the same listener", 0,0,16, 16);
-				overlay.addClickListener(this);
-				vertex.setOverlay(overlay);
+				vertex.addOverlay(addOverlay);
+				vertex.addOverlay(deleteOverlay);
 				graph.addVertex(vertex);
 				if (cell instanceof MxVertex) {
 					graph.addEdge("Edgy", (MxVertex)cell, vertex);
 				}
 			}
 		});
-		graph.setOverlay(node1, overlay);
+
+
 		
+		//node1.addOverlay(overlay);
+		//graph.addVertex(node1);
+		graph.addOverlay(node1, addOverlay);
+		graph.addOverlay(node1, deleteOverlay);
+		graph.addOverlay(node2, addOverlay);
+		graph.addOverlay(node2, deleteOverlay);		
 
 		graph.addEdge("Edge 1", node1, node2);
 		// Define default vertex menu
