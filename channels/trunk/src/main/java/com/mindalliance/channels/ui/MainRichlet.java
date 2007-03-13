d040bf541db69e10e8d3176560a55f27f5f65431
davidf <davidf@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2007-03-13 05:19:15 +0000

    Added grouping, shape styles
    git-svn-id: https://wush.net/svn/mindalliance@74 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- channels/trunk/src/main/java/com/mindalliance/channels/ui/MainRichlet.java
+++ channels/trunk/src/main/java/com/mindalliance/channels/ui/MainRichlet.java
@@ -14,6 +14,7 @@ import com.mindalliance.zk.mxgraph.MxCell;
 import com.mindalliance.zk.mxgraph.MxCellListener;
 import com.mindalliance.zk.mxgraph.MxCompactTreeLayout;
 import com.mindalliance.zk.mxgraph.MxConstants;
+import com.mindalliance.zk.mxgraph.MxEdge;
 import com.mindalliance.zk.mxgraph.MxGraph;
 import com.mindalliance.zk.mxgraph.MxOverlay;
 import com.mindalliance.zk.mxgraph.MxPanningHandler;
@@ -37,12 +38,18 @@ public class MainRichlet extends GenericRichlet {
 		// graph.setProperty(MxGraph.BACKGROUND_IMAGE, "/channels/images/grid.gif", false);
 		// graph.setProperty(MxGraph.AUTO_SIZE, true, false);
 		graph.getPanningHandler().setProperty(MxPanningHandler.IS_SELECT_ON_POPUP, false, false);
-		MxVertex node1 = graph.addVertex("Node A", 0, 0, 60, 30);
-		//MxVertex node1 = new MxVertex("Node A", 0,0,60,30);
-		
+		//MxVertex node1 = graph.addVertex("Node A", 0, 0, 60, 30);
+		MxVertex node1 = new MxVertex("Node A", 0,0,60,30);
+		node1.setStyle("actor");
+		graph.addVertex(node1);
 		MxVertex node2 = graph.addVertex("Node B", 100, 0, 60, 30);
+
+		MxEdge edge1 = graph.addEdge("Edge 1", node1, node2);
+		
 		final MxOverlay addOverlay = new MxOverlay("/channels/images/16x16/add2.png", "Create a new node", 0,0,16, 16);
 		final MxOverlay deleteOverlay = new MxOverlay("/channels/images/16x16/delete2.png", "Delete this node", 0,16,16, 16);
+		final MxOverlay removeOverlay = new MxOverlay("/channels/images/16x16/arrow_right_green.png", "Remove Overlays", 16,0,16,16);
+		final MxOverlay groupOverlay = new MxOverlay("/channels/images/16x16/arrow_right_green.png", "Group nodes", 16,16,16,16);
 		deleteOverlay.addClickListener(new MxCellListener() {
 			public void onEvent(MxGraph graph, MxCell cell) {
 				graph.deleteCell(cell.getId());
@@ -53,23 +60,47 @@ public class MainRichlet extends GenericRichlet {
 				MxVertex vertex = new MxVertex(cell.getId(), 0,0,60,30);
 				vertex.addOverlay(addOverlay);
 				vertex.addOverlay(deleteOverlay);
+				vertex.setParent(cell.getParent());
 				graph.addVertex(vertex);
 				if (cell instanceof MxVertex) {
-					graph.addEdge("Edgy", (MxVertex)cell, vertex);
+					graph.addEdge("", (MxVertex)cell, vertex);
 				}
 			}
 		});
-
-
+		removeOverlay.addClickListener(new MxCellListener() {
+			public void onEvent(MxGraph graph, MxCell cell) {
+				graph.clearOverlays(cell);
+			}
+		});
+		final String node1Id = node1.getId();
+		final String node2Id = node2.getId();
+		final String edge1Id = edge1.getId();
+		groupOverlay.addClickListener(new MxCellListener() {
+			public void onEvent(MxGraph graph, MxCell cell) {
+				final MxVertex group = new MxVertex("");
+				group.setStyle("swimlane");
+				graph.groupCells(group, new String[] {node1Id, node2Id, edge1Id});
+				MxOverlay ungroupOverlay = new MxOverlay("/channels/images/16x16/delete2.png", "Ungroup nodes", 0,0,16,16);
+				ungroupOverlay.addClickListener(new MxCellListener() {
+					public void onEvent(MxGraph graph, MxCell cell) {
+						graph.ungroupCells(group);
+						
+					}
+				});
+				graph.addOverlay(group, ungroupOverlay);
+			}
+		});
+		
 		
 		//node1.addOverlay(overlay);
 		//graph.addVertex(node1);
 		graph.addOverlay(node1, addOverlay);
 		graph.addOverlay(node1, deleteOverlay);
+		graph.addOverlay(node1, groupOverlay);
 		graph.addOverlay(node2, addOverlay);
-		graph.addOverlay(node2, deleteOverlay);		
+		graph.addOverlay(node2, deleteOverlay);
+		graph.addOverlay(node2, removeOverlay);
 
-		graph.addEdge("Edge 1", node1, node2);
 		// Define default vertex menu
 		Menu menu = Menu.named(Menu.CELL)
 						.item("Delete", "/channels/images/16x16/delete2.png", MxConstants.COMMAND_DELETE)
@@ -80,7 +111,7 @@ public class MainRichlet extends GenericRichlet {
 		// Define default vertex style
 		MxStyleSheet sheet = graph.getStyleSheet();
 		Map<String,String> style = sheet.getDefaultVertexStyle();
-		style.put(MxConstants.STYLE_SHAPE, MxConstants.SHAPE_ELLIPSE);
+		style.put(MxConstants.STYLE_SHAPE, MxConstants.SHAPE_RECTANGLE);
 		style.put(MxConstants.STYLE_PERIMETER, MxConstants.STYLE_VERTEX_ELLIPSE_PERIMETER);
 		style.put(MxConstants.STYLE_GRADIENTCOLOR, "white");
 		style.put(MxConstants.STYLE_FONTSIZE, "10");
