4f7aebbc860dba28419988280055c279ebf6c5d6
denis <denis@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2007-03-16 12:58:26 +0000

    Initial version
    git-svn-id: https://wush.net/svn/mindalliance@95 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ continuum-trigger/trunk/src/main/java/com/mindalliance/ContinuumTrigger.java
@@ -0,0 +1,136 @@
+// Copyright (C) 2007 Mind-Alliance Systems LLC.
+// All rights reserved.
+
+package com.mindalliance;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.MessageFormat;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.maven.continuum.model.project.Project;
+import org.apache.maven.continuum.rpc.ProjectsReader;
+import org.apache.xmlrpc.XmlRpcException;
+
+/**
+ * Command-line trigger for continuum build.
+ *
+ * @author <a href="mailto:denis@mind-alliance.com">denis</a>
+ * @version $Revision:$
+ */
+public class ContinuumTrigger implements Runnable {
+
+    private static final int ERROR_CODE = 1;
+    private static final int SUCCESS_CODE = 0;
+
+    private static Log logger = LogFactory.getLog( ContinuumTrigger.class );
+
+    private URL address;
+    private String projectName;
+
+    /**
+     * Default constructor.
+     */
+    public ContinuumTrigger() {
+    }
+
+    /**
+     * Invoke a remote build.
+     * @param args continuum URL, project
+     */
+    public static void main( String[] args ) {
+        ContinuumTrigger trigger = new ContinuumTrigger();
+
+        if ( args.length == 1 )
+            trigger.setProjectName( args[0] );
+        else {
+            try {
+                trigger.setAddress( new URL( args[0] ) );
+            } catch ( MalformedURLException e ) {
+                logger.fatal( e );
+                System.exit( ERROR_CODE );
+            }
+            trigger.setProjectName( args[1] );
+        }
+
+        trigger.run();
+    }
+
+    /**
+     * Return the value of address.
+     */
+    public URL getAddress() {
+        return this.address;
+    }
+
+    /**
+     * Set the value of address.
+     * @param address The new value of address
+     */
+    public void setAddress( URL address ) {
+        this.address = address;
+    }
+
+    /**
+     * Return the value of project.
+     */
+    public String getProjectName() {
+        return this.projectName;
+    }
+
+    /**
+     * Set the value of project.
+     * @param project The new value of project
+     */
+    public void setProjectName( String project ) {
+        this.projectName = project;
+    }
+
+    /**
+     * Trigger the build.
+     */
+    public void run() {
+        ProjectsReader reader = new ProjectsReader( getAddress() );
+        try {
+            String name = getProjectName();
+            Project p = getProject( reader, name );
+            if ( p == null ) {
+                logger.warn( MessageFormat.format(
+                        "Project {0} not defined in continuum", name ) );
+                System.exit( SUCCESS_CODE );
+            }
+
+            reader.buildProject( p );
+            logger.info( MessageFormat.format(
+                    "Project {0} scheduled for rebuild", name ) );
+            System.exit( SUCCESS_CODE );
+
+        } catch ( XmlRpcException e ) {
+            logger.fatal( e );
+            System.exit( ERROR_CODE );
+        } catch ( IOException e ) {
+            logger.fatal( e );
+            System.exit( ERROR_CODE );
+        }
+    }
+
+    /**
+     * Return a project object for the given name.
+     * @param reader the projects reader
+     * @param name the name of the project
+     * @throws IOException on IO errors
+     * @throws XmlRpcException on server errors
+     * @return null if no project by that name was found
+     */
+    private Project getProject( ProjectsReader reader, String name )
+        throws XmlRpcException, IOException {
+
+        for ( Project p : reader.readProjects() )
+            if ( p.getArtifactId().equals( name ) )
+                return p;
+
+        return null;
+    }
+}
