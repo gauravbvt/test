ef5def7b9ede111329e29e0e44c141d135099947
denis <denis@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2007-05-08 20:06:57 +0000

    Added milliseconds value
    git-svn-id: https://wush.net/svn/mindalliance@195 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- channels/trunk/src/main/java/com/mindalliance/channels/util/Duration.java
+++ channels/trunk/src/main/java/com/mindalliance/channels/util/Duration.java
@@ -13,12 +13,23 @@ import java.io.Serializable;
  *
  * @opt attributes
  */
-public class Duration implements Serializable {
+public class Duration implements Serializable, Comparable<Duration> {
 
     /**
      * A unit of time.
      */
-    public enum Unit { years, months, weeks, days, hours, minutes, seconds }
+    public enum Unit {
+        /** 365.25 days. */
+        years,
+
+        /** 30 days. */
+        months, weeks, days, hours, minutes, seconds, milliseconds
+    }
+
+    private static final long[] VALUES = new long[] {
+        31557600000L, 2592000000L, 604800000,
+        86400000, 3600000, 60000, 1000, 1
+    };
 
     private int number;
     private Unit unit;
@@ -27,6 +38,17 @@ public class Duration implements Serializable {
      * Default constructor.
      */
     public Duration() {
+    }
+
+    /**
+     * Default constructor.
+     * @param number the number
+     * @param unit the unit
+     */
+    public Duration( int number, Unit unit ) {
+        this();
+        this.number = number;
+        this.unit = unit;
     }
 
     /**
@@ -57,5 +79,45 @@ public class Duration implements Serializable {
      */
     public void setUnit( Unit unit ) {
         this.unit = unit;
+    }
+
+    /**
+     * Get the numbers of milliseconds implied by this duration.
+     */
+    public long getMilliseconds() {
+        return VALUES[ getUnit().ordinal() ] * getNumber();
+    }
+
+    /**
+     * Compare two durations.
+     * @param o another duration
+     */
+    public int compareTo( Duration o ) {
+        long mine = getMilliseconds();
+        long others = o.getMilliseconds();
+        return mine > others ? 1
+             : mine < others ? -1
+             : 0 ;
+    }
+
+    /**
+     * Test if two durations are equal (if the equivalent
+     * milliseconds matches, regardless of units).
+     * @param obj the thing to compare to
+     */
+    @Override
+    public boolean equals( Object obj ) {
+        return this == obj
+            || ( obj instanceof Duration
+                    && getMilliseconds() == ( (Duration) obj ).getMilliseconds()
+                );
+    }
+
+    /**
+     * Returns a hash code.
+     */
+    @Override
+    public int hashCode() {
+        return (int) getMilliseconds();
     }
 }
