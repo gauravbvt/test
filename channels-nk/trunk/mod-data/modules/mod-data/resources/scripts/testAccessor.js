7a6bdbd1c20f079b5d86d9518e489554e3cd4268
jf <jf@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2007-09-20 14:37:29 +0000

    mod-data: added testAccessor.js and test to expose possible NK bug with lock/unlock (NK hangs)
    git-svn-id: https://wush.net/svn/mindalliance@484 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ channels-nk/trunk/mod-data/modules/mod-data/resources/scripts/testAccessor.js
@@ -0,0 +1,57 @@
+importPackage(Packages.org.ten60.netkernel.xml.representation);
+importPackage(Packages.com.ten60.netkernel.urii.aspect);
+importPackage(Packages.java.lang);
+
+LOG_URL = "ffcpl:/etc/LogConfig.xml";
+
+function log(content, level) {
+  var req=context.createSubRequest("active:application-log");
+  req.addArgument("operand", new StringAspect(content) );
+  req.addArgument("configuration", LOG_URL ); // Defaults to ffcpl:/etc/LogConfig.xml
+  var levelXml = "<log>" + "<" + level + "/>" + "</log>";
+  req.addArgument("operator", new StringAspect(levelXml) );
+//  context.issueAsyncSubRequest(req);
+  context.issueSubRequest(req);
+}
+
+function sleep(msecs) {
+	log("Sleeping for " + msecs, "info");
+	var req = context.createSubRequest("active:sleep");
+	var time = <time>
+								{msecs}
+							</time>;
+	req.addArgument("operator", new XmlObjectAspect(time.getXmlObject()));
+	context.issueSubRequest(req);
+}
+
+function grabLock(lock, who) {
+	log(who + ": Grab " + lock, "info");
+	var req=context.createSubRequest("active:lock");
+	req.addArgument("operand","lock:" + lock);
+	context.issueSubRequest(req);	
+	log(who + ": Grabbed " + lock, "info");
+}
+
+function releaseLock(lock, who) {
+	log(who + ": Release " + lock, "info");
+	var req=context.createSubRequest("active:unlock");
+	req.addArgument("operand","lock:" + lock);
+	context.issueSubRequest(req);
+	log(who + ": Released " + lock, "info");
+}
+
+function issueSleepRequest(who) {
+	log(who + ": Sleeping ", "info");
+	var msecs = Math.round(Math.random() * 1000);
+	sleep(msecs);
+	log(who + ": Waking ", "info");
+}
+
+var op = context.getThisRequest().getArgumentValue( "var:operand" );
+var who = context.transrept(op, IAspectString).getString();
+grabLock("lock:protected", who);
+issueSleepRequest(who);
+releaseLock("lock:protected", who);
+
+var resp = context.createResponseFrom(new BooleanAspect(true));
+context.setResponse(resp);
---
+++ channels-nk/trunk/mod-data/modules/test-mod-data/test/support/testLockUnlock.js
@@ -0,0 +1,39 @@
+utilsURI = "ffcpl:/test/utils.js";
+context.importLibrary(utilsURI);
+
+importPackage(Packages.org.ten60.netkernel.xml.representation);
+importPackage(Packages.com.ten60.netkernel.urii.aspect);
+importPackage(Packages.org.apache.xmlbeans);
+importPackage(Packages.java.lang);
+
+var RUNS = 20;
+
+var operands = [];
+var requestHandles = [];
+
+for (i=1; i< RUNS; i++) {
+	operands[i] = ""+i;
+}
+
+for (i in operands) {
+	var req = context.createSubRequest("active:channels_data_testAccessor");
+	req.addArgument("operand", new StringAspect(operands[i]));
+	requestHandles[i] = context.issueAsyncSubRequest(req);
+}
+
+var success = true;
+
+//Wait for all requests to complete
+try {
+	for each (handle in requestHandles) {
+		handle.join(); 
+	}
+}
+catch (e)	{
+	log("All requests did not complete: " + e, "severe");
+	success = false;
+}
+
+// Respond
+var resp = context.createResponseFrom(new BooleanAspect(success));
+context.setResponse(resp);
