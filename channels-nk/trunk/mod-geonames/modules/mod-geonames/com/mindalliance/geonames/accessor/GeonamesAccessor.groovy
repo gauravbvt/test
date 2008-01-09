ed76e04260b6f6a7d2de966ead97bc7fc0df0d39
dcfeeney <dcfeeney@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2008-01-09 22:08:16 +0000

    Adding channels model for static content
    git-svn-id: https://wush.net/svn/mindalliance@571 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ channels-nk/trunk/mod-geonames/modules/mod-geonames/com/mindalliance/geonames/accessor/GeonamesAccessor.groovy
@@ -0,0 +1,16 @@
+package com.mindalliance.geonames.accessor
+
+import com.mindalliance.channels.nk.accessor.AbstractAccessor
+import org.ten60.netkernel.layer1.nkf.INKFConvenienceHelper as Context
+
+/**
+*
+*/
+class GeonamesAccessor extends AbstractAccessor {
+
+    protected void source(Context context) {
+        
+
+    }
+
+}
