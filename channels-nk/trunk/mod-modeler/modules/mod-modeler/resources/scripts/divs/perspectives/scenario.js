fae13c9c514137239d718b220fd536134bb559de
denis <denis@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2008-03-03 18:09:05 +0000

    Scenario views.
    git-svn-id: https://wush.net/svn/mindalliance@645 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- channels-nk/trunk/mod-modeler/modules/mod-modeler/resources/scripts/divs/perspectives/scenario.js
+++ channels-nk/trunk/mod-modeler/modules/mod-modeler/resources/scripts/divs/perspectives/scenario.js
@@ -7,4 +7,12 @@ $(document).ready( function() {
 
     var finder = $.channels.modeler.finder('scenario');
     finder.grid.setup();
+
+    $( "#scenario .perspective_main_right ul.toggle li" ).click( function(){
+        $( "#scenario .perspective_main_right ul.toggle li" ).removeClass( "selected" );
+        $( this ).addClass( "selected" );
+        var id = $( this ).attr( "id" ).substr( 3 );
+        $( "#scenario div.scenario_view" ).css( "display", "none" );
+        $( "#sv" + id ).css( "display", "" );
+    });
 });
