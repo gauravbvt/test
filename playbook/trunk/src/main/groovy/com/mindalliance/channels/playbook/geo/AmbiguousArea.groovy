42523902495a65b8def735c02b0fb206cf19fcfe
jf <jf@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2008-03-24 11:29:06 +0000

    Added support for ambiguous or unknown location areas
    git-svn-id: https://wush.net/svn/mindalliance@687 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- playbook/trunk/src/main/groovy/com/mindalliance/channels/playbook/geo/AmbiguousArea.groovy
+++ playbook/trunk/src/main/groovy/com/mindalliance/channels/playbook/geo/AmbiguousArea.groovy
@@ -1,6 +1,7 @@
 package com.mindalliance.channels.playbook.geo
 
 import org.geonames.Toponym
+import com.mindalliance.channels.playbook.ifm.Location
 
 /**
 * Copyright (C) 2008 Mind-Alliance Systems. All Rights Reserved.
@@ -13,10 +14,52 @@ class AmbiguousArea extends Area {
 
     List<Toponym> topos
 
-    AmbiguousArea(List<Toponym> topos)  {
+    AmbiguousArea(List<Toponym> topos) {
         this.topos = topos
     }
 
-    // TODO throw exceptions on all methods
+    boolean isAmbiguous() {
+        return true
+    }
+
+    List<Area> findNearbyAreas() {
+        throw new Exception("Area is ambiguous")
+    }
+
+    Area findContainingArea() {
+        throw new Exception("Area is ambiguous")
+    }
+
+    boolean isWithinLocation(Location location) {
+        throw new Exception("Area is ambiguous")
+    }
+
+    List<Area> findHierarchy() {
+        throw new Exception("Area is ambiguous")
+    }
+
+    boolean isGlobe() {
+        return false
+    }
+
+    boolean isContinent() {
+        return false
+    }
+
+    boolean isCountry() {
+        return false
+    }
+
+    boolean isStateLike() {
+        return false
+    }
+
+    boolean isCountyLike() {
+        return false
+    }
+
+    boolean isCityLike() {
+        return false
+    }
 
 }
--- playbook/trunk/src/main/groovy/com/mindalliance/channels/playbook/geo/UnknownArea.groovy
+++ playbook/trunk/src/main/groovy/com/mindalliance/channels/playbook/geo/UnknownArea.groovy
@@ -1,16 +1,57 @@
 package com.mindalliance.channels.playbook.geo
+
+import com.mindalliance.channels.playbook.ifm.Location
+
 /**
- * Copyright (C) 2008 Mind-Alliance Systems. All Rights Reserved.
- * Proprietary and Confidential.
- * User: jf
- * Date: Mar 23, 2008
- * Time: 12:43:14 PM
- */
+* Copyright (C) 2008 Mind-Alliance Systems. All Rights Reserved.
+* Proprietary and Confidential.
+* User: jf
+* Date: Mar 23, 2008
+* Time: 12:43:14 PM
+*/
 class UnknownArea extends Area{
 
     boolean isUnknow() {
         return true
     }
 
-    // TODO throw exceptions on all methods
+        List<Area> findNearbyAreas() {
+        throw new Exception("Area is unknown")
+    }
+
+    Area findContainingArea() {
+        throw new Exception("Area is unknown")
+    }
+
+    boolean isWithinLocation(Location location) {
+        throw new Exception("Area is unknown")
+    }
+
+    List<Area> findHierarchy() {
+        throw new Exception("Area is unknown")
+    }
+
+    boolean isGlobe() {
+        return false
+    }
+
+    boolean isContinent() {
+        return false
+    }
+
+    boolean isCountry() {
+        return false
+    }
+
+    boolean isStateLike() {
+        return false
+    }
+
+    boolean isCountyLike() {
+        return false
+    }
+
+    boolean isCityLike() {
+        return false
+    }
 }
