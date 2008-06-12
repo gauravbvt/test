e28d3b7ea1566ece17c9f4b44e1aab49b14ed1d5
jf <jf@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2008-06-12 05:11:57 +0000

    Reset  to PlaybookPage
    git-svn-id: https://wush.net/svn/mindalliance@887 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- playbook/trunk/src/main/groovy/com/mindalliance/channels/playbook/support/PlaybookApplication.groovy
+++ playbook/trunk/src/main/groovy/com/mindalliance/channels/playbook/support/PlaybookApplication.groovy
@@ -63,8 +63,8 @@ class PlaybookApplication extends AuthenticatedWebApplication implements Memorab
     //----------------------
     @Override
     public Class getHomePage() {
-       // return PlaybookPage.class
-       return FormTest.class
+       return PlaybookPage.class
+       // return FormTest.class
     }
 
     @Override
