48e79a6d162f8ceac196c64aa71ead24b888b0de
jf <jf@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2008-07-16 12:32:44 +0000

    PlaybookFilter was not persisiting ref to filtered playbook. Fixed.
    git-svn-id: https://wush.net/svn/mindalliance@940 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- playbook/trunk/src/main/java/com/mindalliance/channels/playbook/pages/filters/PlaybookFilter.java
+++ playbook/trunk/src/main/java/com/mindalliance/channels/playbook/pages/filters/PlaybookFilter.java
@@ -4,9 +4,13 @@ import com.mindalliance.channels.playbook.ifm.playbook.Playbook;
 import com.mindalliance.channels.playbook.ifm.playbook.PlaybookElement;
 import com.mindalliance.channels.playbook.ref.Ref;
 import com.mindalliance.channels.playbook.ref.Referenceable;
+import com.mindalliance.channels.playbook.support.persistence.Mappable;
+import com.mindalliance.channels.playbook.support.Mapper;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * ...
@@ -15,12 +19,14 @@ public class PlaybookFilter extends Filter {
 
     private Ref playbook;
 
-    public PlaybookFilter( Ref ref ) {
-        super( "in playbook " + getPlaybook( ref ) );
+    public PlaybookFilter() {} // for persistency only
+
+    public PlaybookFilter(Ref ref) {
+        super("in playbook " + getPlaybook(ref));
         this.playbook = ref;
     }
 
-    static Playbook getPlaybook( Ref playbookRef ) {
+    static Playbook getPlaybook(Ref playbookRef) {
         Playbook result = (Playbook) playbookRef.deref();
         playbookRef.detach();
         return result;
@@ -30,20 +36,31 @@ public class PlaybookFilter extends Filter {
         return Collections.emptyList();
     }
 
-    public boolean match( Ref ref ) {
+    public boolean match(Ref ref) {
         Referenceable object = ref.deref();
-        if ( object instanceof PlaybookElement ) {
+        if (object instanceof PlaybookElement) {
             PlaybookElement pe = (PlaybookElement) object;
-            return playbook.equals( pe.getPlaybook() );
+            return playbook.equals(pe.getPlaybook());
         }
         return false;
     }
 
-    protected boolean strictlyAllowsClass( Class<?> c ) {
-        return PlaybookElement.class.isAssignableFrom( c );
+    protected boolean strictlyAllowsClass(Class<?> c) {
+        return PlaybookElement.class.isAssignableFrom(c);
     }
 
     public Ref getPlaybook() {
         return playbook;
+    }
+
+    public Map<String, Object> toMap() {
+        Map<String, Object> map = super.toMap();
+        map.put("playbook", (Object) Mapper.toPersistedValue( playbook ));
+        return map;
+    }
+
+    public void initFromMap(Map map) {
+        playbook = (Ref)Mapper.valueFromPersisted( map.get( "playbook" ));
+        super.initFromMap(map);
     }
 }
