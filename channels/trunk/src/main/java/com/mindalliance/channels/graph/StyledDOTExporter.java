84dc05ccb16b641aae888ada94538888f7da9bfe
mindalliance <mindalliance@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2008-11-21 01:53:45 +0000

    graph building functional (not yet tested)
    git-svn-id: https://wush.net/svn/mindalliance@1029 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- channels/trunk/src/main/java/com/mindalliance/channels/graph/StyledDOTExporter.java
+++ channels/trunk/src/main/java/com/mindalliance/channels/graph/StyledDOTExporter.java
@@ -4,8 +4,10 @@ import org.jgrapht.ext.VertexNameProvider;
 import org.jgrapht.ext.EdgeNameProvider;
 import org.jgrapht.ext.IntegerNameProvider;
 import org.jgrapht.Graph;
+import org.jgrapht.DirectedGraph;
 
 import java.io.Writer;
+import java.io.PrintWriter;
 
 /**
  * Copyright (C) 2008 Mind-Alliance Systems. All Rights Reserved.
@@ -22,7 +24,7 @@ public class StyledDOTExporter<V, E>  {
     private VertexNameProvider<V> vertexIDProvider;
     private VertexNameProvider<V> vertexLabelProvider;
     private EdgeNameProvider<E> edgeLabelProvider;
-    DOTAttributeProvider<V, E> dotStyleProvider;
+    DOTAttributeProvider<V, E> attributeProvider;
 
     public StyledDOTExporter() {
         this(new IntegerNameProvider<V>(), null, null, null);
@@ -32,11 +34,11 @@ public class StyledDOTExporter<V, E>  {
     public StyledDOTExporter(VertexNameProvider<V> vertexIDProvider,
                              VertexNameProvider<V> vertexLabelProvider,
                              EdgeNameProvider<E> edgeLabelProvider,
-                             DOTAttributeProvider<V, E> dotStyleProvider) {
+                             DOTAttributeProvider<V, E> attributeProvider) {
         this.vertexIDProvider = vertexIDProvider;
         this.vertexLabelProvider = vertexLabelProvider;
         this.edgeLabelProvider = edgeLabelProvider;
-        this.dotStyleProvider = dotStyleProvider;
+        this.attributeProvider = attributeProvider;
     }
 
     public void setHighlightedVertex(V highlightedVertex) {
@@ -47,10 +49,61 @@ public class StyledDOTExporter<V, E>  {
         this.highlightedEdge = highlightedEdge;
     }
 
-    public void export(Writer writer, Graph<V,E> graph) {
-        // TODO
+    public void export(Writer writer, Graph<V,E> g) {
+        PrintWriter out = new PrintWriter(writer);
+        String indent = "  ";
+        String connector;
+        // Graph declaration
+        if (g instanceof DirectedGraph) {
+            connector = " -> ";
+            out.println("digraph G {");
+        } else {
+            connector = " -- ";
+            out.println("graph G {");
+        }
+        if (attributeProvider != null) {
+            attributeProvider.getGraphAttributes();
+        }
+        out.println();
+        // Vertices
+        for (V v : g.vertexSet()) {
+            out.print(indent + getVertexID(v));
+            out.print("[");
+            if (vertexLabelProvider != null) {
+                out.print(
+                    "label = \"" + vertexLabelProvider.getVertexName(v) + "\"");
+            }
+            if (attributeProvider != null) {
+                out.print(",");
+                out.print(attributeProvider.getVertexAttributes(v, highlightedVertex == v));
+            }
+            out.println("];");
+        }
+        // Edges
+        for (E e : g.edgeSet()) {
+            String source = getVertexID(g.getEdgeSource(e));
+            String target = getVertexID(g.getEdgeTarget(e));
+
+            out.print(indent + source + connector + target);
+            out.print("[");
+            if (edgeLabelProvider != null) {
+                out.print(
+                    "label = \"" + edgeLabelProvider.getEdgeName(e) + "\"");
+            }
+            if (attributeProvider != null) {
+                out.print(",");
+                out.print(attributeProvider.getEdgeAttributes(e, highlightedEdge == e));
+            }
+            out.println("];");
+        }
+        // Close graph
+        out.println("}");
+        out.flush();
     }
 
-
+    private String getVertexID(V v) {
+       return vertexIDProvider.getVertexName(v);  // assume vertex name is DOT-compliant
+    }
+    
 
 }
