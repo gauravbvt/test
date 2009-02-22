f845794f4e3fdbd94e23150305e0e245a788eb44
mindalliance <mindalliance@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2009-02-22 19:39:57 +0000

    Fixed FlowWithoutChannel issue detector: An issue is detected when a flow requires a channel (notification of a non-connector, or reply from a non-connector) and none is defined explicitly or implictly (a role-only part is contacted and no actor for it is defined or not all such actors have contact info).
    git-svn-id: https://wush.net/svn/mindalliance@1235 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- channels/trunk/src/main/java/com/mindalliance/channels/analysis/detectors/FlowWithoutChannel.java
+++ channels/trunk/src/main/java/com/mindalliance/channels/analysis/detectors/FlowWithoutChannel.java
@@ -5,6 +5,11 @@ import com.mindalliance.channels.analysis.DetectedIssue;
 import com.mindalliance.channels.ModelObject;
 import com.mindalliance.channels.Flow;
 import com.mindalliance.channels.Issue;
+import com.mindalliance.channels.Part;
+import com.mindalliance.channels.Actor;
+import com.mindalliance.channels.ResourceSpec;
+import com.mindalliance.channels.Channel;
+import com.mindalliance.channels.pages.Project;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -25,20 +30,64 @@ public class FlowWithoutChannel extends AbstractIssueDetector {
     /**
      * {@inheritDoc}
      */
-    // TODO -- not an issue when no channel in flow, not to actor and all actors matching resource spec have channels
+    // An issue when no channel in flow if contacting a role
+    // and there is actors matching resource
+    // or not all such actors spec have contact info (resourceSpec-assigned channels
     public List<Issue> doDetectIssues( ModelObject modelObject ) {
         List<Issue> issues = new ArrayList<Issue>();
         Flow flow = (Flow) modelObject;
-        if ( flow.getEffectiveChannels().isEmpty() ) {
-            if ( !( flow.getTarget().isConnector() && !flow.isAskedFor() ) ) {
+        if ( needsAtLeastOneChannel( flow ) && flow.getEffectiveChannels().isEmpty() ) {
+            Part part = partNeedingChannel( flow );
+            if ( part.isOnlyRole() ) {
+                ResourceSpec partSpec = part.resourceSpec();
+                List<Actor> actorsInRole = Project.service().
+                        findAllActors( partSpec  );
+                if ( actorsInRole.isEmpty() ) {
+                    DetectedIssue issue = new DetectedIssue( DetectedIssue.DEFINITION, modelObject );
+                    issue.setDescription(
+                            "Flow involves "
+                                    + partSpec
+                                    + " and there is no known actor in this role." );
+                    issue.setRemediation( "Provide at least one (broadcast) channel, "
+                            + "or define actors in this role with contact info." );
+                    issue.setSeverity( Issue.Level.Severe );
+                    issues.add( issue );
+                } else {
+                    for ( Actor actor : actorsInRole ) {
+                        List<Channel> actorChannels = ResourceSpec.with( actor ).allChannels();
+                        if ( actorChannels.isEmpty() ) {
+                            DetectedIssue issue = new DetectedIssue( DetectedIssue.DEFINITION, modelObject );
+                            issue.setDescription( "Flow involves "
+                                    + partSpec
+                                    + " and "
+                                    + actor.getName()
+                                    + " in this role has no contact information." );
+                            issue.setRemediation( "Provide contact information for "
+                                    + actor.getName()
+                                    + ", or provide at least one (broadcast) channel for the flow." );
+                            issue.setSeverity( Issue.Level.Major );
+                            issues.add( issue );
+                        }
+                    }
+                }
+            } else {
                 DetectedIssue issue = new DetectedIssue( DetectedIssue.DEFINITION, modelObject );
-                issue.setDescription( "Flow without channels." );
+                issue.setDescription( "Flow requires a channel." );
                 issue.setRemediation( "Provide at least one channel." );
                 issue.setSeverity( Issue.Level.Severe );
                 issues.add( issue );
             }
         }
         return issues;
+    }
+
+    private boolean needsAtLeastOneChannel( Flow flow ) {
+        return ( !flow.getTarget().isConnector() && flow.isNotification() )
+                || ( !flow.getSource().isConnector() && flow.isAskedFor() );
+    }
+
+    private Part partNeedingChannel( Flow flow ) {
+        return flow.isNotification() ? (Part) flow.getTarget() : (Part) flow.getSource();
     }
 
     /**
