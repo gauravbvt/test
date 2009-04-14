baccde67ea6696b3518f46c5402392d63961f181
denis <denis@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2009-04-14 15:55:02 +0000

    Added plan map to report.
    git-svn-id: https://wush.net/svn/mindalliance@1372 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- channels/trunk/src/main/java/com/mindalliance/channels/pages/reports/ProjectReportPage.java
+++ channels/trunk/src/main/java/com/mindalliance/channels/pages/reports/ProjectReportPage.java
@@ -2,21 +2,25 @@ package com.mindalliance.channels.pages.reports;
 
 import com.mindalliance.channels.Scenario;
 import com.mindalliance.channels.pages.Project;
+import com.mindalliance.channels.pages.components.diagrams.PlanMapDiagramPanel;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.Model;
 
 import java.text.Collator;
 import java.text.DateFormat;
+import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
 /**
- * The project report
+ * The project report.
  * Copyright (C) 2008 Mind-Alliance Systems. All Rights Reserved.
  * Proprietary and Confidential.
  * User: jf
@@ -27,31 +31,29 @@ public class ProjectReportPage extends WebPage {
 
     public ProjectReportPage( PageParameters parameters ) {
         super( parameters );
-        init();
-    }
 
-    private void init() {
         Project project = Project.getProject();
-
-        add( new Label( "title", "Report: " + project.getProjectName() ) );
-        add( new Label( "project-name", project.getProjectName() ) );
-        add( new Label( "project-client", project.getClient() ) );
-        add( new Label( "project-description", project.getDescription() ) );
-        add( new Label( "date", DateFormat.getDateTimeInstance(
+        add( new Label( "title",                                                          // NON-NLS
+                        MessageFormat.format( "Report: {0}", project.getProjectName() ) ) );
+        add( new Label( "project-name", project.getProjectName() ) );                     // NON-NLS
+        add( new Label( "project-client", project.getClient() ) );                        // NON-NLS
+        add( new Label( "project-description", project.getDescription() ) );              // NON-NLS
+        add( new Label( "date", DateFormat.getDateTimeInstance(                           // NON-NLS
             DateFormat.LONG, DateFormat.LONG ).format( new Date() ) ) );
-
         List<Scenario> scenarios = Project.dqo().list( Scenario.class );
         Collections.sort( scenarios, new Comparator<Scenario>() {
             public int compare( Scenario o1, Scenario o2 ) {
                 return Collator.getInstance().compare( o1.getName(), o2.getName() );
             }
         } );
-        add( new ListView<Scenario>( "scenarios", scenarios ) {
+        add( new ListView<Scenario>( "scenarios", scenarios ) {                           // NON-NLS
             @Override
             protected void populateItem( ListItem<Scenario> item ) {
-                item.add( new ScenarioReportPanel( "scenario", item.getModel() ) );
+                item.add( new ScenarioReportPanel( "scenario", item.getModel() ) );       // NON-NLS
             }
         } );
+        add( new PlanMapDiagramPanel( "planMap",                                          // NON-NLS
+            new Model<ArrayList<Scenario>>( (ArrayList) scenarios ),
+            null, null ) );
     }
-
 }
