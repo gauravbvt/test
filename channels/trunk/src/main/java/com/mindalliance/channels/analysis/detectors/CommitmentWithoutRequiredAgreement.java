3feb8281898a6a566d0cc70e138b9c9faa9140d0
mindalliance <mindalliance@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2009-07-18 11:21:27 +0000

    New issue detected: Sharing commitment across organizations without required MOU.
    git-svn-id: https://wush.net/svn/mindalliance@1522 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- channels/trunk/src/main/java/com/mindalliance/channels/analysis/detectors/CommitmentWithoutRequiredAgreement.java
+++ channels/trunk/src/main/java/com/mindalliance/channels/analysis/detectors/CommitmentWithoutRequiredAgreement.java
@@ -28,45 +28,68 @@ public class CommitmentWithoutRequiredAgreement extends AbstractIssueDetector {
     public CommitmentWithoutRequiredAgreement() {
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public List<Issue> detectIssues( ModelObject modelObject ) {
         List<Issue> issues = new ArrayList<Issue>();
         Flow flow = (Flow) modelObject;
         Part source = (Part) flow.getSource();
         Organization org = source.getOrganization();
         if ( org != null && org.isAgreementsRequired() ) {
-            boolean hasMOU = CollectionUtils.exists(
-                    flow.getAttachments(),
-                    new Predicate() {
-                        public boolean evaluate( Object obj ) {
-                            return ( (Attachment) obj ).getType() == Attachment.Type.MOU;
+            Organization otherOrg = ( (Part) flow.getTarget() ).getOrganization();
+            if ( otherOrg != null && !org.equals( otherOrg ) ) {
+                boolean hasMOU = CollectionUtils.exists(
+                        flow.getAttachments(),
+                        new Predicate() {
+                            public boolean evaluate( Object obj ) {
+                                return ( (Attachment) obj ).getType() == Attachment.Type.MOU;
+                            }
                         }
-                    }
-            );
-            if ( !hasMOU ) {
-                DetectedIssue issue = makeIssue( Issue.COMPLETENESS, flow );
-                issue.setDescription( "Sharing commitments from "
-                        + org.getName()
-                        + " must be subject to an MOU." );
-                issue.setRemediation( "Attach an MOU to this sharing commitment, "
-                        + "or remove the requirement for MOUs for "
-                        + org.getName() );
-                issue.setSeverity( Issue.Level.Major );
-                issues.add( issue );
+                );
+                if ( !hasMOU ) {
+                    DetectedIssue issue = makeIssue( Issue.COMPLETENESS, flow );
+                    issue.setDescription( "Sharing commitments from "
+                            + org.getName()
+                            + " to another organization"
+                            + " must be subject to an MOU." );
+                    issue.setRemediation( "Attach an MOU to this sharing commitment, "
+                            + "or remove the requirement for MOUs for "
+                            + org.getName() );
+                    issue.setSeverity( Issue.Level.Major );
+                    issues.add( issue );
+                }
             }
         }
         return issues;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public boolean appliesTo( ModelObject modelObject ) {
         return modelObject instanceof Flow
                 && ( (Flow) modelObject ).isSharingCommitment();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public String getTestedProperty() {
         return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected String getLabel() {
         return "Sharing commitment without required MOU";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean canBeWaived() {
+        return true;
     }
 }
