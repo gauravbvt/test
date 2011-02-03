dbdecb6ab40def7deb333f325229c272e80cfd56
jf <jf@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2011-02-03 18:20:12 +0000

    integrated with trunk
    git-svn-id: https://wush.net/svn/mindalliance@2146 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- channels/branches/sandbox/src/main/java/com/mindalliance/channels/pages/components/menus/LinkMenuItem.java
+++ channels/branches/sandbox/src/main/java/com/mindalliance/channels/pages/components/menus/LinkMenuItem.java
@@ -1,21 +1,49 @@
 package com.mindalliance.channels.pages.components.menus;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.AbstractLink;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.MarkupContainer;
-import org.apache.wicket.Component;
+import org.apache.wicket.model.PropertyModel;
 
 /**
  * A link menu item.
  */
 public class LinkMenuItem extends Panel {
 
+    /**
+     * Space with width of one en (half of one em).
+     */
+    private static char ENSP = (char)8194;
+
+    IModel<String> model;
+    private String paddedName;
+    private Label label;
+    private AbstractLink link;
+
     public LinkMenuItem( String s, IModel<String> model, AbstractLink link ) {
         super( s, model );
+        this.model = model;
+        this.link = link;
         link.setMarkupId( "link" );
-        add( link.add( new Label( "string", model.getObject() ) ) );
+        add( link );
     }
 
+    protected void onBeforeRender() {
+        super.onBeforeRender();
+        if ( label == null ) {
+            // Just in time setting of label (after name padding).
+            label = new Label( "string", new PropertyModel<String>( this, "name" ) );
+            link.add( label );
+        }
+    }
+
+    public String getName() {
+        return paddedName == null ? model.getObject() : paddedName;
+    }
+
+    public void padName( int length ) {
+        paddedName = StringUtils.rightPad( model.getObject(), length, ENSP  );
+    }
 }
--- channels/branches/sandbox/src/main/java/com/mindalliance/channels/pages/components/menus/MenuPanel.java
+++ channels/branches/sandbox/src/main/java/com/mindalliance/channels/pages/components/menus/MenuPanel.java
@@ -18,6 +18,7 @@ import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
@@ -25,14 +26,28 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-/** An abstract base class for menu panel. */
+/**
+ * An abstract base class for menu panel.
+ */
 public abstract class MenuPanel extends AbstractCommandablePanel {
 
-    /** Confirmation requested. */
+    /**
+     * Class logger.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger( MenuPanel.class );
+    /**
+     * Confirmation requested.
+     */
     protected static final boolean CONFIRM = true;
 
-    /** Title. */
+    /**
+     * Title.
+     */
     private String title;
+    /**
+     * Cached menu items.
+     */
+    private List<Component> menuItems;
 
     protected MenuPanel( String s, String title, IModel<? extends Identifiable> model ) {
         this( s, title, model, null );
@@ -45,13 +60,16 @@ public abstract class MenuPanel extends AbstractCommandablePanel {
         init();
     }
 
-    /** Initialize menu components. */
+    /**
+     * Initialize menu components.
+     */
     protected void init() {
+        menuItems = null;
         setOutputMarkupId( true );
         add(
-            new Label( "menu-title", new Model<String>( title ) ),
-            new ListView<Component>(
-                "items", new PropertyModel<List<Component>>( this, "menuItems" ) ) {
+                new Label( "menu-title", new Model<String>( title ) ),
+                new ListView<Component>(
+                        "items", allMenuItems() ) {
                     @Override
                     protected void populateItem( ListItem<Component> item ) {
                         item.add( item.getModelObject() );
@@ -59,11 +77,55 @@ public abstract class MenuPanel extends AbstractCommandablePanel {
                 } );
     }
 
+    @Override
+    protected void onBeforeRender() {
+        super.onBeforeRender();
+        adjustMenuItems();
+    }
+
+    private void adjustMenuItems() {
+        try {
+            int maxItemLength = maxItemLength();
+            for ( Component menuItem : allMenuItems() ) {
+                if ( menuItem instanceof LinkMenuItem ) {
+                    ( (LinkMenuItem) menuItem ).padName( maxItemLength );
+                }
+            }
+        } catch ( CommandException e ) {
+            LOG.warn( "Fail to adjust menu items", e );
+        }
+
+    }
+
+
+    private int maxItemLength() throws CommandException {
+        int maxLength = 0;
+        for ( Component menuItem : allMenuItems() ) {
+            if ( menuItem instanceof LinkMenuItem ) {
+                LinkMenuItem linkMenuItem = (LinkMenuItem) menuItem;
+                String name = linkMenuItem.getName();
+                maxLength = Math.max( maxLength, name.length() );
+            }
+        }
+        return maxLength;
+    }
+
+    protected List<Component> allMenuItems() {
+        if ( menuItems == null ) {
+            try {
+                menuItems = getMenuItems();
+            } catch ( CommandException e ) {
+                LoggerFactory.getLogger( getClass() ).warn( "Failed to get menu items", e );
+                return new ArrayList<Component>(  );
+            }
+        }
+        return menuItems;
+    }
+
     /**
      * Get menu items.
      *
      * @return a list of components
-     *
      * @throws CommandException if fails to get menu items
      */
     public abstract List<Component> getMenuItems() throws CommandException;
@@ -74,15 +136,9 @@ public abstract class MenuPanel extends AbstractCommandablePanel {
      * @return a boolean
      */
     public boolean isEmpty() {
-        List<Component> menuItems = null;
-        try {
-            menuItems = getMenuItems();
-        } catch ( CommandException e ) {
-            LoggerFactory.getLogger( getClass() ).debug( "While testing for emptyness", e );
-            // do nothing
-        }
-
-        return menuItems != null && menuItems.isEmpty();
+        List<Component> items = null;
+        items = allMenuItems();
+        return items != null && items.isEmpty();
     }
 
     /**
@@ -97,17 +153,17 @@ public abstract class MenuPanel extends AbstractCommandablePanel {
         List<Component> menuItems = new ArrayList<Component>();
         for ( final ModelObjectWrapper moWrapper : moWrappers )
             menuItems.add(
-                new LinkMenuItem(
-                    id,
-                    new PropertyModel<String>( moWrapper, "title" ),
-                    new AjaxFallbackLink( "link" ) {
-                        @Override
-                        public void onClick( AjaxRequestTarget target ) {
-                            update(
-                                target,
-                                new Change( Change.Type.Expanded, moWrapper.getModelObject() ) );
-                        }
-                    } ) );
+                    new LinkMenuItem(
+                            id,
+                            new PropertyModel<String>( moWrapper, "title" ),
+                            new AjaxFallbackLink( "link" ) {
+                                @Override
+                                public void onClick( AjaxRequestTarget target ) {
+                                    update(
+                                            target,
+                                            new Change( Change.Type.Expanded, moWrapper.getModelObject() ) );
+                                }
+                            } ) );
 
         return menuItems;
     }
@@ -120,17 +176,17 @@ public abstract class MenuPanel extends AbstractCommandablePanel {
      */
     protected Component getUndoMenuItem( String id ) {
         return getCommander().canUndo() ?
-              new LinkMenuItem(
-                id,
-                new Model<String>( getCommander().getUndoTitle() ),
-                new AjaxFallbackLink( "link" ) {
-                    @Override
-                    public void onClick( AjaxRequestTarget target ) {
-                        update( target, getCommander().undo() );
-                    }
-                } )
+                new LinkMenuItem(
+                        id,
+                        new Model<String>( getCommander().getUndoTitle() ),
+                        new AjaxFallbackLink( "link" ) {
+                            @Override
+                            public void onClick( AjaxRequestTarget target ) {
+                                update( target, getCommander().undo() );
+                            }
+                        } )
 
-            : new Label( id, "Undo" )
+                : new Label( id, "Undo" )
                 .add( new AttributeModifier( "class", true, new Model<String>( "disabled" ) ) );
     }
 
@@ -142,18 +198,18 @@ public abstract class MenuPanel extends AbstractCommandablePanel {
      */
     protected Component getRedoMenuItem( String id ) {
         return getCommander().canRedo() ?
-              new LinkMenuItem(
-                id,
-                new Model<String>( getCommander().getRedoTitle() ),
-                new AjaxFallbackLink( "link" ) {
-                    @Override
-                    public void onClick( AjaxRequestTarget target ) {
-                        update(
-                            target, getCommander().redo() );
-                    }
-                } )
+                new LinkMenuItem(
+                        id,
+                        new Model<String>( getCommander().getRedoTitle() ),
+                        new AjaxFallbackLink( "link" ) {
+                            @Override
+                            public void onClick( AjaxRequestTarget target ) {
+                                update(
+                                        target, getCommander().redo() );
+                            }
+                        } )
 
-            : new Label( id, "Redo" )
+                : new Label( id, "Redo" )
                 .add( new AttributeModifier( "class", true, new Model<String>( "disabled" ) ) );
     }
 
@@ -166,25 +222,26 @@ public abstract class MenuPanel extends AbstractCommandablePanel {
     protected Component getSendMessageMenuItem( String id ) {
         final Identifiable identifiable = getModel().getObject();
         return identifiable != null && identifiable instanceof ModelObject ?
-              new LinkMenuItem(
-                  id,
-                  new Model<String>( "Send message" ),
-                  new AjaxFallbackLink( "link" ) {
-                          @Override
-                          public void onClick( AjaxRequestTarget target ) {
-                              update( target,
-                                      new Change( Change.Type.Communicated, identifiable ) );
-                          } } )
+                new LinkMenuItem(
+                        id,
+                        new Model<String>( "Send message" ),
+                        new AjaxFallbackLink( "link" ) {
+                            @Override
+                            public void onClick( AjaxRequestTarget target ) {
+                                update( target,
+                                        new Change( Change.Type.Communicated, identifiable ) );
+                            }
+                        } )
 
-            : new Label( id, "Send message" )
+                : new Label( id, "Send message" )
                 .add( new AttributeModifier( "class", true, new Model<String>( "disabled" ) ) );
     }
 
     /**
      * Make menu items from commands.
      *
-     * @param id        id of the menu item
-     * @param commands  a list of wrapped commands
+     * @param id       id of the menu item
+     * @param commands a list of wrapped commands
      * @return a list of menu item components
      */
     protected List<Component> getCommandMenuItems( String id, List<CommandWrapper> commands ) {
@@ -198,36 +255,42 @@ public abstract class MenuPanel extends AbstractCommandablePanel {
                 final Command command = commandWrapper.getCommand();
                 String label = command.getLabel( commander );
                 menuItems.add(
-                    commander.canDo( command ) ? new LinkMenuItem(
-                        id,
-                        new Model<String>( label ),
-                        new ConfirmedAjaxFallbackLink(
-                            "link", commandWrapper.isConfirm() ? "Are you sure?" : null ) {
-                            @Override
-                            public void onClick( AjaxRequestTarget target ) {
-                                commandWrapper.onExecuted(
-                                    target, getCommander().doCommand( command ) );
-                            }
-                        } )
+                        commander.canDo( command ) ? new LinkMenuItem(
+                                id,
+                                new Model<String>( label ),
+                                new ConfirmedAjaxFallbackLink(
+                                        "link", commandWrapper.isConfirm() ? "Are you sure?" : null ) {
+                                    @Override
+                                    public void onClick( AjaxRequestTarget target ) {
+                                        commandWrapper.onExecuted(
+                                                target, getCommander().doCommand( command ) );
+                                    }
+                                } )
 
-                        : new Label( id, new Model<String>( label ) )
-                            .add( new AttributeModifier(
-                                "class", true, new Model<String>( "disabled" ) ) ) );
+                                : new Label( id, new Model<String>( label ) )
+                                .add( new AttributeModifier(
+                                        "class", true, new Model<String>( "disabled" ) ) ) );
 
             } catch ( CommandException e ) {
-            LoggerFactory.getLogger( getClass() ).warn( "Unable to get command label", e );
-        }
+                LoggerFactory.getLogger( getClass() ).warn( "Unable to get command label", e );
+            }
 
         return menuItems;
     }
 
-    /** A Model object wrapper. */
+    /**
+     * A Model object wrapper.
+     */
     public static class ModelObjectWrapper implements Serializable {
 
-        /** Model object. */
+        /**
+         * Model object.
+         */
         private ModelObject modelObject;
 
-        /** Title. */
+        /**
+         * Title.
+         */
         private String title;
 
         public ModelObjectWrapper( String title, ModelObject modelObject ) {
