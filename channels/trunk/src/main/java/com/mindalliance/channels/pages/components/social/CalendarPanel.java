5712c755e80f46c1a031a7241a3fcaaa20bd82f6
jf <jf@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2011-04-21 18:01:52 +0000

    Added calendar login info
    git-svn-id: https://wush.net/svn/mindalliance@2263 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- channels/trunk/src/main/java/com/mindalliance/channels/pages/components/social/CalendarPanel.java
+++ channels/trunk/src/main/java/com/mindalliance/channels/pages/components/social/CalendarPanel.java
@@ -1,8 +1,10 @@
 package com.mindalliance.channels.pages.components.social;
 
 import com.mindalliance.channels.dao.PlanManager;
+import com.mindalliance.channels.dao.User;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
@@ -27,6 +29,7 @@ public class CalendarPanel extends AbstractSocialListPanel {
     private static final String BG_COLOR = "F6F6F6";
 
     private static final String SRC = "{0}?showTitle=0&amp;showDate=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=300&amp;wkst=1&amp;bgcolor=%23{2}&amp;src={1}&amp;color=%232952A3&amp;ctz={3}&pvttk={4} ";
+    private static final String CALENDAR_LINK = "https://www.google.com/calendar/render";
 
     public CalendarPanel( String id, boolean collapsible ) {
         super( id, collapsible );
@@ -35,15 +38,32 @@ public class CalendarPanel extends AbstractSocialListPanel {
 
     protected void init() {
         super.init();
+        addCalendarFrame();
+        addCalendarLogin();
+    }
+
+    private void addCalendarFrame() {
         WebMarkupContainer calendarFrame = new WebMarkupContainer( "calendarFrame" );
         String src = MessageFormat.format( SRC,
                 getCalenderHost(),
-                getCalendar(),
+                getEncodedCalendar(),
                 BG_COLOR,
-                getTimeZone(),
+                getEncodedTimeZone(),
                 getCalendarPrivateTicket() );
         calendarFrame.add( new AttributeModifier( "src", true, new Model<String>( src ) ) );
         add( calendarFrame );
+    }
+
+    private void addCalendarLogin() {
+        WebMarkupContainer loginInfo = new WebMarkupContainer( "loginInfo" );
+        ExternalLink calendarLoginLink = new ExternalLink(
+                "calendarLink",
+                CALENDAR_LINK,
+                getCalendar()
+                 );
+        loginInfo.add( calendarLoginLink );
+        add( loginInfo );
+        loginInfo.setVisible( User.current().isPlanner() );
     }
 
     private String getCalendarPrivateTicket() {
@@ -57,16 +77,18 @@ public class CalendarPanel extends AbstractSocialListPanel {
     }
 
     private String getCalendar() {
+      return getPlan().getCommunityCalendar( planManager.getDefaultCommunityCalendar() );
+    }
+
+    private String getEncodedCalendar() {
         try {
-            return URLEncoder.encode( getPlan().getCommunityCalendar(
-                    planManager.getDefaultCommunityCalendar() ),
-                    "UTF-8" );
+            return URLEncoder.encode( getCalendar(), "UTF-8" );
         } catch ( UnsupportedEncodingException e ) {
             throw new RuntimeException( e );
         }
     }
 
-    private String getTimeZone() {
+    private String getEncodedTimeZone() {
         String timezone = TimeZone.getDefault().getID();
         try {
             return URLEncoder.encode( timezone, "UTF-8" );
