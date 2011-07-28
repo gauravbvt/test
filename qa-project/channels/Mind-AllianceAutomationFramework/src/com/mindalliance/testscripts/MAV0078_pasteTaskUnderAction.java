5c2df5c16bca405fe2649fb3f377681ea15e16d9
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2011-07-28 12:45:52 +0000

    Renamed
    git-svn-id: https://wush.net/svn/mindalliance@2526 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ qa-project/channels/Mind-AllianceAutomationFramework/src/com/mindalliance/testscripts/MAV0078_pasteTaskUnderAction.java
@@ -0,0 +1,162 @@
+package com.mindalliance.testscripts;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import com.mindalliance.globallibrary.ApplicationFunctionLibrary;
+import com.mindalliance.globallibrary.GenericFunctionLibrary;
+import com.mindalliance.globallibrary.GlobalVariables;
+import com.mindalliance.globallibrary.LogFunctions;
+import com.mindalliance.globallibrary.ReportFunctions;
+
+public class MAV0078_pasteTaskUserAction
+{
+	public MAV0078_pasteTaskUserAction() {
+		try {
+			GlobalVariables.sTestCaseId = "MAV0078_pasteTaskUserAction";
+			GlobalVariables.sDescription = "Testcase: " + GlobalVariables.sTestCaseId + " execution started";
+			LogFunctions.writeLogs(GlobalVariables.sDescription);
+			System.out.println(GlobalVariables.sDescription);
+			// Call login()
+			GlobalVariables.bIsSuccess = ApplicationFunctionLibrary.login();
+			if (GlobalVariables.bIsSuccess) {
+				 
+				// Click on 'Information Sharing Model' link
+				GlobalVariables.iStepNo++ ;
+				GlobalVariables.sDescription = "Navigated to Information Sharing Model";
+				GlobalVariables.oDriver.findElement(By.linkText(GlobalVariables.viewElements.get("informationSharingModel"))).click();
+				// Write Results
+				LogFunctions.writeLogs(GlobalVariables.sDescription);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(1000);  	
+				
+				// Clicks on 'Add new Task' link under Action pop up menu option 	
+				GlobalVariables.iStepNo++ ;
+				GlobalVariables.sDescription = "New task added";
+		    	ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathActionsPopUpMenu"),GlobalVariables.viewElements.get("addNewTask"));
+		    	// WebElement Synchronization
+			    Thread.currentThread();
+			    Thread.sleep(1000);
+			    String task="This is Task 1234";
+				// Write Task
+				GlobalVariables.oDriver.findElement(By.name("segment:part:task")).click();
+				GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.name("segment:part:task"));
+				for (int i = 0; i <= 50; i++)
+					GlobalVariables.oElement.sendKeys(Keys.BACK_SPACE);
+				GlobalVariables.oElement.sendKeys(task);
+				// select category
+				GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.name("segment:part:category"));
+				GlobalVariables.oElement.sendKeys("Audit");
+		    	// Write Results
+				LogFunctions.writeLogs(GlobalVariables.sDescription);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				// WebElement Synchronization
+			    Thread.currentThread();
+			    Thread.sleep(5000);
+			    
+			    // Click on cut task under action pop up menu of task details
+			    GlobalVariables.iStepNo++ ;
+				GlobalVariables.sDescription = "Cut task";
+		    	ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathTaskActionsMenu"),GlobalVariables.viewElements.get("cutTask"));
+		    	Alert alert = GlobalVariables.oDriver.switchTo().alert();
+				alert.accept();
+		    	// Write Results
+				LogFunctions.writeLogs(GlobalVariables.sDescription);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				// WebElement Synchronization
+			    Thread.currentThread();
+			    Thread.sleep(3000);
+			    
+			    // Click on Paste Task under Action pop up menu
+			    GlobalVariables.iStepNo++ ;
+				GlobalVariables.sDescription = "Paste task done";
+		    	ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathActionsPopUpMenu"),GlobalVariables.viewElements.get("pasteTask"));
+				// WebElement Synchronization
+			    Thread.currentThread();
+			    Thread.sleep(3000);
+			    // Assertion: Verify that Task has been pasted
+			    ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathShowPopUpMenu"),GlobalVariables.viewElements.get("aboutPlanSegment"));
+				// WebElement Synchronization
+			    Thread.currentThread();
+			    Thread.sleep(3000);
+				ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathAbtPlanSegShowMenu"),GlobalVariables.viewElements.get("taskMover"));
+				// WebElement Synchronization
+			    Thread.currentThread();
+			    Thread.sleep(1000);
+			    GlobalVariables.oElement=GlobalVariables.oDriver.findElement(By.linkText(task));
+			    if(GlobalVariables.oElement.getText().equals(GlobalVariables.viewElements.get("task1234")))
+			    {
+			    	// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+							GlobalVariables.sBlank, GlobalVariables.sBlank);
+			    }
+			    else
+			    {
+			    	GlobalVariables.sVerifyError ="Verification Failed "+"Expected "+GlobalVariables.viewElements.get("task1234")+" Actual "+GlobalVariables.oElement.getText();
+			    	// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription + "" + GlobalVariables.sFailed);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+							GlobalVariables.sBlank, GlobalVariables.sVerifyError);
+			    }
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(5000);					   
+			    
+			    // Call logout()
+				GlobalVariables.iStepNo++ ;
+				GlobalVariables.sDescription = "Logout is successful";
+				ApplicationFunctionLibrary.logout();
+				// Write Results
+				LogFunctions.writeLogs(GlobalVariables.sDescription);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				// WebElement Synchronization
+			    Thread.currentThread();
+			    Thread.sleep(1000);
+				
+				LogFunctions.writeLogs("Testcase: " + GlobalVariables.sTestCaseId + " execution completed");
+				System.out.println("Testcase: " + GlobalVariables.sTestCaseId + " execution completed");
+			
+				}
+			
+				else
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+							GlobalVariables.sBlank, GlobalVariables.sBlank);
+			} 
+			catch (Exception e) {
+				if (GlobalVariables.oDriver.getTitle().equals(GlobalVariables.sInternalErrorPageTitle)) {
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+							e.getMessage(),GlobalVariables.sErrorLogSubDirectoryPath + "\\" + GlobalVariables.sTestCaseId + ".logs");
+					GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.id("stackTrace"));
+					LogFunctions.writeErrorLogs(GlobalVariables.oElement.getText());
+					ApplicationFunctionLibrary.logout();
+				}
+				else {
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+							e.getMessage(),GlobalVariables.sBlank);
+					ApplicationFunctionLibrary.logout();	
+				}
+				System.out.println("Testcase: " + GlobalVariables.sTestCaseId + " execution failed");
+			}
+		}
+	public static void main(String args[]) {
+		try {
+			GenericFunctionLibrary.initializeTestData();
+			GenericFunctionLibrary.loadObjectRepository();
+			new MAV0078_pasteTaskUserAction();
+			GenericFunctionLibrary.tearDownTestData();
+			ReportFunctions.generateAutomationReport();
+		} 
+		catch (Exception oException) {
+			// TODO Auto-generated catch block
+			oException.printStackTrace();
+		}
+	}    
+}
