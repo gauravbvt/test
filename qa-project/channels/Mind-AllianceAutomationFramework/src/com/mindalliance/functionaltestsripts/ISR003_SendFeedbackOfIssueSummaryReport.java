2cbf0c5c13d6b8b0a77bc8fafc37966290ab6b8f
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2011-08-10 08:54:35 +0000

    git-svn-id: https://wush.net/svn/mindalliance@2721 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ qa-project/channels/Mind-AllianceAutomationFramework/src/com/mindalliance/functionaltestsripts/ISR003_SendFeedbackOfIssueSummaryReport.java
@@ -0,0 +1,131 @@
+package com.mindalliance.functionaltestsripts;
+
+import org.openqa.selenium.By;
+
+import com.mindalliance.globallibrary.ApplicationFunctionLibrary;
+import com.mindalliance.globallibrary.GenericFunctionLibrary;
+import com.mindalliance.globallibrary.GlobalVariables;
+import com.mindalliance.globallibrary.LogFunctions;
+import com.mindalliance.globallibrary.ReportFunctions;
+
+public class ISR003_SendFeedbackOfIssueSummaryReport 
+{
+	public ISR003_SendFeedbackOfIssueSummaryReport(){
+    	try {
+			GlobalVariables.sTestCaseId = "ISR003_SendFeedbackOfIssueSummaryReport";
+			GlobalVariables.sDescription = "Testcase: " + GlobalVariables.sTestCaseId + " execution started";
+			LogFunctions.writeLogs(GlobalVariables.sDescription);
+			System.out.println(GlobalVariables.sDescription);
+			// Call login()
+			GlobalVariables.bIsSuccess = ApplicationFunctionLibrary.login();
+			if (GlobalVariables.bIsSuccess) {
+				
+				// Click on 'Issue Summary Report' link
+				GlobalVariables.iStepNo++ ;
+				GlobalVariables.sDescription = "Navigate to Issue Summary Report";
+				GlobalVariables.oDriver.findElement(By.linkText(GlobalVariables.viewElements.get("issueSummaryReport"))).click();
+				// Write Results
+				LogFunctions.writeLogs(GlobalVariables.sDescription);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(1000);
+				
+				// Click on send feedback button
+				GlobalVariables.iStepNo++;
+				GlobalVariables.sDescription="Send Feedback";
+				GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.plan.get("sXpathSendFeedBackOfIssueSummaryReport"))).click();
+				// Write Results
+				LogFunctions.writeLogs(GlobalVariables.sDescription);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(3000);
+				
+				// Assertion: Verify that “Send Feedback” option is accessible
+				GlobalVariables.iStepNo++;
+				GlobalVariables.sDescription="Send feedback is accessible";
+				GlobalVariables.oElement=GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.plan.get("sXpathSendFeedbackWindow")));
+				if(GlobalVariables.oElement.getText().equals("")){
+					// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+							GlobalVariables.sBlank, GlobalVariables.sBlank);
+				}
+				else{
+					GlobalVariables.sVerifyError ="Verification Failed "+"Expected '' "+" Actual "+GlobalVariables.oElement.getText();
+					// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription + "" + GlobalVariables.sFailed);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+							GlobalVariables.sBlank, GlobalVariables.sVerifyError);
+				}
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(3000);
+				
+				// Click on Home Icon
+				GlobalVariables.iStepNo++;
+				GlobalVariables.sDescription="Home Page";
+				GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.plan.get("sXpathIssueSummaryReportToHome"))).click();
+				// Write Results
+				LogFunctions.writeLogs(GlobalVariables.sDescription);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(1000);
+				
+				// Call logout()
+				GlobalVariables.iStepNo++ ;
+				GlobalVariables.sDescription = "Logout is successful";
+				GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.home.get("sXpathLogoutHomePage"))).click();
+				// Write Results
+				LogFunctions.writeLogs(GlobalVariables.sDescription);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(3000);
+				GlobalVariables.oDriver.quit();
+				
+				LogFunctions.writeLogs("Testcase: " + GlobalVariables.sTestCaseId + " execution completed");
+				System.out.println("Testcase: " + GlobalVariables.sTestCaseId + " execution completed");
+			}
+			else
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+						GlobalVariables.sBlank, GlobalVariables.sBlank);
+		}
+		catch (Exception e) {
+			if (GlobalVariables.oDriver.getTitle().equals(GlobalVariables.sInternalErrorPageTitle)) {
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+						e.getMessage(),GlobalVariables.sErrorLogSubDirectoryPath + "\\" + GlobalVariables.sTestCaseId + ".logs");
+				GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.id("stackTrace"));
+				LogFunctions.writeErrorLogs(GlobalVariables.oElement.getText());
+				GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.home.get("sXpathLogoutHomePage"))).click();
+				GlobalVariables.oDriver.quit();
+			}
+			else {
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+						e.getMessage(),GlobalVariables.sBlank);
+				GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.home.get("sXpathLogoutHomePage"))).click();
+				GlobalVariables.oDriver.quit();
+			}
+			System.out.println("Testcase: " + GlobalVariables.sTestCaseId + " execution failed");
+		}
+	}
+	public static void main(String args[]) {
+		try {
+			GenericFunctionLibrary.initializeTestData();
+			GenericFunctionLibrary.loadObjectRepository();
+			new ISR003_SendFeedbackOfIssueSummaryReport();
+			GenericFunctionLibrary.tearDownTestData();
+			ReportFunctions.generateAutomationReport();
+		} 
+		catch (Exception oException) {
+			// TODO Auto-generated catch block
+			oException.printStackTrace();
+		}
+	}
+}
