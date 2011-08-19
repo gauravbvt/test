f3c72210fab2053036b23838c4c1056e64085df5
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2011-08-19 09:21:57 +0000

    Modified
    git-svn-id: https://wush.net/svn/mindalliance@2804 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- qa-project/channels/Mind-AllianceAutomationFramework/src/com/mindalliance/functionaltestsripts/CA020_AssignRole.java
+++ qa-project/channels/Mind-AllianceAutomationFramework/src/com/mindalliance/functionaltestsripts/CA020_AssignRole.java
@@ -1,16 +1,21 @@
 package com.mindalliance.functionaltestsripts;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import com.mindalliance.globallibrary.ApplicationFunctionLibrary;
 import com.mindalliance.globallibrary.GenericFunctionLibrary;
 import com.mindalliance.globallibrary.GlobalVariables;
 import com.mindalliance.globallibrary.LogFunctions;
 import com.mindalliance.globallibrary.ReportFunctions;
 
-public class CA020_AssignRole 
+public class CA020_AssignRole
 {
 	public CA020_AssignRole() {
 		try {
+
 			GlobalVariables.sTestCaseId = "CA020_AssignRole";
 			GlobalVariables.sDescription = "Testcase: " + GlobalVariables.sTestCaseId + " execution started";
 			LogFunctions.writeLogs(GlobalVariables.sDescription);
@@ -18,81 +23,82 @@ public class CA020_AssignRole
 			// Call login()
 			GlobalVariables.bIsSuccess = ApplicationFunctionLibrary.login();
 			if (GlobalVariables.bIsSuccess) {
-				
+
 				// Click on 'Channels Administration' link
 				GlobalVariables.iStepNo++ ;
 				GlobalVariables.sDescription = "Navigated to Channels Administration";
 				GlobalVariables.oDriver.findElement(By.linkText(GlobalVariables.viewElements.get("channelsAdministration"))).click();
 				// Write Results
 				LogFunctions.writeLogs(GlobalVariables.sDescription);
-				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed,
 						GlobalVariables.sBlank, GlobalVariables.sBlank);
 				// WebElement Synchronization
 				Thread.currentThread();
-				Thread.sleep(1000);
-				
+				Thread.sleep(2000);
+
 				// Create User
 				GlobalVariables.iStepNo++;
 				GlobalVariables.sDescription="User Created";
-				GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.channelsAdmin.get("sXpathInputUserID"))).click();
 				GlobalVariables.oElement=GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.channelsAdmin.get("sXpathInputUserID")));
 				GlobalVariables.oElement.sendKeys(GlobalVariables.testData.get("user"));
 				// WebElement Synchronization
 				Thread.currentThread();
-				Thread.sleep(1000);								
+				Thread.sleep(2000);
 				// Click on 'Submit' button
 				GlobalVariables.oDriver.findElement(By.name("Submit")).submit();
 				// WebElement Synchronization
 				Thread.currentThread();
-				Thread.sleep(1000);								
+				Thread.sleep(2000);
 				// Write Results
 				LogFunctions.writeLogs(GlobalVariables.sDescription);
-				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed,
 						GlobalVariables.sBlank, GlobalVariables.sBlank);
 				// WebElement Synchronization
 				Thread.currentThread();
-				Thread.sleep(1000);
-				
+				Thread.sleep(2000);
+
 				//Assign Access Privilege 'Admin' to User
 				GlobalVariables.iStepNo++ ;
 				GlobalVariables.sDescription = "Assign Access Privilege 'Admin' to User";
-				GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.channelsAdmin.get("sXpathUserPassword"))).click();
-				GlobalVariables.oElement=GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.channelsAdmin.get("sXpathUserPassword")));
-				GlobalVariables.oElement.sendKeys(GlobalVariables.testData.get("Password"));
+				GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.xpath("//body/div/div[2]/div/form/table[7]/tbody"));
+				List<WebElement> trs = GlobalVariables.oElement.findElements(By.tagName("tr"));
+				List<WebElement> tds;
+				int i=-1;
+				for(WebElement tr: trs) {
+					i++;
+					tds = tr.findElements(By.tagName("td"));
+					for(WebElement td: tds) {
+						if(td.getText().contains(GlobalVariables.testData.get("user"))){
+							GlobalVariables.oElement=GlobalVariables.oDriver.findElement(By.name("item:"+i+":group:password"));
+							GlobalVariables.oElement.sendKeys(GlobalVariables.testData.get("Password"));
+							// WebElement Synchronization
+							Thread.currentThread();
+							Thread.sleep(2000);
+							GlobalVariables.oDriver.findElement(By.xpath("/html/body/div/div[2]/div/form/table[7]/tbody/tr["+(i+1)+"]/td[5]/input")).click();
+						}
+					}
+				}
 				// WebElement Synchronization
 				Thread.currentThread();
-				Thread.sleep(1000);
-				GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.channelsAdmin.get("sXpathAccessPrivilegeAdmin"))).click();
-				// WebElement Synchronization
-				Thread.currentThread();
-				Thread.sleep(1000);
-				//Click on 'Submit' Button
+				Thread.sleep(2000);
 				GlobalVariables.oDriver.findElement(By.name("Submit")).submit();
 				// WebElement Synchronization
 				Thread.currentThread();
-				Thread.sleep(1000);
-				// Write Results
-				LogFunctions.writeLogs(GlobalVariables.sDescription);
-				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
-						GlobalVariables.sBlank, GlobalVariables.sBlank);
-				// WebElement Synchronization
-				Thread.currentThread();
-				Thread.sleep(1000);
-				
+				Thread.sleep(2000);
+
 			  	// Click on 'Signout<user name>' Link
 				GlobalVariables.iStepNo++;
 				GlobalVariables.sDescription="Logout Successful";
 				GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.channelsAdmin.get("sXpathLogoutAdminPage"))).click();
 				// Write Results
 				LogFunctions.writeLogs(GlobalVariables.sDescription);
-				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed,
 						GlobalVariables.sBlank, GlobalVariables.sBlank);
 				// WebElement Synchronization
 				Thread.currentThread();
-				Thread.sleep(1000);
-				
+				Thread.sleep(2000);
+
 				//Assertion : Verify that newly created user log into the system & 'Channels Administration' Link is accessible to the user
-				// Enter User name and password
 			    GlobalVariables.iStepNo++;
 			    GlobalVariables.sDescription="Login with newly created username";
 			    GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.name("j_username"));
@@ -101,11 +107,12 @@ public class CA020_AssignRole
 			    GlobalVariables.oElement.sendKeys(GlobalVariables.testData.get("Password"));
 			    // Write Results
 		    	LogFunctions.writeLogs(GlobalVariables.sDescription);
-		    	LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+		    	LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed,
 					GlobalVariables.sBlank, GlobalVariables.sBlank);
 		    	// WebElement Synchronization
 			    Thread.currentThread();
-			    Thread.sleep(1000);			    
+			    Thread.sleep(2000);
+
 			    // Click on Sign In button
 			    GlobalVariables.iStepNo++ ;
 			    GlobalVariables.sDescription = "Login is successful";
@@ -113,67 +120,106 @@ public class CA020_AssignRole
 			    GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.login.get("sLogin"))).click();
 			    // Write Results
 		    	LogFunctions.writeLogs(GlobalVariables.sDescription);
-		    	LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+		    	LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed,
 					GlobalVariables.sBlank, GlobalVariables.sBlank);
 		    	// WebElement Synchronization
 			    Thread.currentThread();
-			    Thread.sleep(1000);
-			    
+			    Thread.sleep(2000);
+
 			    // Verify that 'Channels Administration' Link is Available
 			    GlobalVariables.iStepNo++ ;
 			    GlobalVariables.sDescription = "'Channels Administration' Link Available";
 			    GlobalVariables.oElement=GlobalVariables.oDriver.findElement(By.linkText(GlobalVariables.viewElements.get("channelsAdministration")));
 			    if (GlobalVariables.oElement.getText().equals(GlobalVariables.viewElements.get("channelsAdministration"))) {
-			     // Write Results
+			    	// Write Results
 			    	LogFunctions.writeLogs(GlobalVariables.sDescription);
-			    	LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+			    	LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed,
 			    			GlobalVariables.sBlank, GlobalVariables.sBlank);
 			    }
 			    else{
 			    	GlobalVariables.sVerifyError ="Verification Failed "+"Expected "+GlobalVariables.viewElements.get("channelsAdministration")+" Actual "+GlobalVariables.oElement.getText();
 			    	// Write Results
 			    	LogFunctions.writeLogs(GlobalVariables.sDescription + "" + GlobalVariables.sFailed);
-			    	LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+			    	LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed,
 			    	  GlobalVariables.sBlank, GlobalVariables.sVerifyError);
-			    }	
+			    }
 			    // WebElement Synchronization
 			    Thread.currentThread();
-			    Thread.sleep(1000);
+			    Thread.sleep(2000);
 
-			 // Call logout()
+			    // Call logout()
 				GlobalVariables.iStepNo++ ;
 				GlobalVariables.sDescription = "Logout is successful";
 				GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.home.get("sXpathLogoutHomePage"))).click();
 				// WebElement Synchronization
 				Thread.currentThread();
-				Thread.sleep(1000);
-				GlobalVariables.oDriver.quit();
-				// Write Results
-				LogFunctions.writeLogs(GlobalVariables.sDescription);
-				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
-						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				Thread.sleep(2000);
+				// Delete Created User
+			    GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.name("j_username"));
+			    GlobalVariables.oElement.sendKeys(GlobalVariables.login.get("sUsername"));
+			    GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.name("j_password"));
+			    GlobalVariables.oElement.sendKeys(GlobalVariables.login.get("sPassword"));
+		    	// WebElement Synchronization
+			    Thread.currentThread();
+			    Thread.sleep(2000);
+			    GlobalVariables.oDriver.findElement(By.name("_spring_security_remember_me")).click();
+			    GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.login.get("sLogin"))).click();
+		    	// WebElement Synchronization
+			    Thread.currentThread();
+			    Thread.sleep(2000);
+			    // Navigate to Channels Administrations Link
+			    GlobalVariables.oDriver.findElement(By.linkText(GlobalVariables.viewElements.get("channelsAdministration"))).click();
+			    // WebElement Synchronization
+			    Thread.currentThread();
+			    Thread.sleep(2000);
+				// Delete Created User
+				GlobalVariables.iStepNo++;
+				GlobalVariables.sDescription="User Deleted";
+				GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.xpath("//body/div/div[2]/div/form/table[7]/tbody"));
+				trs = GlobalVariables.oElement.findElements(By.tagName("tr"));
+				i=0;
+				for(WebElement tr: trs) {
+					i++;
+					tds = tr.findElements(By.tagName("td"));
+					for(WebElement td: tds) {
+						if(td.getText().contains(GlobalVariables.testData.get("user"))){
+							GlobalVariables.oDriver.findElement(By.xpath("/html/body/div/div[2]/div/form/table[7]/tbody/tr["+i+"]/td[12]/input")).click();
+						}
+					}
+				}
 				// WebElement Synchronization
 				Thread.currentThread();
-				Thread.sleep(1000);
+				Thread.sleep(2000);
+				GlobalVariables.oDriver.findElement(By.name("Submit")).submit();
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(2000);
+				// Logout of Channels
+				GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.channelsAdmin.get("sXpathLogoutAdminPage"))).click();
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(2000);
+
+				GlobalVariables.oDriver.quit();
 
 				LogFunctions.writeLogs("Testcase: " + GlobalVariables.sTestCaseId + " execution completed");
 				System.out.println("Testcase: " + GlobalVariables.sTestCaseId + " execution completed");
-				
+
 			}
 			else
-				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed,
 						GlobalVariables.sBlank, GlobalVariables.sBlank);
 		}
 		catch (Exception e) {
 				if (GlobalVariables.oDriver.getTitle().equals(GlobalVariables.sInternalErrorPageTitle)) {
-					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed,
 							e.getMessage(),GlobalVariables.sErrorLogSubDirectoryPath + "\\" + GlobalVariables.sTestCaseId + ".logs");
 					GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.id("stackTrace"));
 					LogFunctions.writeErrorLogs(GlobalVariables.oElement.getText());
 					ApplicationFunctionLibrary.logout();
 				}
 				else {
-					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed,
 							e.getMessage(),GlobalVariables.sBlank);
 					ApplicationFunctionLibrary.logout();
 				}
@@ -187,7 +233,7 @@ public class CA020_AssignRole
 				new CA020_AssignRole();
 				GenericFunctionLibrary.tearDownTestData();
 				ReportFunctions.generateAutomationReport();
-			} 
+			}
 			catch (Exception oException) {
 				// TODO Auto-generated catch block
 				oException.printStackTrace();
