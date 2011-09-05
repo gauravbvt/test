0f02afe7785289a7a54c04ca82ef76d3de02bc8b
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2011-09-05 11:23:21 +0000

    Modified
    - ManoharS
    
    git-svn-id: https://wush.net/svn/mindalliance@2929 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- qa-project/channels/Mind-AllianceAutomationFramework/src/com/mindalliance/testscripts/MAP0015_removeSegment.java
+++ qa-project/channels/Mind-AllianceAutomationFramework/src/com/mindalliance/testscripts/MAP0015_removeSegment.java
@@ -13,146 +13,187 @@ import com.mindalliance.globallibrary.GenericFunctionLibrary;
 import com.mindalliance.globallibrary.GlobalVariables;
 import com.mindalliance.globallibrary.LogFunctions;
 import com.mindalliance.globallibrary.ReportFunctions;
+
 /**
- * Test Case ID: MAP0015_removeSegment
- * Summary: Verify that Segment can be deleted from the plan
+ * Test Case ID: MAP0015_removeSegment Summary: Verify that Segment can be
+ * deleted from the plan
+ * 
  * @author AfourTech
- *
+ * 
  */
 public class MAP0015_removeSegment {
 	public MAP0015_removeSegment() {
 		try {
 			GlobalVariables.sTestCaseId = "MAP0015_removeSegment";
-			GlobalVariables.sDescription = "Testcase: " + GlobalVariables.sTestCaseId + " execution started";
+			GlobalVariables.sDescription = "Testcase: "
+				+ GlobalVariables.sTestCaseId + " execution started";
 			LogFunctions.writeLogs(GlobalVariables.sDescription);
 			System.out.println(GlobalVariables.sDescription);
 			// Call login()
 			GlobalVariables.bIsSuccess = ApplicationFunctionLibrary.login();
 			if (GlobalVariables.bIsSuccess) {
-				
+
 				// Click on 'Information Sharing Model' link
-				GlobalVariables.iStepNo++ ;
+				GlobalVariables.iStepNo++;
 				GlobalVariables.sDescription = "Navigated to Information Sharing Model";
-				GlobalVariables.oDriver.findElement(By.linkText(GlobalVariables.viewElements.get("informationSharingModel"))).click();
+				GlobalVariables.oDriver.findElement(
+						By.linkText(GlobalVariables.viewElements
+								.get("informationSharingModel"))).click();
 				// Write Results
 				LogFunctions.writeLogs(GlobalVariables.sDescription);
-				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
-						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId,
+						GlobalVariables.iStepNo, GlobalVariables.sDescription,
+						GlobalVariables.sPassed, GlobalVariables.sBlank,
+						GlobalVariables.sBlank);
 				// WebElement Synchronization
 				Thread.currentThread();
 				Thread.sleep(10000);
-			    
+
 				// Click on 'Add new segment' option under 'Actions' pop up menu
-				GlobalVariables.iStepNo++ ;
+				GlobalVariables.iStepNo++;
 				GlobalVariables.sDescription = "New segment added successfully";
-				ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathActionsPopUpMenu"),GlobalVariables.viewElements.get("addNewSegment"));
+				ApplicationFunctionLibrary.MouseOverAndClick(
+						GlobalVariables.plan.get("sXpathActionsPopUpMenu"),
+						GlobalVariables.viewElements.get("addNewSegment"));
 				// Write Results
 				LogFunctions.writeLogs(GlobalVariables.sDescription);
-				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
-						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId,
+						GlobalVariables.iStepNo, GlobalVariables.sDescription,
+						GlobalVariables.sPassed, GlobalVariables.sBlank,
+						GlobalVariables.sBlank);
 				// WebElement Synchronization
 				Thread.currentThread();
 				Thread.sleep(1000);
-				
+
 				// Enter the details for new segment
-				GlobalVariables.iStepNo++ ;
+				GlobalVariables.iStepNo++;
 				GlobalVariables.sDescription = "Details entered";
-				GlobalVariables.oDriver.findElement(By.name("sg-editor:mo:aspect:name")).click();
-				GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.name("sg-editor:mo:aspect:name"));
-					for (int i = 0; i <= 8; i++)
-						GlobalVariables.oElement.sendKeys(Keys.BACK_SPACE);
-				GlobalVariables.oElement.sendKeys(GlobalVariables.testData.get("Segment For Remove Segment"));
+				GlobalVariables.oDriver.findElement(
+						By.name("sg-editor:mo:aspect:name")).click();
+				GlobalVariables.oElement = GlobalVariables.oDriver
+				.findElement(By.name("sg-editor:mo:aspect:name"));
+				for (int i = 0; i <= 8; i++)
+					GlobalVariables.oElement.sendKeys(Keys.BACK_SPACE);
+				GlobalVariables.oElement.sendKeys(GlobalVariables.testData
+						.get("Segment For Remove Segment"));
 				// Write Results
 				LogFunctions.writeLogs(GlobalVariables.sDescription);
-				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
-						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId,
+						GlobalVariables.iStepNo, GlobalVariables.sDescription,
+						GlobalVariables.sPassed, GlobalVariables.sBlank,
+						GlobalVariables.sBlank);
 				// WebElement Synchronization
 				Thread.currentThread();
 				Thread.sleep(1000);
-				
+
 				// Click on 'done' button
-				GlobalVariables.iStepNo++ ;
+				GlobalVariables.iStepNo++;
 				GlobalVariables.sDescription = "Segment updated";
-				GlobalVariables.oDriver.findElement(By.className("close")).click();
+				GlobalVariables.oDriver.findElement(By.className("close"))
+				.click();
 				// Write Results
 				LogFunctions.writeLogs(GlobalVariables.sDescription);
-				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
-						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId,
+						GlobalVariables.iStepNo, GlobalVariables.sDescription,
+						GlobalVariables.sPassed, GlobalVariables.sBlank,
+						GlobalVariables.sBlank);
 				// WebElement Synchronization
 				Thread.currentThread();
 				Thread.sleep(1000);
-				
+
 				// Click on 'Remove this segment' under 'Actions' pop up menu
-				GlobalVariables.iStepNo++ ;
+				GlobalVariables.iStepNo++;
 				GlobalVariables.sDescription = "Remove this segment";
-				ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathActionsPopUpMenu"),GlobalVariables.viewElements.get("removeThisSegment"));
+				ApplicationFunctionLibrary.MouseOverAndClick(
+						GlobalVariables.plan.get("sXpathActionsPopUpMenu"),
+						GlobalVariables.viewElements.get("removeThisSegment"));
 				// Get a handle to the open alert, prompt or confirmation
 				Alert alert = GlobalVariables.oDriver.switchTo().alert();
 				alert.accept();
 				// WebElement Synchronization
 				Thread.currentThread();
 				Thread.sleep(1000);
-				//Assertion:Verify that segment should get removed.
-				GlobalVariables.bIsSuccess=Boolean.FALSE;
-				GlobalVariables.oDropDown = new Select(GlobalVariables.oDriver.findElement(By.name("select-segment:sg-sel")));
-				List<WebElement> options=GlobalVariables.oDropDown.getOptions();
-			    for(WebElement option : options)
-			    {
-			    	if(GlobalVariables.testData.get("Segment For Remove Segment").equals(option.getText())){
-				     // Write Results
-			        GlobalVariables.bIsSuccess=Boolean.TRUE;
-				     break;
-			    	}
-			    }
-			    if(GlobalVariables.bIsSuccess==Boolean.FALSE)
-			    {
-			    	// Write Results
+				// Assertion:Verify that segment should get removed.
+				GlobalVariables.bIsSuccess = Boolean.FALSE;
+				GlobalVariables.oDropDown = new Select(GlobalVariables.oDriver
+						.findElement(By.name("select-segment:sg-sel")));
+				List<WebElement> options = GlobalVariables.oDropDown
+				.getOptions();
+				for (WebElement option : options) {
+					if (GlobalVariables.testData.get(
+							"Segment For Remove Segment").equals(
+									option.getText())) {
+						// Write Results
+						GlobalVariables.bIsSuccess = Boolean.TRUE;
+						break;
+					}
+				}
+				if (GlobalVariables.bIsSuccess == Boolean.FALSE) {
+					// Write Results
 					LogFunctions.writeLogs(GlobalVariables.sDescription);
-					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
-							GlobalVariables.sBlank, GlobalVariables.sBlank);
-			    }
-			    else{
-			    	// Write Results
-					LogFunctions.writeLogs(GlobalVariables.sDescription + "" + GlobalVariables.sFailed);
-					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
-							GlobalVariables.sBlank, GlobalVariables.sVerifyError);
-			    }
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId,
+							GlobalVariables.iStepNo,
+							GlobalVariables.sDescription,
+							GlobalVariables.sPassed, GlobalVariables.sBlank,
+							GlobalVariables.sBlank);
+				} else {
+					// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription + ""
+							+ GlobalVariables.sFailed);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId,
+							GlobalVariables.iStepNo,
+							GlobalVariables.sDescription,
+							GlobalVariables.sFailed, GlobalVariables.sBlank,
+							GlobalVariables.sVerifyError);
+				}
 				// WebElement Synchronization
 				Thread.currentThread();
-				Thread.sleep(1000);	
-				
-			    // Call logout()
-				GlobalVariables.iStepNo++ ;
+				Thread.sleep(1000);
+
+				// Call logout()
+				GlobalVariables.iStepNo++;
 				GlobalVariables.sDescription = "Logout is successful";
 				ApplicationFunctionLibrary.logout();
 				// Write Results
 				LogFunctions.writeLogs(GlobalVariables.sDescription);
-				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
-						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId,
+						GlobalVariables.iStepNo, GlobalVariables.sDescription,
+						GlobalVariables.sPassed, GlobalVariables.sBlank,
+						GlobalVariables.sBlank);
 
-				LogFunctions.writeLogs("Testcase: " + GlobalVariables.sTestCaseId + " execution completed");
-				System.out.println("Testcase: " + GlobalVariables.sTestCaseId + " execution completed");
-			}
-			else
-				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
-						GlobalVariables.sBlank, GlobalVariables.sBlank);
-		} 
-		catch (Exception e) {
-			if (GlobalVariables.oDriver.getTitle().equals(GlobalVariables.sInternalErrorPageTitle)) {
-				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
-						e.getMessage(),GlobalVariables.sErrorLogSubDirectoryPath + "\\" + GlobalVariables.sTestCaseId + ".logs");
-				GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.id("stackTrace"));
+				LogFunctions.writeLogs("Testcase: "
+						+ GlobalVariables.sTestCaseId + " execution completed");
+				System.out.println("Testcase: " + GlobalVariables.sTestCaseId
+						+ " execution completed");
+			} else
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId,
+						GlobalVariables.iStepNo, GlobalVariables.sDescription,
+						GlobalVariables.sFailed, GlobalVariables.sBlank,
+						GlobalVariables.sBlank);
+		} catch (Exception e) {
+			if (GlobalVariables.oDriver.getTitle().equals(
+					GlobalVariables.sInternalErrorPageTitle)) {
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId,
+						GlobalVariables.iStepNo, GlobalVariables.sDescription,
+						GlobalVariables.sFailed, e.getMessage(),
+						GlobalVariables.sErrorLogSubDirectoryPath + "\\"
+						+ GlobalVariables.sTestCaseId + ".logs");
+				GlobalVariables.oElement = GlobalVariables.oDriver
+				.findElement(By.id("stackTrace"));
 				LogFunctions.writeErrorLogs(GlobalVariables.oElement.getText());
 				ApplicationFunctionLibrary.logout();
+			} else {
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId,
+						GlobalVariables.iStepNo, GlobalVariables.sDescription,
+						GlobalVariables.sFailed, e.getMessage(),
+						GlobalVariables.sBlank);
+				ApplicationFunctionLibrary.logout();
 			}
-			else {
-				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
-						e.getMessage(),GlobalVariables.sBlank);
-				ApplicationFunctionLibrary.logout();	
-			}
-			System.out.println("Testcase: " + GlobalVariables.sTestCaseId + " execution failed");
+			System.out.println("Testcase: " + GlobalVariables.sTestCaseId
+					+ " execution failed");
 		}
 	}
+
 	public static void main(String args[]) {
 		try {
 			GenericFunctionLibrary.initializeTestData();
@@ -160,8 +201,7 @@ public class MAP0015_removeSegment {
 			new MAP0015_removeSegment();
 			GenericFunctionLibrary.tearDownTestData();
 			ReportFunctions.generateAutomationReport();
-		} 
-		catch (Exception oException) {
+		} catch (Exception oException) {
 			// TODO Auto-generated catch block
 			oException.printStackTrace();
 		}
