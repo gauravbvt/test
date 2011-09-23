077e82647560c4a3aa158b0ed2cc95f04c05c5d4
denis <denis@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2011-09-23 18:31:03 +0000

    Added planManager and analyst as command listeners...
    git-svn-id: https://wush.net/svn/mindalliance@3131 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- channels/trunk/src/test/java/com/mindalliance/channels/engine/nlp/TestSemanticProximityMatcher.java
+++ channels/trunk/src/test/java/com/mindalliance/channels/engine/nlp/TestSemanticProximityMatcher.java
@@ -1,5 +1,7 @@
 package com.mindalliance.channels.engine.nlp;
 
+import com.mindalliance.channels.core.nlp.Proximity;
+import com.mindalliance.channels.core.nlp.WordnetSemanticMatcher;
 import junit.framework.TestCase;
 import org.springframework.core.io.FileSystemResource;
 
