d502e55474c9cca671c3900a8025dbb339cc000f
denis <denis@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2011-09-24 18:41:05 +0000

    Fixed cache clearing. Relaxed checkstyle a bit.
    git-svn-id: https://wush.net/svn/mindalliance@3132 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- channels/trunk/src/main/java/com/mindalliance/channels/core/query/CacheResetAdvice.java
+++ channels/trunk/src/main/java/com/mindalliance/channels/core/query/CacheResetAdvice.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2011 Mind-Alliance Systems LLC.
+ * All rights reserved.
+ * Proprietary and Confidential.
+ */
+
 package com.mindalliance.channels.core.query;
 
 import org.springframework.aop.AfterReturningAdvice;
@@ -17,25 +23,27 @@ public class CacheResetAdvice implements AfterReturningAdvice {
 
     /**
      * Callback after a given method successfully returned.
-     * @param returnValue the value returned by the method, if any
-     * @param method method being invoked
-     * @param args arguments to the method
-     * @param target target of the method invocation. May be <code>null</code>.
-     * @throws Throwable if this object wishes to abort the call.
-     * Any exception thrown will be returned to the caller if it's
-     * allowed by the method signature. Otherwise the exception
-     * will be wrapped as a runtime exception.
+     *
+     * @param   returnValue  the value returned by the method, if any
+     * @param   method       method being invoked
+     * @param   args         arguments to the method
+     * @param   target       target of the method invocation. May be {@code null}.
+     *
+     * @throws  Throwable  if this object wishes to abort the call. Any exception thrown will be returned to the caller
+     *                     if it's allowed by the method signature. Otherwise the exception will be wrapped as a runtime
+     *                     exception.
      */
-    public void afterReturning( Object returnValue, Method method, Object[] args, Object target )
-        throws Throwable {
-
+    @Override
+    public void afterReturning( Object returnValue, Method method, Object[] args, Object target ) throws Throwable {
         cache.forgetAll();
     }
 
-    public ResultCache getCache() {
-        return cache;
-    }
 
+    /**
+     * Set the value of the cache property.
+     *
+     * @param  cache  the new value
+     */
     public void setCache( ResultCache cache ) {
         this.cache = cache;
     }
