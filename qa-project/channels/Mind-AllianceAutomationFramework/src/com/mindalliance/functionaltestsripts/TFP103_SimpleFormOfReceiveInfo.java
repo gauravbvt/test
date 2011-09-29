8fa81f73f6a4fe5069494a1bd395b8418b43c6a2
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2011-09-29 04:52:37 +0000

    Added and Modified by priyanka
    git-svn-id: https://wush.net/svn/mindalliance@3162 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ qa-project/channels/Mind-AllianceAutomationFramework/src/com/mindalliance/functionaltestsripts/TFP103_SimpleFormOfReceiveInfo.java
@@ -0,0 +1,142 @@
+package com.mindalliance.functionaltestsripts;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+
+import com.mindalliance.globallibrary.ApplicationFunctionLibrary;
+import com.mindalliance.globallibrary.GenericFunctionLibrary;
+import com.mindalliance.globallibrary.GlobalVariables;
+import com.mindalliance.globallibrary.LogFunctions;
+import com.mindalliance.globallibrary.ReportFunctions;
+
+public class TFP103_SimpleFormOfReceiveInfo 
+{
+	public TFP103_SimpleFormOfReceiveInfo(){
+    	try {
+    		
+    		GlobalVariables.sTestCaseId = "TFP103_SimpleFormOfReceiveInfo";
+			GlobalVariables.sDescription = "Testcase: " + GlobalVariables.sTestCaseId + " execution started";
+			LogFunctions.writeLogs(GlobalVariables.sDescription);
+			System.out.println(GlobalVariables.sDescription);
+			// Call login()
+			GlobalVariables.bIsSuccess = ApplicationFunctionLibrary.login();
+			if (GlobalVariables.bIsSuccess) {
+				
+				// Click on 'Information Sharing Model' link
+				GlobalVariables.iStepNo++ ;
+				GlobalVariables.sDescription = "Navigated to Information Sharing Model";
+				GlobalVariables.oDriver.findElement(By.linkText(GlobalVariables.viewElements.get("informationSharingModel"))).click();
+				// Write Results
+				LogFunctions.writeLogs(GlobalVariables.sDescription);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(2000);
+
+				// Click 'Add new Segment' option under 'Actions' pop up menu and enter the details
+				GlobalVariables.iStepNo++ ;
+				GlobalVariables.sDescription = "New segment added";
+				ApplicationFunctionLibrary.addSegment(GlobalVariables.testData.get("Segment For Receive Send Simple Form"), "New");
+				// Write Results
+				LogFunctions.writeLogs(GlobalVariables.sDescription);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(2000);
+				
+				// Stretch Up Task Details
+				GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.plan.get("sXpathStretchUpShrinkBack"))).click();
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(2000);
+				// Click 'Add info received' Link. 
+				GlobalVariables.iStepNo++ ;
+				GlobalVariables.sDescription = "'Add Info Received' Link Clicked";
+				GlobalVariables.oDriver.findElement(By.linkText("Add info received")).click();
+				// Write Results
+				LogFunctions.writeLogs(GlobalVariables.sDescription);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(2000);
+
+				// Click on Show Simple form link
+				GlobalVariables.iStepNo++ ;
+				GlobalVariables.sDescription = "Simple form";
+				GlobalVariables.oElement=GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.plan.get("sXpathShowAdvanceSimpleFormReceiveInfo")));
+				if(GlobalVariables.oElement.getText().equals(GlobalVariables.viewElements.get("showSimpleForm"))){
+					// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+							GlobalVariables.sBlank, GlobalVariables.sBlank);
+				}
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(3000);
+				
+				// Remove Segment
+				ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathActionsPopUpMenu"),GlobalVariables.viewElements.get("removeThisSegment"));
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(2000);
+				// Get a handle to the open alert, prompt or confirmation
+				Alert alert = GlobalVariables.oDriver.switchTo().alert();
+				// And acknowledge the alert (equivalent to clicking "OK")
+				alert.accept();
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(2000);
+				
+				// Call Logout
+				GlobalVariables.iStepNo++;
+				GlobalVariables.sDescription="Logout Successful";
+				ApplicationFunctionLibrary.logout();
+				// Write Results
+				LogFunctions.writeLogs(GlobalVariables.sDescription);
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+						GlobalVariables.sBlank, GlobalVariables.sBlank);
+				// WebElement Synchronization
+				Thread.currentThread();
+				Thread.sleep(2000);
+				
+				LogFunctions.writeLogs("Testcase: " + GlobalVariables.sTestCaseId + " execution completed");
+				System.out.println("Testcase: " + GlobalVariables.sTestCaseId + " execution completed");
+				
+			}
+			else
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+						GlobalVariables.sBlank, GlobalVariables.sBlank);
+    	} 
+    	catch (Exception e) {
+    		if (GlobalVariables.oDriver.getTitle().equals(GlobalVariables.sInternalErrorPageTitle)) {
+    			LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+    					e.getMessage(),GlobalVariables.sErrorLogSubDirectoryPath + "\\" + GlobalVariables.sTestCaseId + ".logs");
+    			GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.id("stackTrace"));
+    			LogFunctions.writeErrorLogs(GlobalVariables.oElement.getText());
+    			ApplicationFunctionLibrary.logout();
+    		}
+    		else {
+    			LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+    					e.getMessage(),GlobalVariables.sBlank);
+    			ApplicationFunctionLibrary.logout();	
+    		}
+    		System.out.println("Testcase: " + GlobalVariables.sTestCaseId + " execution failed");
+    	}
+	}
+	public static void main(String args[]) {
+		try {
+			GenericFunctionLibrary.initializeTestData();
+			GenericFunctionLibrary.loadObjectRepository();
+			new TFP103_SimpleFormOfReceiveInfo();
+			GenericFunctionLibrary.tearDownTestData();
+			ReportFunctions.generateAutomationReport();
+		} 
+		catch (Exception oException) {
+			// TODO Auto-generated catch block
+			oException.printStackTrace();
+		}
+	}
+}
