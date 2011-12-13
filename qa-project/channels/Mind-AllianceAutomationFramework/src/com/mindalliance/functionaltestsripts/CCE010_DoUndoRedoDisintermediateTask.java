e9cf0794653afb0f34f8ce0e5477418461c3479e
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2011-12-13 12:06:51 +0000

    Added by priyanka
    git-svn-id: https://wush.net/svn/mindalliance@3546 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ qa-project/channels/Mind-AllianceAutomationFramework/src/com/mindalliance/functionaltestsripts/CCE010_DoUndoRedoDisintermediateTask.java
@@ -0,0 +1,355 @@
+package com.mindalliance.functionaltestsripts;
+
+import java.util.List;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import com.mindalliance.globallibrary.ApplicationFunctionLibrary;
+import com.mindalliance.globallibrary.GenericFunctionLibrary;
+import com.mindalliance.globallibrary.GlobalVariables;
+import com.mindalliance.globallibrary.LogFunctions;
+import com.mindalliance.globallibrary.ReportFunctions;
+
+public class CCE010_DoUndoRedoDisintermediateTask 
+{
+	public CCE010_DoUndoRedoDisintermediateTask() {
+		try {
+			GlobalVariables.sTestCaseId = "CCE010_DoUndoRedoDisintermediateTask";
+			GlobalVariables.sDescription = "Testcase: " + GlobalVariables.sTestCaseId + " execution started";
+			LogFunctions.writeLogs(GlobalVariables.sDescription);
+			System.out.println(GlobalVariables.sDescription);
+			// Call login()
+			GlobalVariables.bIsSuccess = ApplicationFunctionLibrary.login();
+				if (GlobalVariables.bIsSuccess) {
+
+					// Click on 'Information Sharing Model' link
+					GlobalVariables.iStepNo++ ;
+					GlobalVariables.sDescription = "Navigated to Information Sharing Model";
+					GlobalVariables.oDriver.findElement(By.linkText(GlobalVariables.viewElements.get("informationSharingModel"))).click();
+					// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+							GlobalVariables.sBlank, GlobalVariables.sBlank);
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					
+					// Click on 'Add new segment' option under 'Actions' pop up menu
+					GlobalVariables.iStepNo++ ;
+					GlobalVariables.sDescription = "New segment added";
+					ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathActionsPopUpMenu"),GlobalVariables.viewElements.get("addNewSegment"));
+					// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+							GlobalVariables.sBlank, GlobalVariables.sBlank);
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+			
+					// Enter the details
+					GlobalVariables.iStepNo++ ;
+					GlobalVariables.sDescription = "New segment updated";
+					GlobalVariables.oDriver.findElement(By.name("sg-editor:content:mo:aspect:name")).clear();
+					GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.name("sg-editor:content:mo:aspect:name"));
+					GlobalVariables.oElement.sendKeys(GlobalVariables.testData.get("Segment For Do Undo Redo Disintermediate Task"));
+					// Click on 'done' button
+					GlobalVariables.oDriver.findElement(By.className("close")).click();
+					// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+							GlobalVariables.sBlank, GlobalVariables.sBlank);
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(5000);
+
+					// Update the Information of the default task
+					GlobalVariables.iStepNo++ ;
+					GlobalVariables.sDescription = "Task updated";
+					GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.plan.get("sXpathDoingSomeThingLink"))).click();
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(5000);
+					// Add details for New Task
+					GlobalVariables.oDriver.findElement(By.name("segment:part:task")).clear();
+					GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.name("segment:part:task"));
+					GlobalVariables.oElement.sendKeys(GlobalVariables.testData.get("Task Sender"));
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					GlobalVariables.oElement.sendKeys(Keys.TAB);
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					GlobalVariables.oElement.sendKeys(Keys.TAB);
+				    // WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					// Click on hide details from action pop-menu bar
+					ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathTaskShowMenu"),GlobalVariables.viewElements.get("hideDetails"));
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+							GlobalVariables.sBlank, GlobalVariables.sBlank);
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					
+					// Click on legend for maximize the graph
+					GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.plan.get("sXpathStretchUpShrinkBack"))).click();
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(1000);
+					// Add info sent flow
+					GlobalVariables.iStepNo++ ;
+					GlobalVariables.sDescription = "Sent Flow added";
+					GlobalVariables.oDriver.findElement(By.linkText("Add info sent")).click();
+					// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+							GlobalVariables.sBlank, GlobalVariables.sBlank);
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					
+					// Create Flow between respective nodes
+					GlobalVariables.iStepNo++ ;
+					GlobalVariables.sDescription = "Flow created between nodes";
+					GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.name("segment:sends:flows-div:flows:0:flow:name"));
+					GlobalVariables.oElement.sendKeys(GlobalVariables.testData.get("This is a Flow"));
+					GlobalVariables.oElement.sendKeys(Keys.TAB);
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					// To
+					GlobalVariables.oDropDown =new Select(GlobalVariables.oDriver.findElement(By.name("segment:sends:flows-div:flows:0:flow:other:firstChoice")));
+					List<WebElement> options = GlobalVariables.oDropDown.getOptions();
+				    for(WebElement option : options) {
+				    	if(GlobalVariables.viewElements.get("other").equals(option.getText())){
+				    			option.setSelected();
+				    			break;
+				    	}
+				    }
+				    // WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(2000);
+					GlobalVariables.oDriver.findElement(By.name("segment:sends:flows-div:flows:0:flow:other:secondChoice:secondChoice-input")).click();
+					GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.name("segment:sends:flows-div:flows:0:flow:other:secondChoice:secondChoice-input"));
+					for (int i = 0; i <= 50; i++)
+						GlobalVariables.oElement.sendKeys(Keys.BACK_SPACE);
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(10000);
+					GlobalVariables.oElement.sendKeys(GlobalVariables.testData.get("Task Receiver"));
+					 // WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(10000);
+					GlobalVariables.oElement.sendKeys(Keys.TAB);
+					// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+							GlobalVariables.sBlank, GlobalVariables.sBlank);
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(2000);
+					
+					// Create Intermediate Flow
+					GlobalVariables.iStepNo++ ;
+					GlobalVariables.sDescription = "Intermediate Flow created";
+					ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathSendInfoActionMenu"),GlobalVariables.viewElements.get("addIntermediate"));
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+							GlobalVariables.sBlank, GlobalVariables.sBlank);
+					// Click on Legend to minimize the information flow details
+					GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.plan.get("sXpathStretchUpShrinkBack"))).click();
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					
+					// Click on 'About plan segment' option under 'Show' pop up menu
+					GlobalVariables.iStepNo++ ;
+					GlobalVariables.sDescription = "Task selected to disintermediate";
+					ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathShowPopUpMenu"),GlobalVariables.viewElements.get("aboutPlanSegment"));
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(5000);
+					// Click on 'task mover' option under 'Shows' pop up menu
+					ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathAbtPlanSegShowMenu"),GlobalVariables.viewElements.get("taskMover"));
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					// Select the task to disintermediate
+					GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.className("data-table"));
+					List<WebElement> tds = GlobalVariables.oElement.findElements(By.tagName("td"));
+					for (WebElement td: tds){
+						if (td.getText().equals("doing something")){
+							td.findElement(By.linkText("doing something")).click();
+							break;
+						}
+					}
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					// Click on 'done' button
+					GlobalVariables.oDriver.findElement(By.className("close")).click();
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.plan.get("sXpathDoingSomeThingLink"))).click();
+					// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+							GlobalVariables.sBlank, GlobalVariables.sBlank);
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					
+					// Click on 'Disintermediate' option under 'Actions' pop up menu of task section
+					GlobalVariables.iStepNo++ ;
+					GlobalVariables.sDescription = "Task disintermediated";
+					GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.plan.get("sXpathStretchUpShrinkBack"))).click();
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathTaskActionsMenu"),GlobalVariables.viewElements.get("disintermediate"));
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					GlobalVariables.oDriver.findElement(By.xpath(GlobalVariables.plan.get("sXpathStretchUpShrinkBack"))).click();
+					// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+							GlobalVariables.sBlank, GlobalVariables.sBlank);
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(5000);
+					
+					// Click on 'Undo disintermediate' option under 'Actions' pop up menu
+					GlobalVariables.iStepNo++ ;
+					GlobalVariables.sDescription = "Undo disintermediate done";
+					ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathActionsPopUpMenu"),GlobalVariables.viewElements.get("undoDisintermediate"));
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					// ASSERTION:When clicked on 'Undo disintermediate' option, the disintermediated task should be intermediate again
+					GlobalVariables.bIsSuccess = Boolean.FALSE;
+					GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.name("flow-map"));
+					List<WebElement> areas = GlobalVariables.oElement.findElements(By.tagName("area"));
+					for(WebElement area:areas){
+					if(area.getAttribute("id").equals("node1"))
+						 GlobalVariables.bIsSuccess = Boolean.TRUE;
+						 break;
+				    }
+					if (GlobalVariables.bIsSuccess == Boolean.TRUE) {
+					// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+							GlobalVariables.sBlank, GlobalVariables.sBlank);
+					}
+					else{
+				    	// Write Results
+						LogFunctions.writeLogs(GlobalVariables.sDescription + "" + GlobalVariables.sFailed);
+						LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+								GlobalVariables.sBlank, GlobalVariables.sVerifyError);
+				    }
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(5000);
+
+					// Click on 'Redo disintermediate' option under 'Actions' pop up menu
+					GlobalVariables.iStepNo++ ;
+					GlobalVariables.sDescription = "Redo disintermediate done";
+					ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathActionsPopUpMenu"),GlobalVariables.viewElements.get("redoDisintermediate"));
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(5000);
+					// Assertion: When clicked on 'Redo disintermediate' option, the task should be disntermediate again
+					GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.xpath("//span[@class='menubar']/span[3]/span/ul"));
+					List<WebElement> list = GlobalVariables.oElement.findElements(By.tagName("li"));
+					for (WebElement li: list){
+						if (li.getValue().equals("Redo")){
+							// Write Results
+							LogFunctions.writeLogs(GlobalVariables.sDescription);
+							LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+									GlobalVariables.sBlank, GlobalVariables.sBlank);
+							break;
+						}
+						else{
+							GlobalVariables.sVerifyError ="Verification Failed "+"Expected 'Redo disintermediate' "+" Actual " + li.getValue();
+					    	// Write Results
+							LogFunctions.writeLogs(GlobalVariables.sDescription + "" + GlobalVariables.sFailed);
+							LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+									GlobalVariables.sBlank, GlobalVariables.sVerifyError);
+							break;
+					    }
+					}
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					GlobalVariables.oDriver.navigate().refresh();
+					ApplicationFunctionLibrary.MouseOverAndClick(GlobalVariables.plan.get("sXpathActionsPopUpMenu"),GlobalVariables.viewElements.get("removeThisSegment"));
+					// Get a handle to the open alert, prompt or confirmation
+					Alert alert = GlobalVariables.oDriver.switchTo().alert();
+					// And acknowledge the alert (equivalent to clicking "OK")
+					alert.accept();
+					// WebElement Synchronization
+					Thread.currentThread();
+					Thread.sleep(3000);
+					
+					// Call logout()
+					GlobalVariables.iStepNo++ ;
+					GlobalVariables.sDescription = "Logout is successful";
+					ApplicationFunctionLibrary.logout();
+					// Write Results
+					LogFunctions.writeLogs(GlobalVariables.sDescription);
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sPassed, 
+							GlobalVariables.sBlank, GlobalVariables.sBlank);
+					
+					LogFunctions.writeLogs("Testcase: " + GlobalVariables.sTestCaseId + " execution completed");
+					System.out.println("Testcase: " + GlobalVariables.sTestCaseId + " execution completed");
+				}
+				else
+					LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+							GlobalVariables.sBlank, GlobalVariables.sBlank);
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+			if (GlobalVariables.oDriver.getTitle().equals(GlobalVariables.sInternalErrorPageTitle)) {
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+						e.getMessage(),GlobalVariables.sErrorLogSubDirectoryPath + "\\" + GlobalVariables.sTestCaseId + ".logs");
+				GlobalVariables.oElement = GlobalVariables.oDriver.findElement(By.id("stackTrace"));
+				LogFunctions.writeErrorLogs(GlobalVariables.oElement.getText());
+				ApplicationFunctionLibrary.logout();
+			}
+			else {
+				LogFunctions.writeResults(GlobalVariables.sTestCaseId, GlobalVariables.iStepNo, GlobalVariables.sDescription, GlobalVariables.sFailed, 
+						e.getMessage(),GlobalVariables.sBlank);
+				ApplicationFunctionLibrary.logout();	
+			}
+			System.out.println("Testcase: " + GlobalVariables.sTestCaseId + " execution failed");
+		}
+	}
+	public static void main(String args[]) {
+		try {
+			GenericFunctionLibrary.initializeTestData();
+			GenericFunctionLibrary.loadObjectRepository();
+			new CCE010_DoUndoRedoDisintermediateTask();
+			GenericFunctionLibrary.tearDownTestData();
+			ReportFunctions.generateAutomationReport();
+		} 
+		catch (Exception oException) {
+			// TODO Auto-generated catch block
+			oException.printStackTrace();
+		}
+	}
+}
