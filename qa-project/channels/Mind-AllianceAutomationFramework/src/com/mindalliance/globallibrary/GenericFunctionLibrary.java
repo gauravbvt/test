f8529d90d46aa70ce33001e69089f938fce0dbe2
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2011-12-14 11:19:23 +0000

    Modified by priyanka
    git-svn-id: https://wush.net/svn/mindalliance@3562 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- qa-project/channels/Mind-AllianceAutomationFramework/src/com/mindalliance/globallibrary/GenericFunctionLibrary.java
+++ qa-project/channels/Mind-AllianceAutomationFramework/src/com/mindalliance/globallibrary/GenericFunctionLibrary.java
@@ -392,6 +392,7 @@ public class GenericFunctionLibrary {
 			GlobalVariables.assertion.put("sXpathAssertionInfoNameRequirement",oXmlElePlanPage.getElementsByTagName("xPathAssertionInfoNameRequirement").item(0).getChildNodes().item(0).getNodeValue());
 			GlobalVariables.assertion.put("sXpathAssertionInfoTagsRequirement",oXmlElePlanPage.getElementsByTagName("xPathAssertionInfoTagsRequirement").item(0).getChildNodes().item(0).getNodeValue());
 			// View Elements
+			GlobalVariables.viewElements.put("newPlan",oXmlEleViewElements.getElementsByTagName("newPlan").item(0).getChildNodes().item(0).getNodeValue());
 			GlobalVariables.viewElements.put("versionAssertion",oXmlEleViewElements.getElementsByTagName("versionAssertion").item(0).getChildNodes().item(0).getNodeValue());
 			GlobalVariables.viewElements.put("undoRemoveGoal",oXmlEleViewElements.getElementsByTagName("undoRemoveGoal").item(0).getChildNodes().item(0).getNodeValue());
 			GlobalVariables.viewElements.put("elements",oXmlEleViewElements.getElementsByTagName("elements").item(0).getChildNodes().item(0).getNodeValue());
