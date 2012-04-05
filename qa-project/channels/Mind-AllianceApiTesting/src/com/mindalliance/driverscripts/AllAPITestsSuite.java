b5d74345146201a6bf3a19204f95342025823a10
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2012-04-05 10:52:42 +0000

    git-svn-id: https://wush.net/svn/mindalliance@3883 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- qa-project/channels/Mind-AllianceApiTesting/src/com/mindalliance/driverscripts/AllAPITestsSuite.java
+++ qa-project/channels/Mind-AllianceApiTesting/src/com/mindalliance/driverscripts/AllAPITestsSuite.java
@@ -1,6 +1,11 @@
 package com.mindalliance.driverscripts;
 
 import com.mindalliance.testscripts.REST001_ISPPlans;
+import com.mindalliance.testscripts.REST002_ISPProcedures;
+import com.mindalliance.testscripts.REST003_ISPAgents;
+import com.mindalliance.testscripts.REST004_ISPIssues;
+import com.mindalliance.testscripts.REST005_ISPScope;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -14,6 +19,10 @@ public class AllAPITestsSuite extends TestCase{
 		// Create a test suite which includes all the API tests
 		TestSuite suite = new TestSuite("AllAPITests");
 		suite.addTest(new TestSuite(REST001_ISPPlans.class));
+		suite.addTest(new TestSuite(REST002_ISPProcedures.class));
+		suite.addTest(new TestSuite(REST003_ISPAgents.class));
+		suite.addTest(new TestSuite(REST004_ISPIssues.class));
+		suite.addTest(new TestSuite(REST005_ISPScope.class));
 		return suite;
 	}
 }
