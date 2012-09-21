30ba0956835f565a3c7bb82766a4bc2a47eca314
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2012-09-21 07:30:11 +0000

    git-svn-id: https://wush.net/svn/mindalliance@4307 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/driverscripts/Home.java
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/driverscripts/Home.java
@@ -8,12 +8,11 @@ import com.mindalliance.configuration.ElementController;
 import com.mindalliance.configuration.GlobalVariables;
 import com.mindalliance.configuration.UIAutomationException;
 import com.mindalliance.uitestscripts.*;
+
 /**
  * Class Home page creates a test suite  
- * @return the test suite
- * @throws SAXException 
- * @throws IOException 
- * @throws ParserConfigurationException 
+ * @author afour
+ *
  */
 public class Home {
 	public static Test suite() throws UIAutomationException{	  
@@ -26,6 +25,11 @@ public class Home {
 		suite.addTestSuite(MAP0003_AddNewUserToPlan.class);
 		suite.addTestSuite(MAP0004_DeleteUser.class);
 		suite.addTestSuite(MAP0005_DeletePlan.class);
+		
+		// date:9/20/2012
+		suite.addTestSuite(MAP0006_addOrganizations.class);
+		suite.addTestSuite(MAP0007_addOrganizationsDetails.class);
+		suite.addTestSuite(MAP0008_removeExpectation.class);
 	
 		suite.addTestSuite(MAC0001_UndoAddSegment.class);
 		suite.addTestSuite(MAC0002_UndoRemoveThisSegment.class);
@@ -46,7 +50,7 @@ public class Home {
 		suite.addTestSuite(MAV0011_viewAllSecrecyclassificationsForm.class);
 		suite.addTestSuite(MAV0012_viewAllOrganizationForm.class);
 	
-//		// Date: 28/8/2012
+		// Date: 28/8/2012
 		suite.addTestSuite(MAV0016_viewAllWhoForm.class);
 		suite.addTestSuite(MAV0017_viewAllIssueForm.class);
 		suite.addTestSuite(MAV0019_viewAllIndexForm.class);
@@ -59,11 +63,11 @@ public class Home {
 		suite.addTestSuite(MAV0025_closeAboutPlanWindow.class);
 		suite.addTestSuite(MAV0026_viewAboutPlanSegmentWindow.class);
 		
-//		// Date :30/8/2012
+		// Date :30/8/2012
 		suite.addTestSuite(MAV0028_viewGoalsForm.class);
 		suite.addTestSuite(MAV0029_viewOrganizationsForm.class);
-//			
-//		// date :31/8/2012
+			
+		// date :31/8/2012
 		suite.addTestSuite(MAV0005_logoutOnAdminPage.class);
 		suite.addTestSuite(MAV0007_logoutOnPlanPage.class);
 		suite.addTestSuite(MAV0030_viewMapWindow.class);
@@ -72,14 +76,14 @@ public class Home {
 		suite.addTestSuite(MAV0034_viewSurveryWindow.class);
 		suite.addTestSuite(MAV0035_closeSurveysWindow.class);
 
-//		// date :3/9/2012
+		// date :3/9/2012
 		suite.addTestSuite(MAV0066_viewAllSegmentsActionList.class);
 		suite.addTestSuite(MAV0067_closeAllSegmentWindow.class);
 		suite.addTestSuite(MAV0075_undoAddNewIssueUnderAction.class);
 		suite.addTestSuite(MAV0076_redoAddNewIssueUnderAction.class);
 		suite.addTestSuite(MAV0080_addNewTask.class);
-//		
-//		// date : 9/4/2012
+		
+		// date : 9/4/2012
 		suite.addTestSuite(MAV0081_hideDetails.class);
 		suite.addTestSuite(MAV0082_TaskDetails.class);
 		suite.addTestSuite(MAV0083_addAssignment.class);
@@ -91,8 +95,8 @@ public class Home {
 		suite.addTestSuite(MAV0089_viewActionsPopup.class);
 		suite.addTestSuite(MAV0090_addNewIssue.class);
 		suite.addTestSuite(MAV0091_closeNewIssue.class);
-//
-//		// date :9/5/2012
+
+		// date :9/5/2012
 		suite.addTestSuite(MAV0092_addNewSegment.class);
 		suite.addTestSuite(MAV0093_closeNewSegment.class);
 		suite.addTestSuite(MAV0098_removeSegmentUnderAction.class);
@@ -102,7 +106,7 @@ public class Home {
 		suite.addTestSuite(MAV0103_viewHelpFormWithFeedback.class);
 	
 		
-//		// date : 9/6/2012
+		// date : 9/6/2012
 		suite.addTestSuite(MAV0107_viewPresence.class);
 		suite.addTestSuite(MAV0109_hideInactiveUsers.class);
 		suite.addTestSuite(MAV0110_viewActivitiesAllUsers.class);
@@ -114,7 +118,7 @@ public class Home {
 		suite.addTestSuite(MAV0116_showSent.class);
 		suite.addTestSuite(MAV0117_showReceived.class);
 			
-//		// date : 9/7/2012
+		// date : 9/7/2012
 		suite.addTestSuite(MAV0124_sendMessage.class);
 		suite.addTestSuite(MAV0125_copyFlow.class);
 		suite.addTestSuite(MAV0126_copyNeed.class);
@@ -123,7 +127,7 @@ public class Home {
 		suite.addTestSuite(MAV0150_viewSocialPanelMessage.class);
 		suite.addTestSuite(MAV0151_viewSocialPanelShowSentMessage.class);
 		
-//		// date:9/10/2012
+		// date:9/10/2012
 		suite.addTestSuite(MAV0152_viewSocialPanelShowReceivedMessage.class);
 		suite.addTestSuite(MAV0153_viewSocialPanelHideBroadCast.class);
 		suite.addTestSuite(MAV0154_viewSocialPanelShowAllMessages.class);
@@ -135,7 +139,7 @@ public class Home {
 		suite.addTestSuite(MAV0166_AdvanceFormOfTask.class);
 		suite.addTestSuite(MAV0167_SimpleFormOfReceiveInfo.class);		
 		
-//		// date :9/11/2012
+		// date :9/11/2012
 		suite.addTestSuite(MAV0168_AdvanceFormOfReceivenfo.class);
 		suite.addTestSuite(MAV0169_SimpleFormOfSendInfo.class);
 		suite.addTestSuite(MAV0170_AdvanceFormOfSendInfo.class);
@@ -159,7 +163,7 @@ public class Home {
 		suite.addTestSuite(MAV0189_GoalsLink.class);
 		suite.addTestSuite(MAV0190_GoalsOption.class);
 		
-		// date: 9/13/2012
+//		 date: 9/13/2012
 		suite.addTestSuite(MAV0191_GoalsOptionUnchecked.class);
 		suite.addTestSuite(MAV0193_TaskAttachOptions.class);
 		suite.addTestSuite(MAV0194_TaskNewIssue.class);
@@ -187,12 +191,35 @@ public class Home {
 		suite.addTestSuite(MAV0209_AddInfoReceiveSendAttachOptions.class);
 		suite.addTestSuite(MAV0210_AddInfoReceiveSendNewIssue.class);
 		suite.addTestSuite(MAV0211_AddInfoReceiveSendIssueTypeOption.class);
-		
 		suite.addTestSuite(MAV0212_AddInfoReceiveSendIssueSeverity.class);
 		suite.addTestSuite(MAV0213_AttachReceiveSentAttachOption.class);
 		
+		// date: 9/18/2012
+		suite.addTestSuite(MAV0215_AddInfoReceiveShowPopUpMenu.class);
+		suite.addTestSuite(MAV0216_AddInfoReceiveActionPopUpMenu.class);
+		suite.addTestSuite(MAV0217_AddInfoSentShowPopUpMenu.class);
+		suite.addTestSuite(MAV0218_AddInfoSentActionPopUpMenu.class);
+		suite.addTestSuite(MAV0219_AddInfoReceiveShowHideDetails.class);
 		
+		// date : 9/19/2012
+		suite.addTestSuite(MAV0220_AddInfoSentHideShowDetails.class);
+		suite.addTestSuite(MAV0221_AddInfoReceiveSentSendMessage.class);
+		suite.addTestSuite(MAV0222_AddInfoReceiveShowElements.class);
+		suite.addTestSuite(MAV0223_AddInfoSentCopyCapability.class);
+		suite.addTestSuite(MAV0224_AddInfoReceiveCopyNeed.class);
+		suite.addTestSuite(MAV0225_AddInfoReceiveRemoveInfoNeeds.class);
+		suite.addTestSuite(MAV0226_AddInfoSentRemoveSharingCapability.class);
 		
+		// date : 9/20/2012
+		suite.addTestSuite(MAV0243_ShowRequirements.class);
+		suite.addTestSuite(MAV0245_ShowRequirementDefinition.class);
+		suite.addTestSuite(MAV0246_ShowRequirementNetwork.class);
+		suite.addTestSuite(MAV0247_NewRequirement.class);
+		suite.addTestSuite(MAV0248_RemoveRequirement.class);
+		suite.addTestSuite(MAV0249_EditRequirement.class);
+		suite.addTestSuite(MAV0250_ViewAllSituationRequirement.class);
+		
+				
 		return suite;
 	}
 	
