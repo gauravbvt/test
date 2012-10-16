8cab1297529146f9103eca555ae706e3b1587c84
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2012-10-16 08:21:51 +0000

    git-svn-id: https://wush.net/svn/mindalliance@4452 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/uitestscripts/MAV0092_addNewSegment.java
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/uitestscripts/MAV0092_addNewSegment.java
@@ -19,6 +19,7 @@ import com.mindalliance.configuration.BrowserController;
 import com.mindalliance.configuration.Configuration;
 import com.mindalliance.configuration.ElementController;
 import com.mindalliance.configuration.GlobalVariables;
+import com.mindalliance.configuration.LogFunctions;
 import com.mindalliance.configuration.Reporting;
 import com.mindalliance.configuration.UIAutomationException;
 import com.mindalliance.pages.HeaderController;
@@ -35,11 +36,27 @@ import junit.framework.TestCase;
  * @author afour
  */
 public class MAV0092_addNewSegment extends TestCase{
-
 	public Hashtable<String, String> testData;
+	public String testCaseId="MAV0092_addNewSegment";
+	public String description=null;
+	public int stepNo=1;
+	public String passed="Pass";
+	public String failed="Fail";
+	public String blank=""; 
+	public String exception="";
 	
+	public MAV0092_addNewSegment() throws UIAutomationException{
+		setUp();
+		testMAV0092_addNewSegment();
+		tearDown();
+	}
+	/**
+	 * This method will initialize the setup required for every test case
+	 * @throws UIAutomationException 
+	 * @see junit.framework.TestCase#setUp()
+	 */
 	@Before
-	protected void setUp(){
+	protected void setUp() throws UIAutomationException{
 		try{
 			if (GlobalVariables.configuration == null){
 					GlobalVariables.configuration = Configuration.getConfigurationObject();
@@ -48,54 +65,109 @@ public class MAV0092_addNewSegment extends TestCase{
 				new ElementController();
 			}
 			
-			//Creates Browser instance
-			BrowserController browserController= new BrowserController();
-			browserController.initializeDriver();
-			
-			//Loads Test data 
+			// Loads Test Data
+			description = "Testcase: " + testCaseId + " execution started";
 			loadTestData();
+			// Write log			
+			LogFunctions.writeLogs(description);
+						
+			// Creates Browser instance
+			description="Browser initialized";
+			BrowserController browserController= new BrowserController();
+			browserController.initializeDriver("Mozilla Firefox");			
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
 		}
 		catch(UIAutomationException ue){
-			Assert.fail("Unable to initialize the driver "+ue.getErrorMessage());
+			stepNo++;
+			Assert.fail("Unable to initialize the driver"+ue.getErrorMessage());
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo,exception,failed, ue.getErrorMessage(), blank);
 		}
 	}
+	
+	/**
+	 * This method verifies segment is added to plan
+	 * @throws UIAutomationException
+	 */
 	
 	@Test
 	public void testMAV0092_addNewSegment() throws UIAutomationException {
 		try {
 			// Enter URL of Channels
+			stepNo++;
+			description="URL Entered";	
 			BrowserController browserController=new BrowserController();
 		    browserController.enterURL(testData.get("ChannelsURL"),testData.get("Title"));
+		    // Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
 		 		    
 			// Login page
+		    stepNo++;
+			description="Login successful";	
 		    LoginPage loginPage = new LoginPage();
 		    loginPage.Login(GlobalVariables.configuration.getConfigData().get("UserName"),GlobalVariables.configuration.getConfigData().get("PassWord"));
-				
+		    // Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+		 		
 			// Plan Page
+		    stepNo++;
+ 	 		description="Navigated to plan page";
 			HomePage homePage=new HomePage();
 			homePage.clickCollaborationPlanLink();	
-			
+			 // Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+		 	
 			// Close Plan Map window
+			stepNo++;
+			description="Plan Map window closed";
 			PlanPage planPage=new PlanPage();
 			planPage.closePlanMap();
-					
+			 // Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+		 			
 			// Click Actions pop up menu and Add New Segment
+			stepNo++;
+			description="New Segment is added";
 			planPage.clickPopupMenu(testData.get("Actions"));
 			planPage.clickSubmenu(testData.get("AddNewSegment"));	
-					
+			 // Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+		 			
 			// Close 'Segment' window
+			stepNo++;
+			description="Segment window closed";
 			planPage.closeSegmentWindow();
-					
+			 // Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+		 			
 			// Sign Out from 'Plan' page
+			stepNo++;
+			description="Logout successful";
 			HeaderController headerController=new HeaderController();
 			headerController.signOutPlan();
-
+			 // Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+		 	
 		} catch (UIAutomationException ue) {
-			Reporting.getScreenShot("MAV0092_addNewSegment");
+			Reporting.getScreenShot(testCaseId);
 			
 			// Sign out from plan page
+			stepNo++;
 			HeaderController headerController=new HeaderController();
 			headerController.signOutPlan();
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo,exception,failed, ue.getErrorMessage(), blank);
 				
 			// Quits the Browser
 			GlobalVariables.configuration.getWebDriver().quit();
@@ -103,7 +175,20 @@ public class MAV0092_addNewSegment extends TestCase{
 		}
 		
 	}
+	/**
+	 * (non-Javadoc)
+	 * This method will perform cleanup actions
+	 * @see junit.framework.TestCase#tearDown()
+	*/	
 	
+	@After
+	protected void tearDown(){
+		if(GlobalVariables.configuration.getWebDriver()!=null){
+			GlobalVariables.configuration.getWebDriver().quit();
+		}
+		String endTime=LogFunctions.getDateTime();
+		GlobalVariables.configuration.setEndtime(endTime);
+	}
 	/**
      * Loads Test Data for MAV0092_addNewSegment.
      * @throws UIAutomationException
@@ -111,6 +196,9 @@ public class MAV0092_addNewSegment extends TestCase{
 	public void loadTestData() throws UIAutomationException
 	{		
 		try{
+			String startTime=LogFunctions.getDateTime();
+			GlobalVariables.configuration.setStartTime(startTime);
+			
 			testData=new Hashtable<String,String>();
 			File currentDir=new File(".");
 			
@@ -140,18 +228,5 @@ public class MAV0092_addNewSegment extends TestCase{
 			throw new UIAutomationException("File MAV0092_addNewSegment can not be parsed.");
 		}
 			
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see junit.framework.TestCase#tearDown()
-	 */
-	
-	@After
-	protected void tearDown(){
-		if(GlobalVariables.configuration.getWebDriver()!=null){
-			GlobalVariables.configuration.getWebDriver().quit();
-		}
 	}
 }
--- qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/uitestscripts/MAV0093_closeNewSegment.java
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/uitestscripts/MAV0093_closeNewSegment.java
@@ -19,6 +19,7 @@ import com.mindalliance.configuration.BrowserController;
 import com.mindalliance.configuration.Configuration;
 import com.mindalliance.configuration.ElementController;
 import com.mindalliance.configuration.GlobalVariables;
+import com.mindalliance.configuration.LogFunctions;
 import com.mindalliance.configuration.Reporting;
 import com.mindalliance.configuration.UIAutomationException;
 import com.mindalliance.pages.HeaderController;
@@ -35,9 +36,26 @@ import junit.framework.TestCase;
  */
 public class MAV0093_closeNewSegment extends TestCase{
 	public Hashtable<String, String> testData;
+	public String testCaseId="MAV0093_closeNewSegment";
+	public String description=null;
+	public int stepNo=1;
+	public String passed="Pass";
+	public String failed="Fail";
+	public String blank=""; 
+	public String exception="";
 	
+	public MAV0093_closeNewSegment() throws UIAutomationException{
+		setUp();
+		testMAV0093_closeNewSegment();
+		tearDown();
+	}
+	/**
+	 * This method will initialize the setup required for every test case
+	 * @throws UIAutomationException 
+	 * @see junit.framework.TestCase#setUp()
+	 */
 	@Before
-	protected void setUp(){
+	protected void setUp() throws UIAutomationException{
 		try{
 			if (GlobalVariables.configuration == null){
 					GlobalVariables.configuration = Configuration.getConfigurationObject();
@@ -46,54 +64,109 @@ public class MAV0093_closeNewSegment extends TestCase{
 				new ElementController();
 			}
 			
-			//Creates Browser instance
-			BrowserController browserController= new BrowserController();
-			browserController.initializeDriver();
-			
-			//Loads Test data 
+			// Loads Test Data
+			description = "Testcase: " + testCaseId + " execution started";
 			loadTestData();
+			// Write log			
+			LogFunctions.writeLogs(description);
+						
+			// Creates Browser instance
+			description="Browser initialized";
+			BrowserController browserController= new BrowserController();
+			browserController.initializeDriver("Mozilla Firefox");			
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
 		}
 		catch(UIAutomationException ue){
-			Assert.fail("Unable to initialize the driver "+ue.getErrorMessage());
+			stepNo++;
+			Assert.fail("Unable to initialize the driver"+ue.getErrorMessage());
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo,exception,failed, ue.getErrorMessage(), blank);
 		}
 	}
 	
+	/**
+	 * This method verifies when clicked on 'Close' icon of 'About plan segment' window, window is closed
+	 * @throws UIAutomationException
+	 */
 	@Test
 	public void testMAV0093_closeNewSegment() throws UIAutomationException {
 		try {
 			// Enter URL of Channels
+			stepNo++;
+			description="URL Entered";	
 			BrowserController browserController=new BrowserController();
 		    browserController.enterURL(testData.get("ChannelsURL"),testData.get("Title"));
+		    // Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+		 	
 		 		    
 			// Login page
+		    stepNo++;
+			description="Login successful";	
 		    LoginPage loginPage = new LoginPage();
 		    loginPage.Login(GlobalVariables.configuration.getConfigData().get("UserName"),GlobalVariables.configuration.getConfigData().get("PassWord"));
-				
+		    // Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+		 		
 			// Plan Page
+		    stepNo++;
+ 	 		description="Navigated to plan page";
 			HomePage homePage=new HomePage();
 			homePage.clickCollaborationPlanLink();	
-			
+			 // Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+		 	
 			// Close Plan Map window
+			stepNo++;
+			description="Plan Map window closed";
 			PlanPage planPage=new PlanPage();
 			planPage.closePlanMap();
-					
+			 // Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+		 			
 			// Click Actions pop up menu and Add New Segment
+			stepNo++;
+			description="New Segment is added";
 			planPage.clickPopupMenu(testData.get("Actions"));
 			planPage.clickSubmenu(testData.get("AddNewSegment"));	
-							
+			 // Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+		 					
 			// Close 'Segment' window
+			stepNo++;
+			description="Segment window closed";
 			planPage.closeSegmentWindow();
-						
+			 // Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+		 				
 			// Sign Out from 'Plan' page
+			stepNo++;
+			description="Logout successful";
 			HeaderController headerController=new HeaderController();
 			headerController.signOutPlan();
-
+			 // Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+		 	
 		} catch (UIAutomationException ue) {
-			Reporting.getScreenShot("MAV0093_closeNewSegment");
+			Reporting.getScreenShot(testCaseId);
 			
 			// Sign out from plan page
+			stepNo++;
 			HeaderController headerController=new HeaderController();
 			headerController.signOutPlan();
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo,exception,failed, ue.getErrorMessage(), blank);
 				
 			// Quits the Browser
 			GlobalVariables.configuration.getWebDriver().quit();
@@ -101,7 +174,20 @@ public class MAV0093_closeNewSegment extends TestCase{
 		}
 		
 	}
+	/**
+	 * (non-Javadoc)
+	 * This method will perform cleanup actions
+	 * @see junit.framework.TestCase#tearDown()
+	*/	
 	
+	@After
+	protected void tearDown(){
+		if(GlobalVariables.configuration.getWebDriver()!=null){
+			GlobalVariables.configuration.getWebDriver().quit();
+		}
+		String endTime=LogFunctions.getDateTime();
+		GlobalVariables.configuration.setEndtime(endTime);
+	}
 	/**
      * Loads Test Data for MAV0093_closeNewSegment.
      * @throws UIAutomationException
@@ -109,6 +195,9 @@ public class MAV0093_closeNewSegment extends TestCase{
 	public void loadTestData() throws UIAutomationException
 	{		
 		try{
+			String startTime=LogFunctions.getDateTime();
+			GlobalVariables.configuration.setStartTime(startTime);
+			
 			testData=new Hashtable<String,String>();
 			File currentDir=new File(".");
 			
@@ -138,18 +227,5 @@ public class MAV0093_closeNewSegment extends TestCase{
 			throw new UIAutomationException("File MAV0093_closeNewSegment can not be parsed.");
 		}
 			
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see junit.framework.TestCase#tearDown()
-	 */
-	
-	@After
-	protected void tearDown(){
-		if(GlobalVariables.configuration.getWebDriver()!=null){
-			GlobalVariables.configuration.getWebDriver().quit();
-		}
 	}
 }
