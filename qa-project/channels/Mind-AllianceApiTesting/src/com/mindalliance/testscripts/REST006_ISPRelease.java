3ddc3253c8d139d4cbfc172ad0357a181b2b2a6f
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2012-10-16 06:38:28 +0000

    Add New Files
    git-svn-id: https://wush.net/svn/mindalliance@4445 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ qa-project/channels/Mind-AllianceApiTesting/src/com/mindalliance/testscripts/REST006_ISPRelease.java
@@ -0,0 +1,84 @@
+package com.mindalliance.testscripts;
+
+import com.mindalliance.configurations.Configurations;
+import com.mindalliance.configurations.GlobalVariables;
+import com.mindalliance.configurations.LogFunctions;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class REST006_ISPRelease extends TestCase {
+
+	/**
+	 * Invoke testISPPlanIdentifier() and get planners identities.
+	 */
+	public static void testISPPlanRelease(){
+		try {
+			LogFunctions.generateLogsDirectory();	
+			GlobalVariables.steps = 0;
+			GlobalVariables.testCaseId = "REST006_ISPRelease";
+			
+			// Create Log Files
+			GlobalVariables.testResultLogFile = LogFunctions.generateLogFile(GlobalVariables.logFile + "_"+ GlobalVariables.testCaseId + ".log");
+			GlobalVariables.steps++;
+			System.out.println(GlobalVariables.steps+") Test Case : REST006_ISPRelease Execution Started");
+			LogFunctions.logDescription(GlobalVariables.steps + ") REST006_ISPRelease Execution Started");
+			
+			// For managing SSL connections
+			Configurations.validateTrustManager();			
+			// Reading input data from CSV File
+			GlobalVariables.steps++;
+			Configurations.getTestData("REST006_ISPRelease.csv");
+			System.out.println(GlobalVariables.steps+") Reading Data From CSV File");
+			LogFunctions.logDescription(GlobalVariables.steps + ") Reading Data From CSV File");
+
+			// Send Request
+			String data =GlobalVariables.testData.get("api");
+			GlobalVariables.steps++;
+			Configurations.sendRequest(data);
+			System.out.println(GlobalVariables.steps+") Sending Request");
+			LogFunctions.logDescription(GlobalVariables.steps + ") Request Sent");
+			
+			// Receive Response in XML File (response.xml)
+			GlobalVariables.steps++;
+			Configurations.getResponse();
+			System.out.println(GlobalVariables.steps+") Getting Response");
+			LogFunctions.logDescription(GlobalVariables.steps + ") Response Received");
+					
+			// Assertion: verify that PlanSummary
+			GlobalVariables.steps++;
+			boolean planIdentifierResult;
+			// Verify URI
+			planIdentifierResult=Configurations.parseResponse("planIdentifier","uri",GlobalVariables.testData.get("uri"));	
+			Assert.assertEquals("Plan URI is", true, planIdentifierResult);
+			// Verify Plan Name
+			planIdentifierResult=Configurations.parseResponse("planIdentifier","name",GlobalVariables.testData.get("name"));	
+			Assert.assertEquals("Plan Name is",true,planIdentifierResult);
+			// Verify Release
+			planIdentifierResult=Configurations.parseResponse("planIdentifier","release",GlobalVariables.testData.get("release"));
+			Assert.assertEquals("Plan release is",true,planIdentifierResult);
+			// Verify Version
+			planIdentifierResult=Configurations.parseResponse("planIdentifier","version",GlobalVariables.testData.get("version"));
+			Assert.assertEquals("Plan release is",true,planIdentifierResult);
+			
+			System.out.println(GlobalVariables.steps+") Plan Summary Assertion Pass");
+			LogFunctions.logDescription(GlobalVariables.steps + ") Assertion Pass");
+
+			// Execution Completed
+			GlobalVariables.steps++;
+			System.out.println(GlobalVariables.steps+") Test Case : REST006_ISPRelease Execution Completed");
+			System.out.println("");
+			LogFunctions.logDescription(GlobalVariables.steps+ ") Test Case : REST006_ISPRelease Execution Completed");
+			System.out.println("");
+			
+		}catch (AssertionError ar) {
+			System.out.println(GlobalVariables.steps+") Assertion Failed : ");
+			ar.printStackTrace();
+			LogFunctions.logDescription(GlobalVariables.steps + ") Assertion Failed");
+		} catch (Exception e) {
+			//System.out.println("One of the Value missed");
+			LogFunctions.logException(e.getMessage());
+		}
+	}
+	
+}
