aa3deb8b07d042f775256f52ff06c1417a5d532e
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2012-11-07 05:56:42 +0000

    Modified
    git-svn-id: https://wush.net/svn/mindalliance@4537 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/uitestscripts/MAV0001_viewLoginPage.java
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/uitestscripts/MAV0001_viewLoginPage.java
@@ -14,7 +14,6 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
