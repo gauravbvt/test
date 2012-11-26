b6dbfef8abe548d6a9e74b36e3ef2ebbfffdcd56
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2012-11-26 06:25:03 +0000

    Added new test data from test cases
    git-svn-id: https://wush.net/svn/mindalliance@4590 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/uitestscripts/MAP0002_AddNameAndLocalizePlan.java
@@ -0,0 +1,220 @@
+package com.mindalliance.uitestscripts;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import com.mindalliance.configuration.BrowserController;
+import com.mindalliance.configuration.Configuration;
+import com.mindalliance.configuration.ElementController;
+import com.mindalliance.configuration.GlobalVariables;
+import com.mindalliance.configuration.LogFunctions;
+import com.mindalliance.configuration.Reporting;
+import com.mindalliance.configuration.UIAutomationException;
+import com.mindalliance.pages.ChannelsAdmin;
+import com.mindalliance.pages.HeaderController;
+import com.mindalliance.pages.HomePage;
+import com.mindalliance.pages.LoginPage;
+/**
+ * Test Case ID: MAP0002_AddNameAndLocalizePlan
+ * Summary: Verify that the "Plan gets added"
+ * @author Administrator
+ *
+ */
+public class MAP0002_AddNameAndLocalizePlan extends TestCase {
+	public Hashtable<String, String> testData;
+	public String testCaseId="MAP0002_AddNameAndLocalizePlan";
+	public String description=null;
+	public int stepNo=1;
+	public String passed="Pass";
+	public String failed="FAIL";
+	public String blank=""; 
+	public String scriptException;
+	public String browser="";
+	
+	/*
+	 * This method will initilize the setup required for every test case
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Before
+	protected void setUp() throws UIAutomationException{
+		try{
+			if (GlobalVariables.configuration == null){
+					GlobalVariables.configuration = Configuration.getConfigurationObject();
+			}
+			if(GlobalVariables.configuration.getAttrSearchList() == null){
+				new ElementController();
+			}
+			
+			GlobalVariables.configuration.addTestCaseIdToJList(testCaseId);	
+			// Loads Test Data
+			description = "Testcase: " + testCaseId + " execution started";
+			loadTestData();
+			// Write log			
+			LogFunctions.writeLogs(description);
+						
+			// Creates Browser instance
+			BrowserController browserController= new BrowserController();
+			browserController.initializeDriver();		
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+			
+		}
+		catch(UIAutomationException ue){
+			stepNo++;
+			description="Unable to initialize the driver";
+			Assert.fail("Unable to initialize the driver"+ue.getErrorMessage());
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo, ue.getErrorMessage(), failed, scriptException, blank);
+		}
+	}
+	
+	/**
+	 * This method will add plan to the Channels and verify if the plan is added
+	 * @throws UIAutomationException
+	 * @throws IOException 
+	 */
+	@Test
+	public void testMAP0002_AddNameAndLocalizePlan() throws UIAutomationException, IOException{
+		try {
+			// Enter URL of Channels
+			stepNo++;
+			description="URL Entered";
+			BrowserController browserController=new BrowserController();
+			browserController.enterURL(testData.get("ChannelsURL"),testData.get("Title"));
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			
+			// Login page
+			stepNo++;
+			description="Login Successful";
+		    LoginPage loginPage = new LoginPage();
+		    loginPage.Login(GlobalVariables.configuration.getConfigData().get("UserName"),GlobalVariables.configuration.getConfigData().get("PassWord"));
+		    // Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			
+			// Click on Channels Admin
+			stepNo++;
+			description="Channels Admin Page";
+			HomePage homePage=new HomePage();
+			homePage.clickChannelsAdminLink();
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			
+			// Enter Plan name
+			stepNo++;
+			description="Plan Created";
+			ChannelsAdmin channelsAdmin=new ChannelsAdmin();
+			channelsAdmin.enterPlanName(testData.get("AutomationTestPlan"),testData.get("AuthorAutomationTestPlan"));
+			channelsAdmin.deletePlan(testData.get("PlanName"));
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+						
+			// Sign Out from 'Admin' page
+			stepNo++;
+			description="SignOut Successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOutAdmin();
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Reporting reporting = new Reporting();
+			reporting.generateAutomationReport();
+		} catch (UIAutomationException ue) {
+			Reporting.getScreenShot(testCaseId);
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		
+			// Sign out from Admin page
+			stepNo++;
+			description="Signout";
+			HeaderController headerController=new HeaderController();
+			headerController.signOutAdmin();
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo, ue.getErrorMessage(), failed, scriptException, blank);
+						
+			// Quits the Browser
+			stepNo++;
+			description="Browser Closed";
+			GlobalVariables.configuration.getWebDriver().quit();
+			Assert.fail(ue.getErrorMessage());
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo, ue.getErrorMessage(), failed, scriptException, blank);
+		}
+	}
+	
+	/*
+	 * This method will perform cleanup actions
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	
+	@After
+	protected void tearDown(){
+		if(GlobalVariables.configuration.getWebDriver()!=null){
+			GlobalVariables.configuration.getWebDriver().quit();
+		}
+	}
+	
+	/**
+     * Loads Test Data for MAP0002_AddNameAndLocalizePlan.
+     * @throws UIAutomationException
+     */
+	public void loadTestData() throws UIAutomationException
+	{		
+		try{
+			String startTime=LogFunctions.getDateTime();
+			GlobalVariables.configuration.setStartTime(startTime);
+			testData=new Hashtable<String,String>();
+			File currentDir=new File(".");
+			
+			String path= currentDir.getCanonicalPath().toString() + "\\TestData\\";
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder db = dbf.newDocumentBuilder();
+			File MAP0002_AddNameAndLocalizePlan=new File(path + "MAP0002_AddNameAndLocalizePlan.xml");
+			
+			Document docMAP0002_AddNameAndLocalizePlan=db.parse(MAP0002_AddNameAndLocalizePlan);
+			Element eleMAP0002_AddNameAndLocalizePlan=docMAP0002_AddNameAndLocalizePlan.getDocumentElement();
+	              
+	        Element oXmlEleMAP0002_AddNameAndLocalizePlan = (Element) eleMAP0002_AddNameAndLocalizePlan;
+	       	
+	        
+	        this.testData.put("AutomationTestPlan",oXmlEleMAP0002_AddNameAndLocalizePlan.getElementsByTagName("automationTestPlan").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("AuthorAutomationTestPlan",oXmlEleMAP0002_AddNameAndLocalizePlan.getElementsByTagName("authorAutomationTestPlan").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("ChannelsURL",oXmlEleMAP0002_AddNameAndLocalizePlan.getElementsByTagName("channelsURL").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("Title",oXmlEleMAP0002_AddNameAndLocalizePlan.getElementsByTagName("title").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("PlanName",oXmlEleMAP0002_AddNameAndLocalizePlan.getElementsByTagName("planName").item(0).getChildNodes().item(0).getNodeValue());
+		}
+		catch(SAXException se){
+			throw new UIAutomationException("File MAP0002_AddNameAndLocalizePlan can not be parsed.");
+		}
+		catch (IOException ie) {
+			throw new UIAutomationException("File MAP0002_AddNameAndLocalizePlan.xml not found.");
+		}
+		catch (ParserConfigurationException pe) {
+			throw new UIAutomationException("File MAP0002_AddNameAndLocalizePlan can not be parsed.");
+		}
+			
+	}
+}
---
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/uitestscripts/MAP0025_receiveTask.java
@@ -0,0 +1,271 @@
+package com.mindalliance.uitestscripts;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import com.mindalliance.configuration.BrowserController;
+import com.mindalliance.configuration.Configuration;
+import com.mindalliance.configuration.ElementController;
+import com.mindalliance.configuration.GlobalVariables;
+import com.mindalliance.configuration.LogFunctions;
+import com.mindalliance.configuration.Reporting;
+import com.mindalliance.configuration.UIAutomationException;
+import com.mindalliance.pages.HeaderController;
+import com.mindalliance.pages.HomePage;
+import com.mindalliance.pages.LoginPage;
+import com.mindalliance.pages.PlanPage;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+/**
+ * Test Case ID: MAP0025_receiveTask
+ * Summary: Verify that File can be added to the of the Segment
+ * @author afour
+ *
+ */
+public class MAP0025_receiveTask extends TestCase{
+	public Hashtable<String, String> testData;
+	public String testCaseId="MAP0025_receiveTask";
+	public String description=null;
+	public int stepNo=1;
+	public String passed="Pass";
+	public String failed="FAIL";
+	public String blank=""; 
+	public String scriptException;
+	public String browser="";
+	
+	/*
+	 * This method will initilize the setup required for every test case
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Before
+	protected void setUp() throws UIAutomationException{
+		try{
+			if (GlobalVariables.configuration == null){
+					GlobalVariables.configuration = Configuration.getConfigurationObject();
+			}
+			if(GlobalVariables.configuration.getAttrSearchList() == null){
+				new ElementController();
+			}
+			
+			GlobalVariables.configuration.addTestCaseIdToJList(testCaseId);	
+			// Loads Test Data
+			description = "Testcase: " + testCaseId + " execution started";
+			loadTestData();
+			// Write log			
+			LogFunctions.writeLogs(description);
+						
+			// Creates Browser instance
+			BrowserController browserController= new BrowserController();
+			browserController.initializeDriver();	
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+			
+		}catch(UIAutomationException ue){
+			stepNo++;
+			description="Unable to initialize the driver";
+			Assert.fail("Unable to initialize the driver"+ue.getErrorMessage());
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo, ue.getErrorMessage(), failed, scriptException, blank);
+		}
+	}
+	
+	@Test
+	public void testMAP0025_receiveTask() throws UIAutomationException, IOException {
+		try{
+			stepNo++;
+			description="URL Entered";
+			// Enter URL of Channels
+			BrowserController browserController=new BrowserController();
+			browserController.enterURL(testData.get("ChannelsURL"),testData.get("Title"));
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			
+			// Login page
+			stepNo++;
+			description="Login Successful";
+			LoginPage loginPage = new LoginPage();
+		    loginPage.Login(GlobalVariables.configuration.getConfigData().get("UserName"),GlobalVariables.configuration.getConfigData().get("PassWord"));
+		    // Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 							
+			// Plan Page
+		    stepNo++;
+			description="Collaboration Plan";
+			HomePage homePage=new HomePage();
+			homePage.clickCollaborationPlanLink();	
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 						
+			// Close Plan Map window
+			stepNo++;
+			description="Close Plan Map Window";
+			PlanPage planPage=new PlanPage();
+			planPage.closePlanMap();
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			
+ 			// Click on 'Add New Segment' under 'Actions' pop up menu
+ 			stepNo++;
+			description="Add New Segment";
+			planPage.clickPopupMenu(testData.get("Actions"));
+			planPage.clickSubmenu(testData.get("AddNewSegment"));
+			// Enter Segment Name
+			planPage.enterSegmentName(testData.get("SegmentForAttachFile"));
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			
+			// Close Segment window
+ 			stepNo++;
+			description="Closed About Plan Segment Window";
+			planPage.closeSegmentWindow();
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 				
+			// Click on 'About Plan Segment' under 'Show' pop up menu
+ 			stepNo++;
+			description="About Plan Segment";
+			planPage.clickPopupMenu(testData.get("Show"));
+			planPage.clickSubmenu(testData.get("AboutPlanSegment"));
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			
+			// Enter file name
+ 			stepNo++;
+			description="Attach File";
+			planPage.enterAttachFileNameInSegment(testData.get("FileName1"));
+			// Click on 'Browse' button
+			planPage.clickBrowseInAttachFileInAboutPlanSegment();		
+			// Click on 'Submit' button
+			planPage.clickSubmitInAttachFileInAboutPlanSegment();
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			
+			// Close Segment window
+			stepNo++;
+			description="Close About Plan Segment";
+			planPage.closeSegmentWindow();
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			
+			//Click on Remove this segment
+ 			stepNo++;
+			description="Remove This Segment";
+			planPage.clickPopupMenu(testData.get("Actions"));
+			planPage.clickSubmenu(testData.get("RemoveThisSegment"));
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 								
+			//Sign Out from 'Plan' page
+ 			stepNo++;
+			description="SignOut Successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOutPlan();
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Reporting reporting = new Reporting();
+			reporting.generateAutomationReport();
+		} catch (UIAutomationException ue) {
+			Reporting.getScreenShot(testCaseId);
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+			// Sign out from plan page
+			stepNo++;
+			description="SignOut Successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOutPlan();
+			// Write log
+ 			LogFunctions.writeLogs(ue.getErrorMessage());
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+			
+ 			// Quits the Browser
+			stepNo++;
+			description="Browser Closed";
+			GlobalVariables.configuration.getWebDriver().quit();
+			Assert.fail(ue.getErrorMessage());
+			// Write log
+ 			LogFunctions.writeLogs(ue.getErrorMessage());
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+		}
+	}
+
+	/*
+	 * This method will perform cleanup actions
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	
+	@After
+	protected void tearDown(){
+		if(GlobalVariables.configuration.getWebDriver()!=null){
+			GlobalVariables.configuration.getWebDriver().quit();
+		}
+	}
+	
+	/**
+     * Loads Test Data for MAP0025_receiveTask.
+     * @throws UIAutomationException
+     */
+	public void loadTestData() throws UIAutomationException{
+		try{
+			String startTime=LogFunctions.getDateTime();
+			GlobalVariables.configuration.setStartTime(startTime);
+			testData=new Hashtable<String,String>();
+			File currentDir=new File(".");
+			
+			String path= currentDir.getCanonicalPath().toString() + "\\TestData\\";
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder db = dbf.newDocumentBuilder();
+			File MAP0025_receiveTask=new File(path + "MAP0025_receiveTask.xml");
+			
+			Document docMAP0025_receiveTask=db.parse(MAP0025_receiveTask);
+			Element eleMAP0025_receiveTask=docMAP0025_receiveTask.getDocumentElement();
+	              
+	        Element oXmlEleMAP0025_receiveTask = (Element) eleMAP0025_receiveTask;
+	        
+	               	
+	        this.testData.put("Actions",oXmlEleMAP0025_receiveTask.getElementsByTagName("actions").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("AddNewSegment",oXmlEleMAP0025_receiveTask.getElementsByTagName("addNewSegment").item(0).getChildNodes().item(0).getNodeValue());
+			this.testData.put("SegmentForAttachFile",oXmlEleMAP0025_receiveTask.getElementsByTagName("segmentForAttachFile").item(0).getChildNodes().item(0).getNodeValue());
+			this.testData.put("RemoveThisSegment",oXmlEleMAP0025_receiveTask.getElementsByTagName("removeThisSegment").item(0).getChildNodes().item(0).getNodeValue());
+			this.testData.put("ChannelsURL",oXmlEleMAP0025_receiveTask.getElementsByTagName("channelsURL").item(0).getChildNodes().item(0).getNodeValue());
+		 	this.testData.put("Title",oXmlEleMAP0025_receiveTask.getElementsByTagName("title").item(0).getChildNodes().item(0).getNodeValue());
+		 	this.testData.put("Show",oXmlEleMAP0025_receiveTask.getElementsByTagName("show").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("AboutPlanSegment",oXmlEleMAP0025_receiveTask.getElementsByTagName("aboutPlanSegment").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("FileName1",oXmlEleMAP0025_receiveTask.getElementsByTagName("fileName1").item(0).getChildNodes().item(0).getNodeValue());
+		}
+		catch(SAXException se){
+			throw new UIAutomationException("File MAP0025_receiveTask not found.");
+		}
+		catch (IOException ie) {
+			throw new UIAutomationException("File MAP0025_receiveTask.xml not found.");
+		}
+		catch (ParserConfigurationException pe) {
+			throw new UIAutomationException("File MAP0025_receiveTask can not be parsed.");
+		}
+	}
+}
