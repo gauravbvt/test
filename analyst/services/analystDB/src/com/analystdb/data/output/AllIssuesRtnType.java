f1bb4a7a9808badba6313a21a3e6b65498410aa4
denis <denis@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2013-01-27 23:35:45 +0000

    Added missing files
    git-svn-id: https://wush.net/svn/mindalliance@4685 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ analyst/services/analystDB/src/com/analystdb/data/output/AllIssuesRtnType.java
@@ -0,0 +1,23 @@
+
+package com.analystdb.data.output;
+
+import com.analystdb.data.Issue;
+
+
+/**
+ * Generated for query "allIssues" on 01/27/2013 17:30:33
+ * 
+ */
+public class AllIssuesRtnType {
+
+    private Issue issue;
+
+    public Issue getIssue() {
+        return issue;
+    }
+
+    public void setIssue(Issue issue) {
+        this.issue = issue;
+    }
+
+}
---
+++ analyst/services/analystDB/src/com/analystdb/data/output/MaxIssueSequenceRtnType.java
@@ -0,0 +1,22 @@
+
+package com.analystdb.data.output;
+
+
+
+/**
+ * Generated for query "maxIssueSequence" on 01/27/2013 17:30:33
+ * 
+ */
+public class MaxIssueSequenceRtnType {
+
+    private Integer last;
+
+    public Integer getLast() {
+        return last;
+    }
+
+    public void setLast(Integer last) {
+        this.last = last;
+    }
+
+}
