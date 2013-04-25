2d3a21245f48380c99959926aa84d2ef35960d53
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2013-04-25 06:36:14 +0000

    git-svn-id: https://wush.net/svn/mindalliance@4871 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- qa-project/channels/Mind-AllianceApiTesting/src/com/mindalliance/driverscripts/AllAPITestsSuite.java
+++ qa-project/channels/Mind-AllianceApiTesting/src/com/mindalliance/driverscripts/AllAPITestsSuite.java
@@ -2,9 +2,6 @@ package com.mindalliance.driverscripts;
 
 import com.mindalliance.testscripts.REST001_ISPPlans;
 import com.mindalliance.testscripts.REST002_ISPProcedures;
-import com.mindalliance.testscripts.REST003_ISPAgents;
-import com.mindalliance.testscripts.REST004_ISPIssues;
-import com.mindalliance.testscripts.REST005_ISPScope;
 import com.mindalliance.testscripts.REST006_ISPRelease;
 import com.mindalliance.testscripts.REST007_ISPVersion;
 
@@ -21,12 +18,12 @@ public class AllAPITestsSuite extends TestCase{
 		// Create a test suite which includes all the API tests
 		TestSuite suite = new TestSuite("AllAPITests");
 		suite.addTest(new TestSuite(REST001_ISPPlans.class));
-		suite.addTest(new TestSuite(REST002_ISPProcedures.class));
-//		suite.addTest(new TestSuite(REST003_ISPAgents.class));
-//		suite.addTest(new TestSuite(REST004_ISPIssues.class));
-//		suite.addTest(new TestSuite(REST005_ISPScope.class));
-		suite.addTest(new TestSuite(REST006_ISPRelease.class));
-		suite.addTest(new TestSuite(REST007_ISPVersion.class));
+//		suite.addTest(new TestSuite(REST002_ISPProcedures.class));
+////		suite.addTest(new TestSuite(REST003_ISPAgents.class));
+////		suite.addTest(new TestSuite(REST004_ISPIssues.class));
+////		suite.addTest(new TestSuite(REST005_ISPScope.class));
+//		suite.addTest(new TestSuite(REST006_ISPRelease.class));
+//		suite.addTest(new TestSuite(REST007_ISPVersion.class));
 		return suite;
 	}
 }
