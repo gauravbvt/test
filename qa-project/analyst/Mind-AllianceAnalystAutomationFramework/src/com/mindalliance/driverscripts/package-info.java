0f9a6898a63c6750a4e3e3dc45617f78c7ea45c0
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2013-05-08 09:26:16 +0000

    git-svn-id: https://wush.net/svn/mindalliance@4905 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- qa-project/analyst/Mind-AllianceAnalystAutomationFramework/src/com/mindalliance/driverscripts/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains the Driver classes for the test automation application.
- * @author AFourTech
- */
-
-package com.mindalliance.driverscripts;
-
--- qa-project/analyst/Mind-AllianceAnalystAutomationFramework/src/com/mindalliance/pages/LoginPage.java
+++
@@ -1,5 +0,0 @@
-package com.mindalliance.pages;
-
-public class LoginPage {
-
-}
