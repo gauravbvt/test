16a22df80d003c2c228ba1c8202b8f00e3e9f4b8
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2013-05-20 12:46:35 +0000

    git-svn-id: https://wush.net/svn/mindalliance@4941 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ qa-project/analyst/Mind-AllianceAnalystAutomationFramework/src/com/mindalliance/testscripts/AL0010_InvalidUsername.java
@@ -0,0 +1,167 @@
+package com.mindalliance.testscripts;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import com.mindalliance.configuration.BrowserController;
+import com.mindalliance.configuration.Configuration;
+import com.mindalliance.configuration.DataController;
+import com.mindalliance.configuration.ElementController;
+import com.mindalliance.configuration.GlobalVariables;
+import com.mindalliance.configuration.LogFunctions;
+import com.mindalliance.configuration.Reporting;
+import com.mindalliance.configuration.UIAutomationException;
+
+import com.mindalliance.pages.LoginPage;
+import junit.framework.TestCase;
+
+public class AL0010_InvalidUsername extends TestCase {
+	public Hashtable<String, String> testData;
+	public String testCaseId="AL0010_InvalidUsername";
+	public String description=null;
+	public int stepNo=1;
+	public String passed="Pass";
+	public String failed="Fail";
+	public String blank=""; 	
+	public String exception="";
+	public String browser="";
+	
+	/**
+	 * This method will initialize the setup required for every test case
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	
+	@Before
+	protected void setUp() throws UIAutomationException{	
+		try{
+			if (GlobalVariables.configuration == null){
+					GlobalVariables.configuration = Configuration.getConfigurationObject();
+			}
+			if(GlobalVariables.configuration.getAttrSearchList() == null){
+				new ElementController();
+			}
+			
+			DataController dc= new DataController();
+			dc.createResultFiles();
+			
+			GlobalVariables.configuration.addTestCaseIdToJList(testCaseId);	
+			loadTestData();
+			
+			// Loads Test Data
+			description = "Testcase: " + testCaseId + " execution started";
+			// Creates Browser instance
+			BrowserController browserController= new BrowserController();
+			browserController.initializeDriver();
+			// Write log		
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);		
+		}
+		catch(UIAutomationException ue){
+			stepNo++;
+			Assert.fail("Unable to initialize the driver"+ue.getErrorMessage());
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo,exception,failed, ue.getErrorMessage(), blank);
+		}
+	}
+	
+	/**
+	 * This method verify that login page is displayed after entering the URL of Analyst and user can be logged in to Analyst
+	 * @throws UIAutomationException
+	 * @throws IOException 
+	 */
+	@Test
+	public void testAL0001_Login() throws UIAutomationException, IOException{	
+		
+		// Enter URL	
+		stepNo++;
+		description="URL Entered";
+		BrowserController browserController=new BrowserController();
+		browserController.enterURL(testData.get("AnalystURL"),testData.get("Title"));
+		// Write log
+		LogFunctions.writeLogs(description);
+		LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+	    
+		// Enter URL	
+		stepNo++;
+		description="Login to Analyst using Invalid Username & valid password";
+		LoginPage loginpage= new LoginPage();
+		loginpage.Login(testData.get("UserName"),testData.get("Password"));
+		
+		// Write log
+		LogFunctions.writeLogs(description);
+		LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+
+		LogFunctions.writeLogs("Testcase: " + testCaseId + " execution completed");
+	    Reporting reporting= new Reporting();
+	    reporting.generateAutomationReport();
+	   
+		LoginPage loginpage1= new LoginPage();
+		Assert.assertEquals("Invalid username or password.", loginpage1.getLoginErrorMessage());
+	 }
+	
+	/*
+	 * (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	@After
+	protected void tearDown(){
+		if(GlobalVariables.configuration.getWebDriver()!=null){
+			GlobalVariables.configuration.getWebDriver().quit();
+		}
+	}
+	
+	/**
+     * Loads Test Data for AL0010_LoginAnalyst.
+     * @throws UIAutomationException
+     */
+	public void loadTestData() throws UIAutomationException
+	{
+		try{
+			String startTime=LogFunctions.getDateTime();
+			GlobalVariables.configuration.setStartTime(startTime);
+			
+			testData=new Hashtable<String,String>();
+			File currentDir=new File(".");
+			
+			String path= currentDir.getCanonicalPath().toString() + "\\TestData\\";
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder db = dbf.newDocumentBuilder();
+			File AL0010_InvalidUsername=new File(path + "AL0010_InvalidUsername.xml");
+			
+			Document docAL0010_InvalidUsername=db.parse(AL0010_InvalidUsername);
+			Element eleAL0010_InvalidUsername=docAL0010_InvalidUsername.getDocumentElement();
+	              
+	        Element oXmlEleAL0010_InvalidUsername = (Element) eleAL0010_InvalidUsername;
+	       	
+	        this.testData.put("AnalystURL",oXmlEleAL0010_InvalidUsername.getElementsByTagName("analystURL").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("Title",oXmlEleAL0010_InvalidUsername.getElementsByTagName("title").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("UserName",oXmlEleAL0010_InvalidUsername.getElementsByTagName("userName").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("Password",oXmlEleAL0010_InvalidUsername.getElementsByTagName("password").item(0).getChildNodes().item(0).getNodeValue());
+	        
+		}
+		catch(SAXException se){
+			throw new UIAutomationException("File AL0010_InvalidUsername.xml not found.");
+		}
+		catch (IOException ie) {
+			throw new UIAutomationException("File AL0010_InvalidUsername.xml not found.");
+		}
+		catch (ParserConfigurationException pe) {
+			throw new UIAutomationException("File AL0010_InvalidUsername.xml can not be parsed.");
+		}
+	}
+
+}
