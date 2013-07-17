e2399debb575e2416a16ccb5671e1827c22b9b11
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2013-07-17 13:26:46 +0000

    git-svn-id: https://wush.net/svn/mindalliance@5154 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/uitestscripts/MAV0112_viewAllActivities.java
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/uitestscripts/MAV0112_viewAllActivities.java
@@ -23,6 +23,7 @@ import com.mindalliance.configuration.GlobalVariables;
 import com.mindalliance.configuration.LogFunctions;
 import com.mindalliance.configuration.Reporting;
 import com.mindalliance.configuration.UIAutomationException;
+import com.mindalliance.pages.DomainPlanPage;
 import com.mindalliance.pages.HeaderController;
 import com.mindalliance.pages.HomePage;
 import com.mindalliance.pages.LoginPage;
@@ -113,27 +114,28 @@ public class MAV0112_viewAllActivities extends TestCase{
  			LogFunctions.writeLogs(description);
  			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
 		 							
+			// Domain Plans
+			stepNo++;
+			description="Domain Plans";
+			DomainPlanPage domainPlanPage= new DomainPlanPage();
+			domainPlanPage.clickDomainPlans();	
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+			
 			// Plan Page
 			stepNo++;
- 	 		description="Navigated to plan page";
+			description="Navigated to Plan page";
 			HomePage homePage=new HomePage();
-			homePage.clickCollaborationPlanLink();	
+			homePage.clickDomainPlanEditor();	
 			// Write log			
 			LogFunctions.writeLogs(description);
-			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
-					
-			// Close Plan Map window
-			stepNo++;
-			description="Plan Map window closed";
-			PlanPage planPage=new PlanPage();
-			planPage.closePlanMap();
-			// Write log			
-			LogFunctions.writeLogs(description);
-			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
 					
 			// Click on 'Activities' tab under 'Collaboration Panel'
 			stepNo++;
 			description="Activities tab opened";
+			PlanPage planPage= new PlanPage();
 			planPage.clickActivitiesTab();
 			// Write log			
 			LogFunctions.writeLogs(description);
