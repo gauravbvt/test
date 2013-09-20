b2ed7a300cee0abd905250135c592ef13e65d3b4
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2013-09-20 10:40:54 +0000

    git-svn-id: https://wush.net/svn/mindalliance@5393 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/driverscripts/Home.java
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/driverscripts/Home.java
@@ -171,11 +171,11 @@ public class Home {
 //		suite.addTestSuite(MAV0254_ViewRequirementAtAnyEvent.class);
 		suite.addTestSuite(MAV0269_ViewOrganizationsNetworkTab.class);
 		suite.addTestSuite(MAV0270_ViewOrganizationStructureTab.class);
-//		suite.addTestSuite(MAV0271_ViewOrganizationAgreementTab.class);
+		suite.addTestSuite(MAV0271_ViewOrganizationParticipationTab.class);
 		suite.addTestSuite(MAV0272_ViewOrganizationAnalyticsTab.class);
 		suite.addTestSuite(MAV0273_ViewOrganizationIssueTab.class);
-//		suite.addTestSuite(MAV0274_ViewEventAnalyticsTab.class);
-//		suite.addTestSuite(MAV0275_ViewEventIssueTab.class);
+		suite.addTestSuite(MAV0274_ViewEventAnalyticsTab.class);
+		suite.addTestSuite(MAV0275_ViewEventIssueTab.class);
 	
 //		
 ////		Plan
