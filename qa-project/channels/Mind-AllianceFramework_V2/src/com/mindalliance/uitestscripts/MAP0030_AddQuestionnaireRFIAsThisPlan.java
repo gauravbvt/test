6c8e30b186da70ff0d99280f57efcd571a07f984
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2013-09-23 12:21:58 +0000

    git-svn-id: https://wush.net/svn/mindalliance@5401 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/uitestscripts/MAP0030_AddQuestionnaireRFIAsThisPlan.java
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/uitestscripts/MAP0030_AddQuestionnaireRFIAsThisPlan.java
@@ -49,7 +49,7 @@ public class MAP0030_AddQuestionnaireRFIAsThisPlan extends TestCase{
 	public String browser="";
 	
 	/*
-	 * This method will initilize the setup required for every test case
+	 * This method will initialize the setup required for every test case
 	 * @see junit.framework.TestCase#setUp()
 	 */
 	@Before
