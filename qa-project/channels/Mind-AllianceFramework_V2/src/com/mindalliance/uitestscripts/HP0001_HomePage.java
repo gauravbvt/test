f1b3857db0774e240f1b94a42654ee19acd32830
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2013-10-29 12:45:12 +0000

    git-svn-id: https://wush.net/svn/mindalliance@5509 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/uitestscripts/HP0001_HomePage.java
@@ -0,0 +1,199 @@
+package com.mindalliance.uitestscripts;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import com.mindalliance.configuration.BrowserController;
+import com.mindalliance.configuration.Configuration;
+import com.mindalliance.configuration.DataController;
+import com.mindalliance.configuration.ElementController;
+import com.mindalliance.configuration.GlobalVariables;
+import com.mindalliance.configuration.LogFunctions;
+import com.mindalliance.configuration.Reporting;
+import com.mindalliance.configuration.UIAutomationException;
+import com.mindalliance.pages.HeaderController;
+import com.mindalliance.pages.LoginPage;
+
+/**
+ * Test Case ID: HP0001_HomePage
+ * Summary: Verify that the "Home" page renders
+ * @author Administrator
+ *
+ */
+public class HP0001_HomePage extends TestCase{
+	public Hashtable<String, String> testData;
+	public String testCaseId="HP0001_HomePage";
+	public String description=null;
+	public int stepNo=1;
+	public String passed="Pass";
+	public String failed="Fail";
+	public String blank=""; 
+	public String exception="";
+	public String browser="";
+	
+	/**
+	 * This method will initialize the setup required for every test case
+	 * @throws UIAutomationException 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Before
+	protected void setUp() throws UIAutomationException{
+		try{
+			if (GlobalVariables.configuration == null){
+					GlobalVariables.configuration = Configuration.getConfigurationObject();
+			}
+			if(GlobalVariables.configuration.getAttrSearchList() == null){
+				new ElementController();
+			}
+			DataController dataController= new DataController();
+			dataController.createResultFiles();
+			
+			GlobalVariables.configuration.addTestCaseIdToJList(testCaseId);	
+			// Loads Test Data
+			description = "Testcase: " + testCaseId + " execution started";
+			loadTestData();
+			// Write log			
+			LogFunctions.writeLogs(description);
+						
+			// Creates Browser instance
+			BrowserController browserController= new BrowserController();
+			browserController.initializeDriver();	
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+		}
+		catch(UIAutomationException ue){
+			stepNo++;
+			Assert.fail("Unable to initialize the driver"+ue.getErrorMessage());
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo,exception,failed, ue.getErrorMessage(), blank);
+			
+		}
+	}
+	/**
+	 * This method verify that home page is displayed after login to the Channels
+	 * @throws UIAutomationException
+	 * @throws IOException 
+	 */
+	@Test
+	public void testHP0001_HomePage() throws UIAutomationException, IOException{
+		try{
+			// Enter URL of Channels
+			stepNo++;
+			description="URL Entered";
+			BrowserController browserController=new BrowserController();
+			browserController.enterURL(testData.get("ChannelsURL"),testData.get("Title"));
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);		
+			    
+			// Login page
+			stepNo++;
+			description="Login successful";
+		    LoginPage loginPage = new LoginPage();
+		    loginPage.Login(GlobalVariables.configuration.getConfigData().get("UserName"),GlobalVariables.configuration.getConfigData().get("PassWord"));
+		    // Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);		
+	
+			// Sign Out from 'Home' page
+			stepNo++;
+			description="Logout successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOut();
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+		}catch (UIAutomationException ue) {
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo,description,failed, ue.getErrorMessage(), blank);
+			Reporting.getScreenShot(testCaseId);
+		    
+			// Sign out from home page
+		    stepNo++;
+		    description="Logout successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOut();
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+				
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+			// Quits the Browser
+			GlobalVariables.configuration.getWebDriver().quit();
+			Assert.fail(ue.getErrorMessage());		
+		}
+	}
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	
+	@After
+	protected void tearDown(){
+		if(GlobalVariables.configuration.getWebDriver()!=null){
+			GlobalVariables.configuration.getWebDriver().quit();
+		}
+	}
+	/**
+     * Loads Test Data for HP0001_HomePage.
+     * @throws UIAutomationException
+     */
+	public void loadTestData() throws UIAutomationException
+	{
+		try{
+			String startTime=LogFunctions.getDateTime();
+			GlobalVariables.configuration.setStartTime(startTime);
+			
+			testData=new Hashtable<String,String>();
+			File currentDir=new File(".");
+			
+			String path= currentDir.getCanonicalPath().toString() + "\\TestData\\";
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder db = dbf.newDocumentBuilder();
+			File HP0001_HomePage=new File(path + "HP0001_HomePage.xml");
+			
+			Document docHP0001_HomePage=db.parse(HP0001_HomePage);
+			Element eleHP0001_HomePage=docHP0001_HomePage.getDocumentElement();
+	              
+	        Element oXmlEleHP0001_HomePage = (Element) eleHP0001_HomePage;
+	                     	
+	        
+			this.testData.put("ChannelsURL",oXmlEleHP0001_HomePage.getElementsByTagName("channelsURL").item(0).getChildNodes().item(0).getNodeValue());
+			this.testData.put("Title",oXmlEleHP0001_HomePage.getElementsByTagName("title").item(0).getChildNodes().item(0).getNodeValue());
+		}
+		catch(SAXException se){
+			throw new UIAutomationException("File HP0001_HomePage not found.");
+		}
+		catch (IOException ie) {
+			throw new UIAutomationException("File HP0001_HomePage.xml not found.");
+		}
+		catch (ParserConfigurationException pe) {
+			throw new UIAutomationException("File HP0001_HomePage can not be parsed.");
+		}
+	}
+}
