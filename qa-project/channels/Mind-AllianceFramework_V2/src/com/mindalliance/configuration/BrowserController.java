6f63fe942e0bcf822335c184e70435d7fa0bd670
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2013-10-29 12:43:55 +0000

    git-svn-id: https://wush.net/svn/mindalliance@5504 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/configuration/BrowserController.java
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/configuration/BrowserController.java
@@ -23,6 +23,7 @@ public class BrowserController{
 	 * @throws UIAutomationException 
 	*/
 	
+	@SuppressWarnings("deprecation")
 	public void initializeDriver() throws UIAutomationException	{
 		String browser=GlobalVariables.configuration.getConfigData().get("Browser");
 		try{
