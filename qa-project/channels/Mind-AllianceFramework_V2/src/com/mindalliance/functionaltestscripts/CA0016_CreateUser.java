f19bb79140a773cd139ddc28ac46d66844009c57
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2013-11-19 10:49:10 +0000

    git-svn-id: https://wush.net/svn/mindalliance@5674 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/functionaltestscripts/CA0016_CreateUser.java
@@ -0,0 +1,247 @@
+package com.mindalliance.functionaltestscripts;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import com.mindalliance.configuration.BrowserController;
+import com.mindalliance.configuration.Configuration;
+import com.mindalliance.configuration.DataController;
+import com.mindalliance.configuration.ElementController;
+import com.mindalliance.configuration.GlobalVariables;
+import com.mindalliance.configuration.LogFunctions;
+import com.mindalliance.configuration.Reporting;
+import com.mindalliance.configuration.UIAutomationException;
+import com.mindalliance.pages.ChannelsAdmin;
+import com.mindalliance.pages.HeaderController;
+import com.mindalliance.pages.HomePage;
+import com.mindalliance.pages.LoginPage;
+import com.mindalliance.pages.PlanPage;
+
+/**
+ * Test Case ID: CA0016_CreateUser
+ * Summary: Verify that the new user gets added
+ * @author Administrator
+ *
+ */
+public class CA0016_CreateUser extends TestCase{
+	public Hashtable<String, String> testData;
+	public String testCaseId="CA0016_CreateUser";
+	public String description=null;
+	public int stepNo=1;
+	public String passed="Pass";
+	public String failed="FAIL";
+	public String blank=""; 
+	public String scriptException;
+	public String browser="";
+	
+	/*
+	 * This method will initialize the setup required for every test case
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Before
+	protected void setUp() throws UIAutomationException{
+		try{
+			if (GlobalVariables.configuration == null){
+					GlobalVariables.configuration = Configuration.getConfigurationObject();
+			}
+			if(GlobalVariables.configuration.getAttrSearchList() == null){
+				new ElementController();
+			}
+			DataController dataController= new DataController();
+			dataController.createResultFiles();
+			
+			GlobalVariables.configuration.addTestCaseIdToJList(testCaseId);	
+			// Loads Test Data
+			description = "Testcase: " + testCaseId + " execution started";
+			loadTestData();
+			// Write log			
+			LogFunctions.writeLogs(description);
+						
+			// Creates Browser instance
+			BrowserController browserController= new BrowserController();
+			browserController.initializeDriver();	
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+			
+		}
+		catch(UIAutomationException ue){
+			stepNo++;
+			description="Unable to initialize the driver";
+			Assert.fail("Unable to initialize the driver"+ue.getErrorMessage());
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo, ue.getErrorMessage(), failed, scriptException, blank);
+		}
+	}
+	
+	/**
+	 * This method adds new user to the plan and verify if the user is added
+	 * @throws UIAutomationException
+	 * @throws IOException 
+	 */
+	@Test
+	public void testCA0016_CreateUser() throws UIAutomationException, IOException{
+		try {
+			// Enter URL of Channels
+			stepNo++;
+			description="URL Entered";
+			BrowserController browserController=new BrowserController();
+			browserController.enterURL(testData.get("ChannelsURL"),testData.get("Title"));
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+						
+			// Login page
+			stepNo++;
+			description="Login Sucessful";
+		    LoginPage loginPage = new LoginPage();
+		    loginPage.Login(GlobalVariables.configuration.getConfigData().get("UserName"),GlobalVariables.configuration.getConfigData().get("PassWord"));
+		    // Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+		 			
+			// Click on Channels Admin
+		    stepNo++;
+			description="Channels Admin Page";
+			HomePage homePage=new HomePage();
+			homePage.clickChannelsAdminLink();
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+	
+ 			//Click Users tab
+ 			stepNo++;
+ 			ChannelsAdmin ca=new ChannelsAdmin();
+ 			ca.clickUsersTab();
+ 		    // Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			
+			// Add user
+			stepNo++;
+			description="User created";
+			ChannelsAdmin channelsAdmin=new ChannelsAdmin();
+			channelsAdmin.addUser(testData.get("User"));
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			
+ 			//Add User Details
+ 			stepNo++;
+ 			description="Add User Details";
+			channelsAdmin.addUserDetails(testData.get("Email"),testData.get("Password"),testData.get("isAdministrator"),testData.get("isDisabled"));
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+
+			//Sign Out from 'Admin' page
+			stepNo++;
+			description="SignOut Successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOutAdmin();
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+		}catch (UIAutomationException ue) {
+			PlanPage planPage= new PlanPage();
+			planPage.getStackTraceOnPlanPage();
+			
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo,description,failed, ue.getErrorMessage(), blank);
+			Reporting.getScreenShot(testCaseId);
+			
+			// Sign out from home page
+		    stepNo++;
+		    description="Logout successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOut();
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+				
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+			// Quits the Browser
+			GlobalVariables.configuration.getWebDriver().quit();
+			Assert.fail(ue.getErrorMessage());		
+		}
+	}
+	
+	/*
+	 * This method will perform cleanup actions
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	
+	@After
+	protected void tearDown(){
+		if(GlobalVariables.configuration.getWebDriver()!=null){
+			GlobalVariables.configuration.getWebDriver().quit();
+		}
+	}
+	
+	/**
+     * Loads Test Data for CA0016_CreateUser.
+     * @throws UIAutomationException
+     */
+	public void loadTestData() throws UIAutomationException
+	{
+		try{
+			String startTime=LogFunctions.getDateTime();
+			GlobalVariables.configuration.setStartTime(startTime);
+			testData=new Hashtable<String,String>();
+			File currentDir=new File(".");
+			
+			String path= currentDir.getCanonicalPath().toString() + "\\TestData\\";
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder db = dbf.newDocumentBuilder();
+			File CA0016_CreateUser=new File(path + "CA0016_CreateUser.xml");
+			
+			Document docCA0016_CreateUser=db.parse(CA0016_CreateUser);
+			Element eleCA0016_CreateUser=docCA0016_CreateUser.getDocumentElement();
+	              
+	        Element oXmlEleCA0016_CreateUser = (Element) eleCA0016_CreateUser;
+	     	        
+	        this.testData.put("User",oXmlEleCA0016_CreateUser.getElementsByTagName("user").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("ChannelsURL",oXmlEleCA0016_CreateUser.getElementsByTagName("channelsURL").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("Title",oXmlEleCA0016_CreateUser.getElementsByTagName("title").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("Email",oXmlEleCA0016_CreateUser.getElementsByTagName("email").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("Password",oXmlEleCA0016_CreateUser.getElementsByTagName("password").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("IsAdministrator",oXmlEleCA0016_CreateUser.getElementsByTagName("isAdministrator").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("IsDisabled",oXmlEleCA0016_CreateUser.getElementsByTagName("isDisabled").item(0).getChildNodes().item(0).getNodeValue());
+		
+		}
+		catch(SAXException se){
+			throw new UIAutomationException("File CA0016_CreateUser not found.");
+		}
+		catch (IOException ie) {
+			throw new UIAutomationException("File CA0016_CreateUser.xml not found.");
+		}
+		catch (ParserConfigurationException pe) {
+			throw new UIAutomationException("File CA0016_CreateUser can not be parsed.");
+		}
+	}
+	
+	
+}
