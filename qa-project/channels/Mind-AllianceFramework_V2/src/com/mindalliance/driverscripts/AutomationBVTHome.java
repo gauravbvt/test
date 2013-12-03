c6531384c3909e14c8919785fc0c214a3c67bc5d
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2013-12-03 06:55:20 +0000

    git-svn-id: https://wush.net/svn/mindalliance@5718 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/driverscripts/AutomationBVTHome.java
@@ -0,0 +1,20 @@
+package com.mindalliance.driverscripts;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import com.mindalliance.automationbvt.*;
+import com.mindalliance.configuration.UIAutomationException;
+
+public class AutomationBVTHome {
+	public static Test suite() throws UIAutomationException{
+		TestSuite suite = new TestSuite("Mind-Alliance Automation Framework");	
+		
+		suite.addTestSuite(MABVT0001_VerifyBrowsersVersion.class);
+		suite.addTestSuite(MABVT0002_VerifyOSInstalled.class);
+		suite.addTestSuite(MABVT0003_VerifyJavaVersion.class);
+		suite.addTestSuite(MABVT0005_VerifyBrowsersInstalled.class);
+		return suite;
+	
+	}
+}
