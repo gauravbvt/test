410244b2b6ef4fc6e24ae397af66fb68629e8d7e
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2014-01-24 12:02:01 +0000

    git-svn-id: https://wush.net/svn/mindalliance@5813 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/functionaltestscripts/CA0007_CreateTemplateWithSpecialCharacters.java
@@ -0,0 +1,254 @@
+package com.mindalliance.functionaltestscripts;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import com.mindalliance.configuration.BrowserController;
+import com.mindalliance.configuration.Configuration;
+import com.mindalliance.configuration.DataController;
+import com.mindalliance.configuration.ElementController;
+import com.mindalliance.configuration.GlobalVariables;
+import com.mindalliance.configuration.Log4J;
+import com.mindalliance.configuration.LogFunctions;
+import com.mindalliance.configuration.Reporting;
+import com.mindalliance.configuration.UIAutomationException;
+import com.mindalliance.pages.ChannelsAdmin;
+import com.mindalliance.pages.HeaderController;
+import com.mindalliance.pages.HomePage;
+import com.mindalliance.pages.LoginPage;
+/**
+ * Test Case ID: CA0007_CreateTemplateWithSpecialCharacters
+ * Summary: Verify that the new template with special characters does not get added
+ * @author Administrator
+ *
+ */
+public class CA0007_CreateTemplateWithSpecialCharacters extends TestCase {
+	public Hashtable<String, String> testData;
+	public String testCaseId="CA0007_CreateTemplateWithSpecialCharacters";
+	public String description=null;
+	public int stepNo=1;
+	public String passed="Pass";
+	public String failed="FAIL";
+	public String blank=""; 
+	public String scriptException;
+	public String browser="";
+	
+	/*
+	 * This method will initialize the setup required for every test case
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Before
+	protected void setUp() throws UIAutomationException{
+		try{
+			if (GlobalVariables.configuration == null){
+					GlobalVariables.configuration = Configuration.getConfigurationObject();
+			}
+			if(GlobalVariables.configuration.getAttrSearchList() == null){
+				new ElementController();
+			}
+			DataController dataController= new DataController();
+			dataController.createResultFiles();
+			
+			GlobalVariables.configuration.addTestCaseIdToJList(testCaseId);	
+			// Loads the Test Data
+			description = "Testcase: " + testCaseId + " execution started";
+			loadTestData();
+			// Write log			
+			LogFunctions.writeLogs(description);
+						
+			// Creates Browser instance
+			BrowserController browserController= new BrowserController();
+			browserController.initializeDriver();		
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Browser initialized");
+			
+		}
+		catch(UIAutomationException ue){
+			stepNo++;
+			description="Unable to initialize the driver";
+			Assert.fail("Unable to initialize the driver"+ue.getErrorMessage());
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo, ue.getErrorMessage(), failed, scriptException, blank);
+			Log4J.getlogger(this.getClass()).error(testCaseId +"Unable to initialize the driver");
+				
+		}
+	}
+	
+	/**
+	 * This method will add plan to the Channels and verify if the plan is added
+	 * @throws UIAutomationException
+	 * @throws IOException 
+	 */
+	@Test
+	public void testCA0007_CreateTemplateWithSpecialCharacters() throws UIAutomationException, IOException{
+		try {
+			// Enter URL of Channels
+			stepNo++;
+			description="URL Entered";
+			BrowserController browserController=new BrowserController();
+			browserController.enterURL();
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"URL Entered");
+				
+			// Login page
+			stepNo++;
+			description="Login Successful";
+		    LoginPage loginPage = new LoginPage();
+		    loginPage.Login(GlobalVariables.configuration.getConfigData().get("UserName"),GlobalVariables.configuration.getConfigData().get("PassWord"));
+		    // Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Login Successful");
+				
+			// Click on Channels Admin
+			stepNo++;
+			description="Channels Admin Page";
+			HomePage homePage=new HomePage();
+			homePage.clickChannelsAdminLink();
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Channels Admin Page");
+				
+			// Enter Plan name
+			stepNo++;
+			description="Enter Template name";
+			ChannelsAdmin channelsAdmin=new ChannelsAdmin();
+			channelsAdmin.enterPlanName(testData.get("AutomationTestPlan"));
+			channelsAdmin.clickAddPlanButton();
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Enter Template name");
+							
+			//Click Save button
+			stepNo++;
+			description="Click Save button";
+			channelsAdmin.clickAddPlanButtonInvalidPlan();
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Click Save button");
+				
+			/*//Delete the created template
+			stepNo++;
+			description="Delete created template";
+			channelsAdmin.deletePlan(testData.get("AutomationTestPlan"));
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Delete created template");
+				*/
+			// Sign Out from 'Admin' page
+			stepNo++;
+			description="SignOut Successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOutAdmin();
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"SignOut Successful");
+				
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+		}catch (UIAutomationException ue) {
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo,description,failed, ue.getErrorMessage(), blank);
+			Reporting.getScreenShot(testCaseId);
+			Log4J.getlogger(this.getClass()).error(testCaseId +ue.getErrorMessage());
+		    
+			// Sign out from home page
+		    stepNo++;
+		    description="Logout successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOut();
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Logout successful");
+				
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+			// Quits the Browser
+			GlobalVariables.configuration.getWebDriver().quit();
+			Assert.fail(ue.getErrorMessage());		
+		}
+	}
+	
+	/*
+	 * This method will perform cleanup actions
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	
+	@After
+	protected void tearDown(){
+		if(GlobalVariables.configuration.getWebDriver()!=null){
+			GlobalVariables.configuration.getWebDriver().quit();
+			Log4J.getlogger(this.getClass()).info(testCaseId +" Browser quit");
+		}
+	}
+	
+	/**
+     * Loads Test Data for CA0007_CreateTemplateWithSpecialCharacters.
+     * @throws UIAutomationException
+     */
+	public void loadTestData() throws UIAutomationException
+	{		
+		try{
+			String startTime=LogFunctions.getDateTime();
+			GlobalVariables.configuration.setStartTime(startTime);
+			testData=new Hashtable<String,String>();
+			File currentDir=new File(".");
+			
+			String path= currentDir.getCanonicalPath().toString() + "\\TestData\\";
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder db = dbf.newDocumentBuilder();
+			File CA0007_CreateTemplateWithSpecialCharacters=new File(path + "CA0007_CreateTemplateWithSpecialCharacters.xml");
+			
+			Document docCA0007_CreateTemplateWithSpecialCharacters=db.parse(CA0007_CreateTemplateWithSpecialCharacters);
+			Element eleCA0007_CreateTemplateWithSpecialCharacters=docCA0007_CreateTemplateWithSpecialCharacters.getDocumentElement();
+	              
+	        Element oXmlEleCA0007_CreateTemplateWithSpecialCharacters = (Element) eleCA0007_CreateTemplateWithSpecialCharacters;
+	       	
+	        
+	        this.testData.put("AutomationTestPlan",oXmlEleCA0007_CreateTemplateWithSpecialCharacters.getElementsByTagName("automationTestPlan").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("AuthorAutomationTestPlan",oXmlEleCA0007_CreateTemplateWithSpecialCharacters.getElementsByTagName("authorAutomationTestPlan").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("ChannelsURL",oXmlEleCA0007_CreateTemplateWithSpecialCharacters.getElementsByTagName("channelsURL").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("Title",oXmlEleCA0007_CreateTemplateWithSpecialCharacters.getElementsByTagName("title").item(0).getChildNodes().item(0).getNodeValue());
+	 
+		}
+		catch(SAXException se){
+			throw new UIAutomationException("File CA0007_CreateTemplateWithSpecialCharacters can not be parsed.");
+		}
+		catch (IOException ie) {
+			throw new UIAutomationException("File CA0007_CreateTemplateWithSpecialCharacters.xml not found.");
+		}
+		catch (ParserConfigurationException pe) {
+			throw new UIAutomationException("File CA0007_CreateTemplateWithSpecialCharacters can not be parsed.");
+		}
+			
+	}
+}
---
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/functionaltestscripts/CA0009_CreateTemplateWithoutOwnerName.java
@@ -0,0 +1,252 @@
+package com.mindalliance.functionaltestscripts;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import com.mindalliance.configuration.BrowserController;
+import com.mindalliance.configuration.Configuration;
+import com.mindalliance.configuration.DataController;
+import com.mindalliance.configuration.ElementController;
+import com.mindalliance.configuration.GlobalVariables;
+import com.mindalliance.configuration.Log4J;
+import com.mindalliance.configuration.LogFunctions;
+import com.mindalliance.configuration.Reporting;
+import com.mindalliance.configuration.UIAutomationException;
+import com.mindalliance.pages.ChannelsAdmin;
+import com.mindalliance.pages.HeaderController;
+import com.mindalliance.pages.HomePage;
+import com.mindalliance.pages.LoginPage;
+/**
+ * Test Case ID: CA0009_CreateTemplateWithoutOwnerName
+ * Summary: Verify that the new template gets saved without entering owner name
+ * @author Administrator
+ *
+ */
+public class CA0009_CreateTemplateWithoutOwnerName extends TestCase {
+	public Hashtable<String, String> testData;
+	public String testCaseId="CA0009_CreateTemplateWithoutOwnerName";
+	public String description=null;
+	public int stepNo=1;
+	public String passed="Pass";
+	public String failed="FAIL";
+	public String blank=""; 
+	public String scriptException;
+	public String browser="";
+	
+	/*
+	 * This method will initialize the setup required for every test case
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Before
+	protected void setUp() throws UIAutomationException{
+		try{
+			if (GlobalVariables.configuration == null){
+					GlobalVariables.configuration = Configuration.getConfigurationObject();
+			}
+			if(GlobalVariables.configuration.getAttrSearchList() == null){
+				new ElementController();
+			}
+			DataController dataController= new DataController();
+			dataController.createResultFiles();
+			
+			GlobalVariables.configuration.addTestCaseIdToJList(testCaseId);	
+			// Loads the Test Data
+			description = "Testcase: " + testCaseId + " execution started";
+			loadTestData();
+			// Write log			
+			LogFunctions.writeLogs(description);
+						
+			// Creates Browser instance
+			BrowserController browserController= new BrowserController();
+			browserController.initializeDriver();		
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Browser initialized");
+			
+		}
+		catch(UIAutomationException ue){
+			stepNo++;
+			description="Unable to initialize the driver";
+			Assert.fail("Unable to initialize the driver"+ue.getErrorMessage());
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo, ue.getErrorMessage(), failed, scriptException, blank);
+			Log4J.getlogger(this.getClass()).error(testCaseId +"Unable to initialize the driver");
+				
+		}
+	}
+	
+	/**
+	 * This method will add plan to the Channels and verify if the plan is added
+	 * @throws UIAutomationException
+	 * @throws IOException 
+	 */
+	@Test
+	public void testCA0009_CreateTemplateWithoutOwnerName() throws UIAutomationException, IOException{
+		try {
+			// Enter URL of Channels
+			stepNo++;
+			description="URL Entered";
+			BrowserController browserController=new BrowserController();
+			browserController.enterURL();
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"URL Entered");
+				
+			// Login page
+			stepNo++;
+			description="Login Successful";
+		    LoginPage loginPage = new LoginPage();
+		    loginPage.Login(GlobalVariables.configuration.getConfigData().get("UserName"),GlobalVariables.configuration.getConfigData().get("PassWord"));
+		    // Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Login Successful");
+				
+			// Click on Channels Admin
+			stepNo++;
+			description="Channels Admin Page";
+			HomePage homePage=new HomePage();
+			homePage.clickChannelsAdminLink();
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Channels Admin Page");
+				
+			// Enter Plan name
+			stepNo++;
+			description="Enter Template name";
+			ChannelsAdmin channelsAdmin=new ChannelsAdmin();
+			channelsAdmin.enterPlanName(testData.get("AutomationTestPlan"));
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Enter Template name");
+							
+			//Click Save button
+			stepNo++;
+			description="Click Save button";
+			channelsAdmin.clickSavePlanButton();
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Click Save button");
+				
+			//Delete the created template
+			stepNo++;
+			description="Delete created template";
+			channelsAdmin.deletePlan(testData.get("AutomationTestPlan"));
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Delete created template");
+				
+			// Sign Out from 'Admin' page
+			stepNo++;
+			description="SignOut Successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOutAdmin();
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"SignOut Successful");
+				
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+		}catch (UIAutomationException ue) {
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo,description,failed, ue.getErrorMessage(), blank);
+			Reporting.getScreenShot(testCaseId);
+			Log4J.getlogger(this.getClass()).error(testCaseId +ue.getErrorMessage());
+		    
+			// Sign out from home page
+		    stepNo++;
+		    description="Logout successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOut();
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Logout successful");
+				
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+			// Quits the Browser
+			GlobalVariables.configuration.getWebDriver().quit();
+			Assert.fail(ue.getErrorMessage());		
+		}
+	}
+	
+	/*
+	 * This method will perform cleanup actions
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	
+	@After
+	protected void tearDown(){
+		if(GlobalVariables.configuration.getWebDriver()!=null){
+			GlobalVariables.configuration.getWebDriver().quit();
+			Log4J.getlogger(this.getClass()).info(testCaseId +" Browser quit");
+		}
+	}
+	
+	/**
+     * Loads Test Data for CA0009_CreateTemplateWithoutOwnerName.
+     * @throws UIAutomationException
+     */
+	public void loadTestData() throws UIAutomationException
+	{		
+		try{
+			String startTime=LogFunctions.getDateTime();
+			GlobalVariables.configuration.setStartTime(startTime);
+			testData=new Hashtable<String,String>();
+			File currentDir=new File(".");
+			
+			String path= currentDir.getCanonicalPath().toString() + "\\TestData\\";
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder db = dbf.newDocumentBuilder();
+			File CA0009_CreateTemplateWithoutOwnerName=new File(path + "CA0009_CreateTemplateWithoutOwnerName.xml");
+			
+			Document docCA0009_CreateTemplateWithoutOwnerName=db.parse(CA0009_CreateTemplateWithoutOwnerName);
+			Element eleCA0009_CreateTemplateWithoutOwnerName=docCA0009_CreateTemplateWithoutOwnerName.getDocumentElement();
+	              
+	        Element oXmlEleCA0009_CreateTemplateWithoutOwnerName = (Element) eleCA0009_CreateTemplateWithoutOwnerName;
+	       	
+	        
+	        this.testData.put("AutomationTestPlan",oXmlEleCA0009_CreateTemplateWithoutOwnerName.getElementsByTagName("automationTestPlan").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("ChannelsURL",oXmlEleCA0009_CreateTemplateWithoutOwnerName.getElementsByTagName("channelsURL").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("Title",oXmlEleCA0009_CreateTemplateWithoutOwnerName.getElementsByTagName("title").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("PlanName",oXmlEleCA0009_CreateTemplateWithoutOwnerName.getElementsByTagName("planName").item(0).getChildNodes().item(0).getNodeValue());
+		}
+		catch(SAXException se){
+			throw new UIAutomationException("File CA0009_CreateTemplateWithoutOwnerName can not be parsed.");
+		}
+		catch (IOException ie) {
+			throw new UIAutomationException("File CA0009_CreateTemplateWithoutOwnerName.xml not found.");
+		}
+		catch (ParserConfigurationException pe) {
+			throw new UIAutomationException("File CA0009_CreateTemplateWithoutOwnerName can not be parsed.");
+		}
+			
+	}
+}
