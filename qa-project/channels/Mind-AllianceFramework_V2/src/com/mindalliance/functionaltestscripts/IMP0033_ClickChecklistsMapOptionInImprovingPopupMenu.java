11f47f6a4491b8c1aa2ef8d1bc379fd11712e94f
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2014-02-17 12:17:49 +0000

    git-svn-id: https://wush.net/svn/mindalliance@5891 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/functionaltestscripts/IMP0033_ClickChecklistsMapOptionInImprovingPopupMenu.java
@@ -0,0 +1,252 @@
+package com.mindalliance.functionaltestscripts;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import com.mindalliance.configuration.BrowserController;
+import com.mindalliance.configuration.Configuration;
+import com.mindalliance.configuration.DataController;
+import com.mindalliance.configuration.ElementController;
+import com.mindalliance.configuration.GlobalVariables;
+import com.mindalliance.configuration.LogFunctions;
+import com.mindalliance.configuration.Reporting;
+import com.mindalliance.configuration.Log4J;
+import com.mindalliance.configuration.UIAutomationException;
+import com.mindalliance.pages.DomainPlanPage;
+import com.mindalliance.pages.HeaderController;
+import com.mindalliance.pages.LoginPage;
+import com.mindalliance.pages.HomePage;
+import com.mindalliance.pages.PlanPage;
+
+/**
+ * Testcase ID: IMP0033_ClickChecklistsMapOptionInImprovingPopupMenu
+ * 	   Summary: Verify that Checklists Map window opens when clicked on "Checklists Map" option in Improving popup menu
+ * @author Afour
+ *
+ */
+
+public class IMP0033_ClickChecklistsMapOptionInImprovingPopupMenu extends TestCase {
+	public Hashtable<String, String> testData;
+	public String testCaseId="IMP0033_ClickChecklistsMapOptionInImprovingPopupMenu";
+	public String description=null;
+	public int stepNo=1;
+	public String passed="Pass";
+	public String failed="FAIL";
+	public String blank=""; 
+	public String scriptException;
+	public String browser="";
+
+	/*
+	 * This method will initialize the setup required for every test case
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Before
+	protected void setUp() throws UIAutomationException{
+		try{
+			if (GlobalVariables.configuration == null){
+					GlobalVariables.configuration = Configuration.getConfigurationObject();
+			}
+			if(GlobalVariables.configuration.getAttrSearchList() == null){
+				new ElementController();
+			}
+			DataController dataController= new DataController();
+			dataController.createResultFiles();
+			
+			GlobalVariables.configuration.addTestCaseIdToJList(testCaseId);	
+			// Loads Test Data
+			description = "Testcase: " + testCaseId + " execution started";
+			loadTestData();
+			// Write log			
+			LogFunctions.writeLogs(description);
+						
+			// Creates Browser instance
+			BrowserController browserController= new BrowserController();
+			browserController.initializeDriver();		
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Browser initialized");
+			
+		}
+		catch(UIAutomationException ue){
+			stepNo++;
+			description="Unable to initialize the driver";
+			Assert.fail("Unable to initialize the driver"+ue.getErrorMessage());
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo, ue.getErrorMessage(), failed, scriptException, blank);
+			Log4J.getlogger(this.getClass()).error(testCaseId +"Unable to initialize the driver");
+		}
+	}
+	
+	@Test
+	public void testIMP0033_ClickChecklistsMapOptionInImprovingPopupMenu() throws UIAutomationException, IOException {
+		try{
+			stepNo++;
+			description="URL Entered";
+			// Enter URL of Channels
+			BrowserController browserController=new BrowserController();
+			browserController.enterURL();
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"URL Entered");
+ 			
+			// Login page
+			stepNo++;
+			description="Login Successful";
+			LoginPage loginPage = new LoginPage();
+		    loginPage.Login(GlobalVariables.configuration.getConfigData().get("UserName"),GlobalVariables.configuration.getConfigData().get("PassWord"));
+		    // Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Login Successful");
+ 			
+ 			// Domain Plans
+ 		    stepNo++;
+ 			description="Domain Plans";
+ 			DomainPlanPage domainPlanPage= new DomainPlanPage();
+ 			domainPlanPage.clickDomainPlans();	
+ 			// Write log			
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Domain Plans");
+ 			
+			// Plan Page
+		    stepNo++;
+			description="Domain Plan Editor";
+			HomePage homePage=new HomePage();
+			homePage.clickDomainPlanEditor();	
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Domain Plan Editor");
+ 			
+ 			//Click on Improving popup menu
+ 			stepNo++;
+ 			description="Improving popup menu";
+ 			PlanPage planPage=new PlanPage();
+ 			planPage.clickPopupMenu(testData.get("Improving"));
+ 			 // Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Improving popup menu");
+ 			
+ 			//Click on Checklists Map option
+ 			stepNo++;
+ 			description="Checklists Map";
+ 			planPage.clickSubmenu(testData.get("ChecklistsMap"));
+ 			 // Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Checklists Map");
+ 			
+ 			// Sign out from home page
+		    stepNo++;
+		    description="Logout successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOut();
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Logout Successful");	
+			
+ 			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+		}catch (UIAutomationException ue) {
+			stepNo++;
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo,description,failed, ue.getErrorMessage(), blank);
+			Reporting.getScreenShot(testCaseId);
+			Log4J.getlogger(this.getClass()).error(testCaseId +ue.getErrorMessage());
+			
+			// Sign out from home page
+		    stepNo++;
+		    description="Logout successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOut();
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Logout Successful");	
+			
+			
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+			// Quits the Browser
+			GlobalVariables.configuration.getWebDriver().quit();
+			Assert.fail(ue.getErrorMessage());		
+		}
+	}
+	
+	/*
+	 * This method will perform cleanup actions
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	
+	@After
+	protected void tearDown(){
+		if(GlobalVariables.configuration.getWebDriver()!=null){
+			GlobalVariables.configuration.getWebDriver().quit();
+			Log4J.getlogger(this.getClass()).info(testCaseId +" Browser quit");
+		}
+	}
+	
+	/**
+     * Loads Test Data for IMP0033_ClickChecklistsMapOptionInImprovingPopupMenu.
+     * @throws UIAutomationException
+     */
+	public void loadTestData() throws UIAutomationException{
+		try{
+			String startTime=LogFunctions.getDateTime();
+			GlobalVariables.configuration.setStartTime(startTime);
+			testData=new Hashtable<String,String>();
+			File currentDir=new File(".");
+			
+			String path= currentDir.getCanonicalPath().toString() + "\\TestData\\";
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder db = dbf.newDocumentBuilder();
+			File IMP0033_ClickChecklistsMapOptionInImprovingPopupMenu=new File(path + "IMP0033_ClickChecklistsMapOptionInImprovingPopupMenu.xml");
+			
+			Document docIMP0033_ClickChecklistsMapOptionInImprovingPopupMenu=db.parse(IMP0033_ClickChecklistsMapOptionInImprovingPopupMenu);
+			Element eleIMP0033_ClickChecklistsMapOptionInImprovingPopupMenu=docIMP0033_ClickChecklistsMapOptionInImprovingPopupMenu.getDocumentElement();
+	              
+	        Element oXmlEleIMP0033_ClickChecklistsMapOptionInImprovingPopupMenu = (Element) eleIMP0033_ClickChecklistsMapOptionInImprovingPopupMenu;
+	       	
+			this.testData.put("ChannelsURL",oXmlEleIMP0033_ClickChecklistsMapOptionInImprovingPopupMenu.getElementsByTagName("channelsURL").item(0).getChildNodes().item(0).getNodeValue());
+			this.testData.put("Title",oXmlEleIMP0033_ClickChecklistsMapOptionInImprovingPopupMenu.getElementsByTagName("title").item(0).getChildNodes().item(0).getNodeValue());
+			this.testData.put("Improving",oXmlEleIMP0033_ClickChecklistsMapOptionInImprovingPopupMenu.getElementsByTagName("improving").item(0).getChildNodes().item(0).getNodeValue());
+			this.testData.put("ChecklistsMap",oXmlEleIMP0033_ClickChecklistsMapOptionInImprovingPopupMenu.getElementsByTagName("checklistsMap").item(0).getChildNodes().item(0).getNodeValue());
+			
+			
+		}
+		catch(SAXException se){
+			throw new UIAutomationException("File IMP0033_ClickChecklistsMapOptionInImprovingPopupMenu can not be parsed.");
+		}
+		catch (IOException ie) {
+			throw new UIAutomationException("File IMP0033_ClickChecklistsMapOptionInImprovingPopupMenu.xml not found.");
+		}
+		catch (ParserConfigurationException pe) {
+			throw new UIAutomationException("File IMP0033_ClickChecklistsMapOptionInImprovingPopupMenu can not be parsed.");
+		}
+			
+	}
+}
