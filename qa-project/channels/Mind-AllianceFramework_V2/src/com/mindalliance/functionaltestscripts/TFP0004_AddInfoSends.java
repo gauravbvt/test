36493d5560346226679d1136069098cc3307c453
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2014-02-19 12:05:24 +0000

    git-svn-id: https://wush.net/svn/mindalliance@5919 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/functionaltestscripts/TFP0004_AddInfoSends.java
@@ -0,0 +1,253 @@
+package com.mindalliance.functionaltestscripts;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import com.mindalliance.configuration.BrowserController;
+import com.mindalliance.configuration.Configuration;
+import com.mindalliance.configuration.DataController;
+import com.mindalliance.configuration.ElementController;
+import com.mindalliance.configuration.GlobalVariables;
+import com.mindalliance.configuration.Log4J;
+import com.mindalliance.configuration.LogFunctions;
+import com.mindalliance.configuration.Reporting;
+import com.mindalliance.configuration.UIAutomationException;
+import com.mindalliance.pages.DomainPlanPage;
+import com.mindalliance.pages.HeaderController;
+import com.mindalliance.pages.HomePage;
+import com.mindalliance.pages.LoginPage;
+import com.mindalliance.pages.PlanPage;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ * TestCase Id: TFP0004_AddInfoSends 
+ * Summary: Verify by clicking on 'Add' in Sends panel info sharing capability gets added.
+ * @author afour
+ *
+ */
+public class TFP0004_AddInfoSends extends TestCase {
+	public Hashtable<String, String> testData;
+	public String testCaseId="TFP0004_AddInfoSends";
+	public String description=null;
+	public int stepNo=1;
+	public String passed="Pass";
+	public String failed="Fail";
+	public String blank=""; 
+	public String exception="";
+	public String browser="";
+	public String scriptException;
+	
+	/**
+	 * This method will initialize the setup required for every test case
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Before
+	protected void setUp() throws UIAutomationException{
+		try{
+			if (GlobalVariables.configuration == null){
+					GlobalVariables.configuration = Configuration.getConfigurationObject();
+			}
+			if(GlobalVariables.configuration.getAttrSearchList() == null){
+				new ElementController();
+			}
+			DataController dataController= new DataController();
+			dataController.createResultFiles();
+			
+			GlobalVariables.configuration.addTestCaseIdToJList(testCaseId);	
+			// Loads Test Data
+			description = "Testcase: " + testCaseId + " execution started";
+			loadTestData();
+			// Write log			
+			LogFunctions.writeLogs(description);
+						
+			// Creates Browser instance
+			BrowserController browserController= new BrowserController();
+			browserController.initializeDriver();		
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Browser initialized");	
+			
+		}
+		catch(UIAutomationException ue){
+			stepNo++;
+			description="Unable to initialize the driver";
+			Assert.fail("Unable to initialize the driver"+ue.getErrorMessage());
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo, ue.getErrorMessage(), failed, scriptException, blank);
+			Log4J.getlogger(this.getClass()).error(testCaseId +"Unable to initialize the driver");	
+			
+		}
+	}
+	/**
+	 *  This method verifies info sharing capability is added
+	 * @throws UIAutomationException
+	 * @throws IOException 
+	 */
+	@Test
+	public void testTFP0004_AddInfoSends() throws UIAutomationException, IOException {
+		try{
+		    // Enter URL of Channels
+			stepNo++;
+			description="URL Entered";	
+			BrowserController browserController=new BrowserController();
+			browserController.enterURL();
+			// Write log			
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);    
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"URL Entered");	
+			    
+			// Login page
+			stepNo++;
+			description="Login successful";	
+		    LoginPage loginPage = new LoginPage();
+		    loginPage.Login(GlobalVariables.configuration.getConfigData().get("UserName"),GlobalVariables.configuration.getConfigData().get("PassWord"));
+		    // Write log			
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);    
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Login successful");	
+			
+			// Domain Plans
+			stepNo++;
+			description="Domain Plans";
+			DomainPlanPage domainPlanPage= new DomainPlanPage();
+			domainPlanPage.clickDomainPlans();	
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Domain Plans");	
+			
+			// Plan Page
+			stepNo++;
+			description="Navigated to Plan page";
+			HomePage homePage=new HomePage();
+			homePage.clickDomainPlanEditor();	
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	 
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Navigated to Plan page");	
+			
+			// Click on 'Add' button under 'Sends' panel
+			stepNo++;
+			description="Add info sharing capability";
+			PlanPage planPage = new PlanPage();
+			planPage.clickAddInfoSendsPanel();
+			 // Write log			
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);    
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Add info sharing capability");	
+					
+			// Click on 'Remove Info Sharing Capability' under 'Actions' pop up menu
+ 			stepNo++;
+			description="Remove Info Sharing Capability";
+			planPage.clickPopupMenu(testData.get("ActionsInSendsPanel"));
+			planPage.clickSubmenu(testData.get("RemoveInfoSharingCapabilityInSendsPanel"));	
+			 // Write log			
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);    
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Remove Info Sharing Capability");	
+							
+			// Sign Out from 'Plan' page
+			stepNo++;
+			description="Logout successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOutPlan();
+			 // Write log			
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);    
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Logout successful");	
+			
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+		}catch (UIAutomationException ue) {
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo,description,failed, ue.getErrorMessage(), blank);
+			Reporting.getScreenShot(testCaseId);
+			Log4J.getlogger(this.getClass()).error(testCaseId +ue.getErrorMessage());	
+			
+			// Sign out from home page
+		    stepNo++;
+		    description="Logout successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOut();
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Logout successful");	
+				
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+			// Quits the Browser
+			GlobalVariables.configuration.getWebDriver().quit();
+			Assert.fail(ue.getErrorMessage());		
+		}
+	}
+	
+	/**
+	 * (non-Javadoc)
+	 * This method will perform cleanup actions
+	 * @see junit.framework.TestCase#tearDown()
+	*/	
+	
+	@After
+	protected void tearDown(){
+		if(GlobalVariables.configuration.getWebDriver()!=null){
+			GlobalVariables.configuration.getWebDriver().quit();
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Browser Quit");	
+			}
+	}
+	
+	/**
+     * Loads Test Data for TFP0004_AddInfoSends.
+     * @throws UIAutomationException
+     */
+	public void loadTestData() throws UIAutomationException{
+		try{
+			testData=new Hashtable<String,String>();
+			File currentDir=new File(".");
+			
+			String path= currentDir.getCanonicalPath().toString() + "\\TestData\\";
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder db = dbf.newDocumentBuilder();
+			File TFP0004_AddInfoSends=new File(path + "TFP0004_AddInfoSends.xml");
+			
+			Document docTFP0004_AddInfoSends=db.parse(TFP0004_AddInfoSends);
+			Element eleTFP0004_AddInfoSends=docTFP0004_AddInfoSends.getDocumentElement();
+	              
+	        Element oXmlEleTFP0004_AddInfoSends = (Element) eleTFP0004_AddInfoSends;
+	        
+	               	
+	        this.testData.put("ChannelsURL",oXmlEleTFP0004_AddInfoSends.getElementsByTagName("channelsURL").item(0).getChildNodes().item(0).getNodeValue());
+		 	this.testData.put("Title",oXmlEleTFP0004_AddInfoSends.getElementsByTagName("title").item(0).getChildNodes().item(0).getNodeValue());
+		 	this.testData.put("ActionsInSendsPanel",oXmlEleTFP0004_AddInfoSends.getElementsByTagName("actionsInSendsPanel").item(0).getChildNodes().item(0).getNodeValue());
+		 	this.testData.put("RemoveInfoSharingCapabilityInSendsPanel",oXmlEleTFP0004_AddInfoSends.getElementsByTagName("removeInfoSharingCapabilityInSendsPanel").item(0).getChildNodes().item(0).getNodeValue());
+		 }
+		catch(SAXException se){
+			throw new UIAutomationException("File TFP0004_AddInfoSends not found.");
+		}
+		catch (IOException ie) {
+			throw new UIAutomationException("File TFP0004_AddInfoSends.xml not found.");
+		}
+		catch (ParserConfigurationException pe) {
+			throw new UIAutomationException("File TFP0004_AddInfoSends can not be parsed.");
+		}
+	}
+}
