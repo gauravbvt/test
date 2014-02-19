0b50216f6ea949febe3264f472d4c3ad8d64cf0e
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2014-02-19 12:33:16 +0000

    git-svn-id: https://wush.net/svn/mindalliance@5927 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/functionaltestscripts/CCS0002_ClickCheckListsIconPresentInTaskPanel.java
@@ -0,0 +1,240 @@
+package com.mindalliance.functionaltestscripts;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import com.mindalliance.configuration.BrowserController;
+import com.mindalliance.configuration.Configuration;
+import com.mindalliance.configuration.DataController;
+import com.mindalliance.configuration.ElementController;
+import com.mindalliance.configuration.GlobalVariables;
+import com.mindalliance.configuration.LogFunctions;
+import com.mindalliance.configuration.Reporting;
+import com.mindalliance.configuration.Log4J;
+import com.mindalliance.configuration.UIAutomationException;
+import com.mindalliance.pages.DomainPlanPage;
+import com.mindalliance.pages.HeaderController;
+import com.mindalliance.pages.LoginPage;
+import com.mindalliance.pages.HomePage;
+import com.mindalliance.pages.PlanPage;
+
+/**
+ * Testcase ID: CCS0002_ClickCheckListsIconPresentInTaskPanel
+ * 	   Summary: Click on Checklists icon to verify whether "Checklists" window opens
+ * @author Afour
+ *
+ */
+
+public class CCS0002_ClickCheckListsIconPresentInTaskPanel extends TestCase {
+	public Hashtable<String, String> testData;
+	public String testCaseId="CCS0002_ClickCheckListsIconPresentInTaskPanel";
+	public String description=null;
+	public int stepNo=1;
+	public String passed="Pass";
+	public String failed="FAIL";
+	public String blank=""; 
+	public String scriptException;
+	public String browser="";
+
+	/*
+	 * This method will initialize the setup required for every test case
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Before
+	protected void setUp() throws UIAutomationException{
+		try{
+			if (GlobalVariables.configuration == null){
+					GlobalVariables.configuration = Configuration.getConfigurationObject();
+			}
+			if(GlobalVariables.configuration.getAttrSearchList() == null){
+				new ElementController();
+			}
+			DataController dataController= new DataController();
+			dataController.createResultFiles();
+			
+			GlobalVariables.configuration.addTestCaseIdToJList(testCaseId);	
+			// Loads Test Data
+			description = "Testcase: " + testCaseId + " execution started";
+			loadTestData();
+			// Write log			
+			LogFunctions.writeLogs(description);
+						
+			// Creates Browser instance
+			BrowserController browserController= new BrowserController();
+			browserController.initializeDriver();		
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Browser initialized");
+			
+		}
+		catch(UIAutomationException ue){
+			stepNo++;
+			description="Unable to initialize the driver";
+			Assert.fail("Unable to initialize the driver"+ue.getErrorMessage());
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo, ue.getErrorMessage(), failed, scriptException, blank);
+			Log4J.getlogger(this.getClass()).error(testCaseId +"Unable to initialize the driver");
+		}
+	}
+	
+	@Test
+	public void testCCS0002_ClickCheckListsIconPresentInTaskPanel() throws UIAutomationException, IOException {
+		try{
+			stepNo++;
+			description="URL Entered";
+			// Enter URL of Channels
+			BrowserController browserController=new BrowserController();
+			browserController.enterURL();
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"URL Entered");
+ 			
+			// Login page
+			stepNo++;
+			description="Login Successful";
+			LoginPage loginPage = new LoginPage();
+		    loginPage.Login(GlobalVariables.configuration.getConfigData().get("UserName"),GlobalVariables.configuration.getConfigData().get("PassWord"));
+		    // Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Login Successful");
+ 			
+ 			 // Domain Plans
+ 		    stepNo++;
+ 			description="Domain Plans";
+ 			DomainPlanPage domainPlanPage= new DomainPlanPage();
+ 			domainPlanPage.clickDomainPlans();	
+ 			// Write log			
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Domain Plans");
+ 			
+			// Plan Page
+		    stepNo++;
+			description="Domain Plan Editor";
+			HomePage homePage=new HomePage();
+			homePage.clickDomainPlanEditor();	
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Domain Plan Editor");
+ 			
+ 			//Click Checklists icon
+ 			stepNo++;
+ 			description="Click Checklists Icon";
+ 			PlanPage planPage=new PlanPage();
+ 			planPage.clickChecklistsIcon();
+ 			 // Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Click Checklists Icon");
+ 			
+ 			// Sign out from home page
+		    stepNo++;
+		    description="Logout successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOut();
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Logout Successful");	
+			
+ 			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+		}catch (UIAutomationException ue) {
+			stepNo++;
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo,description,failed, ue.getErrorMessage(), blank);
+			Reporting.getScreenShot(testCaseId);
+			Log4J.getlogger(this.getClass()).error(testCaseId +ue.getErrorMessage());
+			
+			// Sign out from home page
+		    stepNo++;
+		    description="Logout successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOut();
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Logout Successful");	
+			
+			
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+			// Quits the Browser
+			GlobalVariables.configuration.getWebDriver().quit();
+			Assert.fail(ue.getErrorMessage());		
+		}
+	}
+	
+	/*
+	 * This method will perform cleanup actions
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	
+	@After
+	protected void tearDown(){
+		if(GlobalVariables.configuration.getWebDriver()!=null){
+			GlobalVariables.configuration.getWebDriver().quit();
+			Log4J.getlogger(this.getClass()).info(testCaseId +" Browser quit");
+		}
+	}
+	
+	/**
+     * Loads Test Data for CCS0002_ClickCheckListsIconPresentInTaskPanel.
+     * @throws UIAutomationException
+     */
+	public void loadTestData() throws UIAutomationException{
+		try{
+			String startTime=LogFunctions.getDateTime();
+			GlobalVariables.configuration.setStartTime(startTime);
+			testData=new Hashtable<String,String>();
+			File currentDir=new File(".");
+			
+			String path= currentDir.getCanonicalPath().toString() + "\\TestData\\";
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder db = dbf.newDocumentBuilder();
+			File CCS0002_ClickCheckListsIconPresentInTaskPanel=new File(path + "CCS0002_ClickCheckListsIconPresentInTaskPanel.xml");
+			
+			Document docCCS0002_ClickCheckListsIconPresentInTaskPanel=db.parse(CCS0002_ClickCheckListsIconPresentInTaskPanel);
+			Element eleCCS0002_ClickCheckListsIconPresentInTaskPanel=docCCS0002_ClickCheckListsIconPresentInTaskPanel.getDocumentElement();
+	              
+	        Element oXmlEleCCS0002_ClickCheckListsIconPresentInTaskPanel = (Element) eleCCS0002_ClickCheckListsIconPresentInTaskPanel;
+	       	
+			this.testData.put("ChannelsURL",oXmlEleCCS0002_ClickCheckListsIconPresentInTaskPanel.getElementsByTagName("channelsURL").item(0).getChildNodes().item(0).getNodeValue());
+			this.testData.put("Title",oXmlEleCCS0002_ClickCheckListsIconPresentInTaskPanel.getElementsByTagName("title").item(0).getChildNodes().item(0).getNodeValue());
+			
+		}
+		catch(SAXException se){
+			throw new UIAutomationException("File CCS0002_ClickCheckListsIconPresentInTaskPanel can not be parsed.");
+		}
+		catch (IOException ie) {
+			throw new UIAutomationException("File CCS0002_ClickCheckListsIconPresentInTaskPanel.xml not found.");
+		}
+		catch (ParserConfigurationException pe) {
+			throw new UIAutomationException("File CCS0002_ClickCheckListsIconPresentInTaskPanel can not be parsed.");
+		}
+			
+	}
+}
