25ff4fd165abe81a15a36e9c1177570c007a7607
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2014-02-24 12:10:26 +0000

    git-svn-id: https://wush.net/svn/mindalliance@5958 baad322d-9929-0410-88f0-f92e8ff3e1bd

---
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/functionaltestscripts/ISR0001_ViewIssuesSummaryReport.java
@@ -0,0 +1,231 @@
+package com.mindalliance.functionaltestscripts;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import com.mindalliance.configuration.BrowserController;
+import com.mindalliance.configuration.Configuration;
+import com.mindalliance.configuration.DataController;
+import com.mindalliance.configuration.ElementController;
+import com.mindalliance.configuration.GlobalVariables;
+import com.mindalliance.configuration.Log4J;
+import com.mindalliance.configuration.LogFunctions;
+import com.mindalliance.configuration.Reporting;
+import com.mindalliance.configuration.UIAutomationException;
+import com.mindalliance.pages.DomainPlanPage;
+import com.mindalliance.pages.HeaderController;
+import com.mindalliance.pages.LoginPage;
+
+/**
+ * Testcase ID: ISR0001_ViewIssuesSummaryReport
+ * 	   Summary: Verify that user is able to view the Issues Summary Report page
+ * @author Afour
+ *
+ */
+public class ISR0001_ViewIssuesSummaryReport extends TestCase {
+
+	public Hashtable<String, String> testData;
+    public String testCaseId="ISR0001_ViewIssuesSummaryReport";
+    public String description=null;
+    public int stepNo=1;
+    public String passed="Pass";
+    public String failed="Fail";
+    public String blank=""; 
+    public String exception="";
+    public String browser="";
+	
+	/**
+	 * This method will initialize the setup required for every test case
+	 * @throws UIAutomationException 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Before
+	protected void setUp() throws UIAutomationException{
+		try{
+			if (GlobalVariables.configuration == null){
+					GlobalVariables.configuration = Configuration.getConfigurationObject();
+			}
+			if(GlobalVariables.configuration.getAttrSearchList() == null){
+				new ElementController();
+			}
+			DataController dataController= new DataController();
+			dataController.createResultFiles();
+			
+			GlobalVariables.configuration.addTestCaseIdToJList(testCaseId);	
+			// Loads Test Data
+			description = "Testcase: " + testCaseId + " execution started";
+			loadTestData();
+			// Write log			
+			LogFunctions.writeLogs(description);
+						
+			// Creates Browser instance
+			BrowserController browserController= new BrowserController();
+			browserController.initializeDriver();	
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Browser initialized");	
+			
+		}
+		catch(UIAutomationException ue){
+			stepNo++;
+			Assert.fail("Unable to initialize the driver"+ue.getErrorMessage());
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo,exception,failed, ue.getErrorMessage(), blank);
+			Log4J.getlogger(this.getClass()).error(testCaseId +"Unable to initialize the driver");	
+			
+		}
+	}
+	/**
+	 * This method Verifies that user successfully navigates to Issues Summary Report  page.
+	 * @throws UIAutomationException
+	 * @throws IOException 
+	 * @throws InterruptedException 
+	 */
+	@Test
+	public void testISR0001_ViewIssuesSummaryReport() throws UIAutomationException, IOException, InterruptedException{
+		try{
+			// Enter URL of Channels
+			stepNo++;
+			description="URL Entered";
+			BrowserController browserController=new BrowserController();
+			browserController.enterURL();
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);		
+			Log4J.getlogger(this.getClass()).info(testCaseId +"URL Entered");	
+			    
+			// Login page
+			stepNo++;
+			description="Login successful";
+		    LoginPage loginPage = new LoginPage();
+		    loginPage.Login(GlobalVariables.configuration.getConfigData().get("UserName"),GlobalVariables.configuration.getConfigData().get("PassWord"));
+		    // Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);		
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Login successful");	
+			
+			//Click on Collaboration Templates link
+			stepNo++;
+	 		description="Collaboration Templates";
+	 		DomainPlanPage domainPlanPage= new DomainPlanPage();
+	 		domainPlanPage.clickDomainPlans();	
+	 		// Write log			
+	 		LogFunctions.writeLogs(description);
+	 		LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+	 		Log4J.getlogger(this.getClass()).info(testCaseId +"Collaboration Templates");	
+			
+	 		//Click on Template Issues Link
+	 		stepNo++;
+	 		description="Template Issues link";
+	 		domainPlanPage.clickTemplateIssuesLink();
+	 		// Write log			
+	 		LogFunctions.writeLogs(description);
+	 		LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+	 		Log4J.getlogger(this.getClass()).info(testCaseId +"Template Issues link");	
+				
+			// Sign Out from 'Home' page
+			stepNo++;
+			description="Logout successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOut();
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Logout successful");	
+			
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+		}catch (UIAutomationException ue) {
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo,description,failed, ue.getErrorMessage(), blank);
+			Reporting.getScreenShot(testCaseId);
+			Log4J.getlogger(this.getClass()).info(testCaseId +ue.getErrorMessage());	
+			
+			// Sign out from home page
+		    stepNo++;
+		    description="Logout successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOut();
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Logout successful");	
+				
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+			// Quits the Browser
+			GlobalVariables.configuration.getWebDriver().quit();
+			Assert.fail(ue.getErrorMessage());		
+		}
+	}
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	
+	@After
+	protected void tearDown(){
+		if(GlobalVariables.configuration.getWebDriver()!=null){
+			GlobalVariables.configuration.getWebDriver().quit();
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Browser Quit");	
+			}
+	}
+	/**
+     * Loads Test Data for ISR0001_ViewIssuesSummaryReport.
+     * @throws UIAutomationException
+     */
+	public void loadTestData() throws UIAutomationException
+	{
+		try{
+			String startTime=LogFunctions.getDateTime();
+			GlobalVariables.configuration.setStartTime(startTime);
+			
+			testData=new Hashtable<String,String>();
+			File currentDir=new File(".");
+			
+			String path= currentDir.getCanonicalPath().toString() + "\\TestData\\";
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder db = dbf.newDocumentBuilder();
+			File ISR0001_ViewIssuesSummaryReport=new File(path + "ISR0001_ViewIssuesSummaryReport.xml");
+			
+			Document docISR0001_ViewIssuesSummaryReport=db.parse(ISR0001_ViewIssuesSummaryReport);
+			Element eleISR0001_ViewIssuesSummaryReport=docISR0001_ViewIssuesSummaryReport.getDocumentElement();
+	              
+	        Element oXmlEleISR0001_ViewIssuesSummaryReport = (Element) eleISR0001_ViewIssuesSummaryReport;
+	                     	
+			this.testData.put("ChannelsURL",oXmlEleISR0001_ViewIssuesSummaryReport.getElementsByTagName("channelsURL").item(0).getChildNodes().item(0).getNodeValue());
+			this.testData.put("Title",oXmlEleISR0001_ViewIssuesSummaryReport.getElementsByTagName("title").item(0).getChildNodes().item(0).getNodeValue());
+			
+		}
+		catch(SAXException se){
+			throw new UIAutomationException("File ISR0001_ViewIssuesSummaryReport not found.");
+		}
+		catch (IOException ie) {
+			throw new UIAutomationException("File ISR0001_ViewIssuesSummaryReport.xml not found.");
+		}
+		catch (ParserConfigurationException pe) {
+			throw new UIAutomationException("File ISR0001_ViewIssuesSummaryReport can not be parsed.");
+		}
+	}
+}
---
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/functionaltestscripts/SQ0015_ActivateAQuestionnaire.java
@@ -0,0 +1,283 @@
+package com.mindalliance.functionaltestscripts;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import com.mindalliance.configuration.BrowserController;
+import com.mindalliance.configuration.Configuration;
+import com.mindalliance.configuration.DataController;
+import com.mindalliance.configuration.ElementController;
+import com.mindalliance.configuration.GlobalVariables;
+import com.mindalliance.configuration.Log4J;
+import com.mindalliance.configuration.LogFunctions;
+import com.mindalliance.configuration.Reporting;
+import com.mindalliance.configuration.UIAutomationException;
+import com.mindalliance.pages.DomainPlanPage;
+import com.mindalliance.pages.HeaderController;
+import com.mindalliance.pages.HomePage;
+import com.mindalliance.pages.LoginPage;
+import com.mindalliance.pages.PlanPage;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+/**
+ * Test Case ID: SQ0015_ActivateAQuestionnaire
+ * Summary: Verify that Questionnaire can be added
+ * @author afour
+ *
+ */
+public class SQ0015_ActivateAQuestionnaire extends TestCase {
+	public Hashtable<String, String> testData;
+	public String testCaseId="SQ0015_ActivateAQuestionnaire";
+	public String description=null;
+	public int stepNo=1;
+	public String passed="Pass";
+	public String failed="FAIL";
+	public String blank=""; 
+	public String scriptException;
+	public String browser="";
+
+	/*
+	 * This method will initialize the setup required for every test case
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Before
+	protected void setUp() throws UIAutomationException{
+		try{
+			if (GlobalVariables.configuration == null){
+					GlobalVariables.configuration = Configuration.getConfigurationObject();
+			}
+			if(GlobalVariables.configuration.getAttrSearchList() == null){
+				new ElementController();
+			}
+			DataController dataController= new DataController();
+			dataController.createResultFiles();
+			
+			GlobalVariables.configuration.addTestCaseIdToJList(testCaseId);	
+			// Loads Test Data
+			description = "Testcase: " + testCaseId + " execution started";
+			loadTestData();
+			// Write log			
+			LogFunctions.writeLogs(description);
+						
+			// Creates Browser instance
+			BrowserController browserController= new BrowserController();
+			browserController.initializeDriver();	
+			// Write log
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Browser initialized");	
+			
+		}catch(UIAutomationException ue){
+			stepNo++;
+			description="Unable to initialize the driver";
+			Assert.fail("Unable to initialize the driver"+ue.getErrorMessage());
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo, ue.getErrorMessage(), failed, scriptException, blank);
+			Log4J.getlogger(this.getClass()).error(testCaseId +"Unable to initialize the driver");	
+			}
+	}
+	@Test
+	public void testSQ0015_ActivateAQuestionnaire() throws UIAutomationException, IOException{
+		try {
+			stepNo++;
+			description="URL Entered";
+			// Enter URL of Channels
+			BrowserController browserController=new BrowserController();
+			browserController.enterURL();
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"URL Entered");	
+			
+			// Login page
+			stepNo++;
+			description="Login Successful";
+			LoginPage loginPage = new LoginPage();
+		    loginPage.Login(GlobalVariables.configuration.getConfigData().get("UserName"),GlobalVariables.configuration.getConfigData().get("PassWord"));
+		    // Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Login Successful");	
+							
+ 		    // Domain Plans
+ 		    stepNo++;
+ 			description="Domain Plans";
+ 			DomainPlanPage domainPlanPage= new DomainPlanPage();
+ 			domainPlanPage.clickDomainPlans();	
+ 			// Write log			
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Domain Plans");	
+			
+			// Plan Page
+		    stepNo++;
+			description="Domain Plan Editor";
+			HomePage homePage=new HomePage();
+			homePage.clickDomainPlanEditor();	
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Domain Plan Editor");	
+				
+			// Click on 'All Surveys' under 'Learning' pop up menu
+ 			stepNo++;
+			description="All Surveys";
+			PlanPage planPage=new PlanPage();
+			planPage.clickPopupMenu(testData.get("Learning"));
+			planPage.clickSubmenu(testData.get("AllSurveys"));
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"All Surveys");	
+			
+			// Click on 'Questionnaire' tab
+ 			stepNo++;
+			description="Questionnaire Tab";
+			planPage.clickQuestionnaireTab();
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Questionnaire Tab");	
+			
+			// Add Questionnaire
+ 			stepNo++;
+			description="Add Questionnaire";
+			planPage.clickAddNewQuestionnaire();
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Add Questionnaire");	
+			
+			// Verify Questionnaire is added
+ 			stepNo++;
+			description="Questionnaire Added";
+			planPage.verifyQuestionnaireIsAdded(testData.get("Unnamed"));
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Questionnaire Added");	
+			
+ 			// Activate the Questionnaire
+ 			stepNo++;
+			description="Activate the Questionnaire";
+			planPage.activateQuestionnaire();
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Activate the Questionnaire");	
+			
+			// Close 'All Surveys' window
+ 			stepNo++;
+			description="Close All Surveys Window";
+			planPage.closeAllSurveysWindow();
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"Close All Surveys Window");	
+							
+			//Sign Out from 'Plan' page
+ 			stepNo++;
+			description="SignOut Successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOutPlan();
+			// Write log
+ 			LogFunctions.writeLogs(description);
+ 			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);
+ 			Log4J.getlogger(this.getClass()).info(testCaseId +"SignOut Successful");	
+			
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+		}catch (UIAutomationException ue) {
+			// Write log
+			LogFunctions.writeLogs(ue.getErrorMessage());
+			LogFunctions.writeResults(testCaseId, stepNo,description,failed, ue.getErrorMessage(), blank);
+			Reporting.getScreenShot(testCaseId);
+			Log4J.getlogger(this.getClass()).error(testCaseId +ue.getErrorMessage());	
+			
+			// Sign out from home page
+		    stepNo++;
+		    description="Logout successful";
+			HeaderController headerController=new HeaderController();
+			headerController.signOut();
+			// Write log			
+			LogFunctions.writeLogs(description);
+			LogFunctions.writeResults(testCaseId,stepNo, description,passed,blank,blank);	
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Logout successful");	
+				
+			Reporting reporting= new Reporting();
+		    reporting.generateAutomationReport();
+		    
+			// Quits the Browser
+			GlobalVariables.configuration.getWebDriver().quit();
+			Assert.fail(ue.getErrorMessage());		
+		}
+	}
+
+	/*
+	 * This method will perform cleanup actions
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	
+	@After
+	protected void tearDown(){
+		if(GlobalVariables.configuration.getWebDriver()!=null){
+			GlobalVariables.configuration.getWebDriver().quit();
+			Log4J.getlogger(this.getClass()).info(testCaseId +"Browser Quit");	
+			}
+	}
+	/**
+     * Loads Test Data for SQ0015_ActivateAQuestionnaire
+     * @throws UIAutomationException
+     */
+	public void loadTestData() throws UIAutomationException
+	{
+		try{
+			String startTime=LogFunctions.getDateTime();
+			GlobalVariables.configuration.setStartTime(startTime);
+			testData=new Hashtable<String,String>();
+			File currentDir=new File(".");
+			
+			String path= currentDir.getCanonicalPath().toString() + "\\TestData\\";
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder db = dbf.newDocumentBuilder();
+			File SQ0015_ActivateAQuestionnaire=new File(path + "SQ0015_ActivateAQuestionnaire.xml");
+			
+			Document docSQ0015_ActivateAQuestionnaire=db.parse(SQ0015_ActivateAQuestionnaire);
+			Element eleSQ0015_ActivateAQuestionnaire=docSQ0015_ActivateAQuestionnaire.getDocumentElement();
+	              
+	        Element oXmlEleSQ0015_ActivateAQuestionnaire = (Element) eleSQ0015_ActivateAQuestionnaire;           
+	        
+	        this.testData.put("Title",oXmlEleSQ0015_ActivateAQuestionnaire.getElementsByTagName("title").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("Unnamed",oXmlEleSQ0015_ActivateAQuestionnaire.getElementsByTagName("unnamed").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("Learning", oXmlEleSQ0015_ActivateAQuestionnaire.getElementsByTagName("learning").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("AllSurveys", oXmlEleSQ0015_ActivateAQuestionnaire.getElementsByTagName("allSurveys").item(0).getChildNodes().item(0).getNodeValue());
+	        this.testData.put("Questionnaire", oXmlEleSQ0015_ActivateAQuestionnaire.getElementsByTagName("questionnaire").item(0).getChildNodes().item(0).getNodeValue());
+		}
+		catch(SAXException se){
+			throw new UIAutomationException("File SQ0015_ActivateAQuestionnaire.xml can not be parsed.");
+		}
+		catch (IOException ie) {
+			throw new UIAutomationException("File SQ0015_ActivateAQuestionnaire.xml not found.");
+		}
+		catch (ParserConfigurationException pe) {
+			throw new UIAutomationException("File SQ0015_ActivateAQuestionnaire.xml can not be parsed.");
+		}
+	}
+
+}
