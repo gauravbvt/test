6d03db9a63debd6ab06ba3321d965f26b8ca36e6
afourtech <afourtech@baad322d-9929-0410-88f0-f92e8ff3e1bd>
2014-03-17 14:01:03 +0000

    git-svn-id: https://wush.net/svn/mindalliance@6145 baad322d-9929-0410-88f0-f92e8ff3e1bd

--- qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/functionaltestscripts/package-info.java
+++ qa-project/channels/Mind-AllianceFramework_V2/src/com/mindalliance/functionaltestscripts/package-info.java
@@ -1,8 +1,5 @@
 /**
- * 
- */
-/**
- * @author Afour
- *
+ * This package contains all the automated test scripts which are called when the functional suite starts execution.
+ * @author AFourTech
  */
 package com.mindalliance.functionaltestscripts;
